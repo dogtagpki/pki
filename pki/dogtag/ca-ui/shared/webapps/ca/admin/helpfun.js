42a3125c4ff53da6a29a2ce4c426bc2bc0d22e02
mharmsen <mharmsen@c9f7a03b-bd48-0410-a16d-cbbf54688b0b>
2010-07-29 19:04:00 +0000

    Bugzilla Bug #472597 - Disable policy code,UI
    git-svn-id: svn+ssh://svn.fedorahosted.org/svn/pki/trunk@1139 c9f7a03b-bd48-0410-a16d-cbbf54688b0b

---
+++ pki/dogtag/ca-ui/shared/webapps/ca/admin/helpfun.js
@@ -0,0 +1,35 @@
+// --- BEGIN COPYRIGHT BLOCK ---
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// Copyright (C) 2007 Red Hat, Inc.
+// All rights reserved.
+// --- END COPYRIGHT BLOCK ---
+
+function help(helptopic) { 
+
+     var HelpWin=window.open("","MyWin", "toolbar=no,directories=no,menubar=no,status=no,scrollbars=yes,resizable=yes,width=500,height=500"); 
+
+     HelpWin.location = helptopic;
+     HelpWin.focus();
+
+}
+
+function helpstatus(helpline) {
+
+     window.status = helpline;
+
+     return true;
+
+}
+
