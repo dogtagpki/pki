bbd73cc0eb4dca54484eef51cf778f899bb19904
Niranjan Mallapadi <mrniranjan@redhat.com>
2014-01-31 18:07:31 +0530

    Modified pki-cert-show.sh to add Test Description
    And also modified to use CA Agent Cert instead of Administrator

--- tests/dogtag/acceptance/cli-tests/pki-cert-cli/pki-cert-show.sh
+++ tests/dogtag/acceptance/cli-tests/pki-cert-cli/pki-cert-show.sh
@@ -42,7 +42,8 @@ run_pki_cert_show()
 {
 
 	local invalid_serialNumber=`cat /dev/urandom | tr -dc '1-9' | fold -w 10 | head -n 1`
-	
+	CA_agentV_user=CA_agentV
+
 	# Creating Temporary Directory for pki cert-show
 
         rlPhaseStartSetup "pki cert-show Temporary Directory"
@@ -50,46 +51,34 @@ run_pki_cert_show()
         rlRun "pushd $TmpDir"
         rlPhaseEnd
 	
-	rlPhaseStartSetup "Executing pki cert show"
-	rlLog "Executing pki cert-show"
-	rlRun "pki cert-show 1> $TmpDir/cert-show.out" 1 
+	rlPhaseStartSetup "pki_cert_cli-configtest: pki cert-show --help configuration test"
+	rlRun "pki cert-show --help > $TmpDir/cert-show.out 2>&1" 0 "pki cert-show --help"
 	rlAssertGrep "usage:" "$TmpDir/cert-show.out"
     	rlAssertGrep "--encoded         Base-64 encoded" "$TmpDir/cert-show.out"
     	rlAssertGrep "--output <file>   Output file" "$TmpDir/cert-show.out"
     	rlAssertGrep "--pretty          Pretty print" "$TmpDir/cert-show.out"
+	rlAssertNotGrep "Error: Unrecognized option: --help" "$TmpDir/cert-show.out"
+	rlLog "FAIL :: https://engineering.redhat.com/trac/pki-tests/ticket/490"
 	rlPhaseEnd
 
-	# Import CA Agent and CA Cert to CERTDB Directory
-
-	rlPhaseStartSetup "Import CA agent cert into a nss certificate db and trust CA root cert"
-        admin_cert_nickname="PKI Administrator for $CA_DOMAIN"
-        rlRun "source /opt/rhqa_pki/env.sh"
-	local MY_CERTDB_DIR=$TmpDir/certdb
-        rlLog "Admin Certificate is located at: $CA_ADMIN_CERT_LOCATION"
-        rlLog "importP12FileNew $CA_ADMIN_CERT_LOCATION $CA_CLIENT_PKCS12_PASSWORD $MY_CERTDB_DIR $CERTDB_DIR_PASSWORD $admin_cert_nickname"
-        rlRun "importP12FileNew $CA_ADMIN_CERT_LOCATION $CA_CLIENT_PKCS12_PASSWORD $MY_CERTDB_DIR $CERTDB_DIR_PASSWORD $admin_cert_nickname" 0 "Import Admin certificate to $MY_CERTDB_DIR"
-        rlRun "install_and_trust_CA_cert $CA_SERVER_ROOT $MY_CERTDB_DIR"
-	rlPhaseEnd
-	
 	# Create a Temporary NSS DB Directory and generate Certificate
 
-	rlPhaseStartSetup "Generating temporary Cert"
+	rlPhaseStartSetup "Generating temporary Cert to be used pki cert-show automation Tests"
         local TEMP_NSS_DB="$TmpDir/nssdb"
         local reqstatus
         local requestid
-        rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 \"Test1 User\" "test1" "test1@example.org" "Marketing" "FooBar.Inc" "IN" "--" "reqstatus" "requestid""
+        rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 "--" "--" "--" "--" "--" "--" "--" "reqstatus" "requestid""
 	rlLog "To Approve the request we would need CA Admin Cert Nick Name stored in $MY_CERTDB_DIR"
-	local MY_ADMIN_NICK=`certutil -L -d $MY_CERTDB_DIR  | grep "PKI Administrator" | awk -F "     " '{print $1}'`
         rlLog "Approve Certificate requeset"
-        rlRun "pki -d $MY_CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \"$MY_ADMIN_NICK\" ca-cert-request-review $requestid --action approve 1> $TmpDir/pki-approve-out"
+        rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \"$CA_agentV_user\" ca-cert-request-review $requestid --action approve 1> $TmpDir/pki-approve-out"
 	rlAssertGrep "Approved certificate request $requestid" "$TmpDir/pki-approve-out"
 	rlRun "valid_serialNumber=`pki cert-request-show $requestid | grep \"Certificate ID\" | sed 's/ //g' | cut -d: -f2`"
         rlPhaseEnd
 
 
-	#Run pki cert-show with valid serial number in HexDecimal
+	#Run pki cert-show with valid serial number in HexaDecimal
 
-	rlPhaseStartSetup "pki_cert_show-001: Show certificate of specific serial number"
+	rlPhaseStartSetup "pki_cert_show-001: pki cert-show < valid serialNumber(HexaDecimal) >  should show Certificate Details"
 	rlLog "run get_cert_serialnumber function first to get the serial Number"
 	local temp_out1="$TmpDir/cert-show1.out"
 	rlLog "Running pki cert-show $valid_serialNumber"
@@ -112,7 +101,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show with Invalid Serial Number in decimal
 
-	rlPhaseStartSetup "pki_cert_show-003: pki cert-show should fail when invalid serial Number is given"
+	rlPhaseStartSetup "pki_cert_show-003: pki cert-show < invalid serialNumber(Decimal) > should Fail"
 	local temp_out3="$TmpDir/cert-show3.out"
 	rlRun "pki cert-show $invalid_serialNumber 2> $temp_out3" 1 "command pki cert-show $serialNumber"
 	rlAssertGrep "CertNotFoundException" "$temp_out3"
@@ -120,7 +109,7 @@ run_pki_cert_show()
 
 	# Run pki cert-show with valid serial Number given in decimal 
 
-	rlPhaseStartSetup "pki_cert_show-004: pki cert-show with valid serialNumber given in decimal"
+	rlPhaseStartSetup "pki_cert_show-004: pki cert-show < valid serialNumber(Decimal) > should show Certificate Details"
 	local temp_out4="$TmpDir/cert-show4.out"
 	local decimal_valid_serialNumber
 	decimal_valid_serialNumber=$(printf %d $valid_serialNumber)
@@ -136,7 +125,7 @@ run_pki_cert_show()
 
 	#Run pki cert-show with invalid serialNumber given in Hexadecimal
 	
-	rlPhaseStartSetup "pki_cert_show-005: pki cert-show with invalid serialNumber given in hexadecimal"
+	rlPhaseStartSetup "pki_cert_show-005: pki cert-show < invalid serialNumber(hexadecimal) > should fail"
 	local temp_out5="$TmpDir/cert-show5.out"
 	local rand=`cat /dev/urandom | tr -dc '0-9' | fold -w 5 | head -n 1`
 	local invalid_hex_serialNumber=`expr $valid_serialNumber$rand`
@@ -146,7 +135,7 @@ run_pki_cert_show()
 
 	# Run pki cert-show with Junk Characters
 
-	rlPhaseStartSetup "pki_cert_show-006: pki cert-show should fail when junk characteres are given are passed as serialNumber"
+	rlPhaseStartSetup "pki_cert_show-006: pki cert-show < junk characters > should fail to show any certificate Details"
 	local temp_out6="$TmpDir/cert-show6.out"
 	local junk="axb124?$5@@_%^$#$@\!(_)043112321412321"
 	rlLog "Executing pki cert-show \"$junk\""
@@ -156,7 +145,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show <valid serialNumber> --encoded to produce a valid pem output
 
-	rlPhaseStartSetup "pki_cert_show-007: pki cert-show with --encoded should produce a valid pem output"
+	rlPhaseStartSetup "pki_cert_show-007: pki cert-show <valid SerialNumber>  --encoded should produce a valid pem output"
 	local temp_out7="$TmpDir/cert-show7.out"
 	rlLog "Running pki cert-show $serialNumber"
         rlRun "pki cert-show $valid_serialNumber --encoded > $temp_out7" 0 "command pki cert-show $serialNumber --encoded"
@@ -172,7 +161,7 @@ run_pki_cert_show()
 	
 	#Run pki cert-show --encoded with No serial Number
 
-	rlPhaseStartSetup "pki_cert_show-008: pki cert-show with --encoded and No Serial Number should fail"
+	rlPhaseStartSetup "pki_cert_show-008: pki cert-show <No SerialNumber> --encoded should fail"
 	local temp_out8=$TmpDir/cert-show8.out
 	rlLog "Running pki cert-show <No-serial-Number> --encoded"
 	rlRun "pki cert-show --encoded 1> $temp_out8" 1
@@ -181,7 +170,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show --encoded with Invalid Serial Number
 
-	rlPhaseStartSetup "pki_cert_show-009: pki cert-show with --encoded & In-valid serial number should fail"
+	rlPhaseStartSetup "pki_cert_show-009: pki cert-show < In-Valid SerialNumber > --encoded should fail"
 	local temp_out9=$TmpDir/cert-show9.out
 	rlLog "Running pki cer-show <invalid-serial-Number> --encoded"
 	rlRun "pki cert-show $invalid_serialNumber --encoded 2> $temp_out9" 1 "pki cert-show $serialNumber"
@@ -189,7 +178,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show <valid serialNumber> --output <filename>
 
-	rlPhaseStartSetup "pki_cert_show-0010: pki cert-show with --output <filename>"
+	rlPhaseStartSetup "pki_cert_show-0010: pki cert-show <valid SerialNumber(decimal)> --output <filename> should save the Certificate in File"
 	local temp_out10=$TmpDir/cert-show10.out
 	rlLog "Running pki cert-show $serialNumber --output <file-name>"
 	rlRun "pki cert-show $valid_serialNumber --output $temp_out10" 0
@@ -217,7 +206,7 @@ run_pki_cert_show()
 
 	# Run pki cert-show <No serial number> --output <filename>
 
-	rlPhaseStartSetup "pki_cert_show-0012: pki cert-show with <No serialNumber> --output <filename>"
+	rlPhaseStartSetup "pki_cert_show-0012: pki cert-show <No serialNumber> --output <filename> should fail"
 	local temp_out12=$TmpDir/cert-show12.out
 	local temp_out12_err=$TmpDir/cert-err12.out
 	rlLog "Running pki cert-show --output $temp_out12 1> $temp_out12_err" 
@@ -231,7 +220,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show <valid-serial-number> --pretty 
 
-        rlPhaseStartSetup "pki_cert_show-0013: pki cert-show with --pretty <filename>"
+        rlPhaseStartSetup "pki_cert_show-0013: pki cert-show <valid SerialNumber(decimal)> --pretty <filename> should show PrettyPrint output of cert and save the the Cert in File."
         local temp_out13=$TmpDir/cert-show13.out
         rlLog "Running pki cert-show $valid_serialNumber --pretty"
         rlRun "pki cert-show $valid_serialNumber --pretty > $temp_out13" 0
@@ -246,7 +235,7 @@ run_pki_cert_show()
 	
 	 # Run pki cert-show <in-valid-serial-number> --pretty 
 
-	rlPhaseStartSetup "pki_cert_show-0014: pki cert-show with $invalid_serialNumber --pretty <filename>"
+	rlPhaseStartSetup "pki_cert_show-0014: pki cert-show < $invalid_serialNumber > --pretty <filename> should fail to produce any PrettyPrint output"
 	local temp_out14=$TmpDir/cert-show14.out
 	rlLog "Running pki cert-show $invalid_serialNumber --pretty"
 	rlRun "pki cert-show $invalid_serialNumber --pretty 2> $temp_out13" 1
@@ -255,7 +244,7 @@ run_pki_cert_show()
 
 	# Run pki cert-show <No serial Number> --pretty
 
-	rlPhaseStartSetup "pki_cert_Show-0015: pki cert-show with <No serialNumber --pretty <filename>"
+	rlPhaseStartSetup "pki_cert_Show-0015: pki cert-show <No serialNumber --pretty <filename> should fail to produce any PrettyPrint output"
 	local temp_out15=$TmpDir/cert-show15.out
 	rlLog "Running pki cert-show --pretty" 1
 	rlRun "pki cert-show --pretty 1> $temp_out15" 1
@@ -269,4 +258,3 @@ run_pki_cert_show()
 	rlRun "popd"
     	rlPhaseEnd
 }
-
