9163e0029ee3bb5d85a0763db61e5bb7ab79788e
Niranjan Mallapadi <mrniranjan@redhat.com>
2014-02-17 19:10:42 +0530

    Rename IPA cli with pki cert cli

--- tests/dogtag/acceptance/cli-tests/pki-cert-cli/pki-cert-show.sh
+++ tests/dogtag/acceptance/cli-tests/pki-cert-cli/pki-cert-show.sh
@@ -5,7 +5,7 @@
 #   runtest.sh of /CoreOS/rhcs/acceptance/cli-tests/pki-cert-cli
 #   Description: PKI CERT CLI tests
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# The following ipa cli commands needs to be tested:
+# The following pki cert cli commands needs to be tested:
 #  pki-cert-request-submit
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
@@ -51,16 +51,6 @@ run_pki_cert_show()
         rlRun "pushd $TmpDir"
         rlPhaseEnd
 	
-	rlPhaseStartSetup "pki_cert_cli-configtest: pki cert-show --help configuration test"
-	rlRun "pki cert-show --help > $TmpDir/cert-show.out 2>&1" 0 "pki cert-show --help"
-	rlAssertGrep "usage:" "$TmpDir/cert-show.out"
-    	rlAssertGrep "--encoded         Base-64 encoded" "$TmpDir/cert-show.out"
-    	rlAssertGrep "--output <file>   Output file" "$TmpDir/cert-show.out"
-    	rlAssertGrep "--pretty          Pretty print" "$TmpDir/cert-show.out"
-	rlAssertNotGrep "Error: Unrecognized option: --help" "$TmpDir/cert-show.out"
-	rlLog "FAIL :: https://engineering.redhat.com/trac/pki-tests/ticket/490"
-	rlPhaseEnd
-
 	# Create a Temporary NSS DB Directory and generate Certificate
 
 	rlPhaseStartSetup "Generating temporary Cert to be used pki cert-show automation Tests"
@@ -75,10 +65,22 @@ run_pki_cert_show()
 	rlRun "valid_serialNumber=`pki cert-request-show $requestid | grep \"Certificate ID\" | sed 's/ //g' | cut -d: -f2`"
         rlPhaseEnd
 
+	# pki cert cli config test
+
+	rlPhaseStartTest "pki_cert_cli-configtest: pki cert-show --help configuration test"
+	rlRun "pki cert-show --help > $TmpDir/cert-show.out 2>&1" 0 "pki cert-show --help"
+	rlAssertGrep "usage:" "$TmpDir/cert-show.out"
+    	rlAssertGrep "--encoded         Base-64 encoded" "$TmpDir/cert-show.out"
+    	rlAssertGrep "--output <file>   Output file" "$TmpDir/cert-show.out"
+    	rlAssertGrep "--pretty          Pretty print" "$TmpDir/cert-show.out"
+	rlAssertNotGrep "Error: Unrecognized option: --help" "$TmpDir/cert-show.out"
+	rlLog "FAIL :: https://engineering.redhat.com/trac/pki-tests/ticket/490"
+	rlPhaseEnd
+
 
 	#Run pki cert-show with valid serial number in HexaDecimal
 
-	rlPhaseStartSetup "pki_cert_show-001: pki cert-show < valid serialNumber(HexaDecimal) >  should show Certificate Details"
+	rlPhaseStartTest "pki_cert_show-001: pki cert-show < valid serialNumber(HexaDecimal) >  should show Certificate Details"
 	rlLog "run get_cert_serialnumber function first to get the serial Number"
 	local temp_out1="$TmpDir/cert-show1.out"
 	rlLog "Running pki cert-show $valid_serialNumber"
@@ -93,7 +95,7 @@ run_pki_cert_show()
 
 	#Run pki cert-show with No serial Number 
 
-	rlPhaseStartSetup "pki_cert_show-002: pki cert-show should fail when no serial Number is given"
+	rlPhaseStartTest "pki_cert_show-002: pki cert-show should fail when no serial Number is given"
 	local temp_out2="$TmpDir/cert-show2.out"
 	rlRun "pki cert-show  > $temp_out2" 1 "pki cert-show without any serial number fails"
 	rlAssertGrep "usage: cert-show" "$temp_out2"
@@ -101,7 +103,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show with Invalid Serial Number in decimal
 
-	rlPhaseStartSetup "pki_cert_show-003: pki cert-show < invalid serialNumber(Decimal) > should Fail"
+	rlPhaseStartTest "pki_cert_show-003: pki cert-show < invalid serialNumber(Decimal) > should Fail"
 	local temp_out3="$TmpDir/cert-show3.out"
 	rlRun "pki cert-show $invalid_serialNumber 2> $temp_out3" 1 "command pki cert-show $serialNumber"
 	rlAssertGrep "CertNotFoundException" "$temp_out3"
@@ -109,7 +111,7 @@ run_pki_cert_show()
 
 	# Run pki cert-show with valid serial Number given in decimal 
 
-	rlPhaseStartSetup "pki_cert_show-004: pki cert-show < valid serialNumber(Decimal) > should show Certificate Details"
+	rlPhaseStartTest "pki_cert_show-004: pki cert-show < valid serialNumber(Decimal) > should show Certificate Details"
 	local temp_out4="$TmpDir/cert-show4.out"
 	local decimal_valid_serialNumber
 	decimal_valid_serialNumber=$(printf %d $valid_serialNumber)
@@ -125,7 +127,7 @@ run_pki_cert_show()
 
 	#Run pki cert-show with invalid serialNumber given in Hexadecimal
 	
-	rlPhaseStartSetup "pki_cert_show-005: pki cert-show < invalid serialNumber(hexadecimal) > should fail"
+	rlPhaseStartTest "pki_cert_show-005: pki cert-show < invalid serialNumber(hexadecimal) > should fail"
 	local temp_out5="$TmpDir/cert-show5.out"
 	local rand=`cat /dev/urandom | tr -dc '0-9' | fold -w 5 | head -n 1`
 	local invalid_hex_serialNumber=`expr $valid_serialNumber$rand`
@@ -135,7 +137,7 @@ run_pki_cert_show()
 
 	# Run pki cert-show with Junk Characters
 
-	rlPhaseStartSetup "pki_cert_show-006: pki cert-show < junk characters > should fail to show any certificate Details"
+	rlPhaseStartTest "pki_cert_show-006: pki cert-show < junk characters > should fail to show any certificate Details"
 	local temp_out6="$TmpDir/cert-show6.out"
 	local junk="axb124?$5@@_%^$#$@\!(_)043112321412321"
 	rlLog "Executing pki cert-show \"$junk\""
@@ -145,7 +147,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show <valid serialNumber> --encoded to produce a valid pem output
 
-	rlPhaseStartSetup "pki_cert_show-007: pki cert-show <valid SerialNumber>  --encoded should produce a valid pem output"
+	rlPhaseStartTest "pki_cert_show-007: pki cert-show <valid SerialNumber>  --encoded should produce a valid pem output"
 	local temp_out7="$TmpDir/cert-show7.out"
 	rlLog "Running pki cert-show $serialNumber"
         rlRun "pki cert-show $valid_serialNumber --encoded > $temp_out7" 0 "command pki cert-show $serialNumber --encoded"
@@ -161,7 +163,7 @@ run_pki_cert_show()
 	
 	#Run pki cert-show --encoded with No serial Number
 
-	rlPhaseStartSetup "pki_cert_show-008: pki cert-show <No SerialNumber> --encoded should fail"
+	rlPhaseStartTest "pki_cert_show-008: pki cert-show <No SerialNumber> --encoded should fail"
 	local temp_out8=$TmpDir/cert-show8.out
 	rlLog "Running pki cert-show <No-serial-Number> --encoded"
 	rlRun "pki cert-show --encoded 1> $temp_out8" 1
@@ -170,7 +172,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show --encoded with Invalid Serial Number
 
-	rlPhaseStartSetup "pki_cert_show-009: pki cert-show < In-Valid SerialNumber > --encoded should fail"
+	rlPhaseStartTest "pki_cert_show-009: pki cert-show < In-Valid SerialNumber > --encoded should fail"
 	local temp_out9=$TmpDir/cert-show9.out
 	rlLog "Running pki cer-show <invalid-serial-Number> --encoded"
 	rlRun "pki cert-show $invalid_serialNumber --encoded 2> $temp_out9" 1 "pki cert-show $serialNumber"
@@ -178,7 +180,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show <valid serialNumber> --output <filename>
 
-	rlPhaseStartSetup "pki_cert_show-0010: pki cert-show <valid SerialNumber(decimal)> --output <filename> should save the Certificate in File"
+	rlPhaseStartTest "pki_cert_show-0010: pki cert-show <valid SerialNumber(decimal)> --output <filename> should save the Certificate in File"
 	local temp_out10=$TmpDir/cert-show10.out
 	rlLog "Running pki cert-show $serialNumber --output <file-name>"
 	rlRun "pki cert-show $valid_serialNumber --output $temp_out10" 0
@@ -193,7 +195,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show <invalid-serial-number> --output 
 
-	rlPhaseStartSetup "pki_cert_show-0011: pki cert-show <invalid-serial-Number> --output <filename> should not create any file"
+	rlPhaseStartTest "pki_cert_show-0011: pki cert-show <invalid-serial-Number> --output <filename> should not create any file"
 	local temp_out11=$TmpDir/cert-show11.out
 	rlLog "Running pki cert-show <invalid-serialNumber> --output <filename>"
 	rlRun "pki cert-show $invalid_serialNumber --output $temp_out11" 1 "pki cert-show <invalid-serial-number> --output <file>"
@@ -206,7 +208,7 @@ run_pki_cert_show()
 
 	# Run pki cert-show <No serial number> --output <filename>
 
-	rlPhaseStartSetup "pki_cert_show-0012: pki cert-show <No serialNumber> --output <filename> should fail"
+	rlPhaseStartTest "pki_cert_show-0012: pki cert-show <No serialNumber> --output <filename> should fail"
 	local temp_out12=$TmpDir/cert-show12.out
 	local temp_out12_err=$TmpDir/cert-err12.out
 	rlLog "Running pki cert-show --output $temp_out12 1> $temp_out12_err" 
@@ -220,7 +222,7 @@ run_pki_cert_show()
 	
 	# Run pki cert-show <valid-serial-number> --pretty 
 
-        rlPhaseStartSetup "pki_cert_show-0013: pki cert-show <valid SerialNumber(decimal)> --pretty <filename> should show PrettyPrint output of cert and save the the Cert in File."
+        rlPhaseStartTest "pki_cert_show-0013: pki cert-show <valid SerialNumber(decimal)> --pretty <filename> should show PrettyPrint output of cert and save the the Cert in File."
         local temp_out13=$TmpDir/cert-show13.out
         rlLog "Running pki cert-show $valid_serialNumber --pretty"
         rlRun "pki cert-show $valid_serialNumber --pretty > $temp_out13" 0
@@ -235,7 +237,7 @@ run_pki_cert_show()
 	
 	 # Run pki cert-show <in-valid-serial-number> --pretty 
 
-	rlPhaseStartSetup "pki_cert_show-0014: pki cert-show < $invalid_serialNumber > --pretty <filename> should fail to produce any PrettyPrint output"
+	rlPhaseStartTest "pki_cert_show-0014: pki cert-show < $invalid_serialNumber > --pretty <filename> should fail to produce any PrettyPrint output"
 	local temp_out14=$TmpDir/cert-show14.out
 	rlLog "Running pki cert-show $invalid_serialNumber --pretty"
 	rlRun "pki cert-show $invalid_serialNumber --pretty 2> $temp_out13" 1
@@ -244,7 +246,7 @@ run_pki_cert_show()
 
 	# Run pki cert-show <No serial Number> --pretty
 
-	rlPhaseStartSetup "pki_cert_Show-0015: pki cert-show <No serialNumber --pretty <filename> should fail to produce any PrettyPrint output"
+	rlPhaseStartTest "pki_cert_Show-0015: pki cert-show <No serialNumber --pretty <filename> should fail to produce any PrettyPrint output"
 	local temp_out15=$TmpDir/cert-show15.out
 	rlLog "Running pki cert-show --pretty" 1
 	rlRun "pki cert-show --pretty 1> $temp_out15" 1
