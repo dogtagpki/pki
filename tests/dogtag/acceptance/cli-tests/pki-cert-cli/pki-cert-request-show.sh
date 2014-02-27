9e14d6f3e5e69093067f0a76e5b7090c102e11d3
Niranjan Mallapadi <mrniranjan@redhat.com>
2014-02-27 17:25:52 +0530

    Add more test cases to pki-cert-request-show
    Added test cases to check CRMF requests
    Added use cases  for validate,assign & unassign actions
    Modified to delete Temporary Directory

--- tests/dogtag/acceptance/cli-tests/pki-cert-cli/pki-cert-request-show.sh
+++ tests/dogtag/acceptance/cli-tests/pki-cert-cli/pki-cert-request-show.sh
@@ -52,11 +52,13 @@ run_pki_cert_request_show()
         local TEMP_NSS_DB="$TmpDir/nssdb"
         local ret_reqstatus
         local ret_requestid
-	local invalid_requestid=`cat /dev/urandom | tr -dc '0-9' | fold -w 10 | head -n 1`
-	local junk_requestid=`cat /dev/urandom | tr -dc 'a-bA-Z0-9' | fold -w 40 | head -n 1` 	
+	local invalid_requestid=$(cat /dev/urandom | tr -dc '0-9' | fold -w 10 | head -n 1)
+	local junk_requestid=$(cat /dev/urandom | tr -dc 'a-bA-Z0-9' | fold -w 40 | head -n 1)
 	local temp_cert_out="$TmpDir/cert-request.out"
-	local hex_invalid_requestid=`printf 0x%x $invalid_requestid`
-	
+	local hex_invalid_requestid=$(printf 0x%x $invalid_requestid)
+
+	local temp_out="$TmpDir/cert-request-show.out"	
+
 	# Config test of pki cert-request-show
 	rlPhaseStartTest "pki_cert_cli-configtest: pki cert-request-show --help configuration test"
 	rlRun "pki cert-request-show --help > $TmpDir/cert-show.out 2>&1" 0 "pki cert-request-show --help"
@@ -66,123 +68,169 @@ run_pki_cert_request_show()
 	rlPhaseEnd
 
 	# Create a Temporary NSS DB Directory and generate Certificate
-
 	rlPhaseStartSetup "Generating temporary Cert to be used pki cert-show automation Tests"
-	rlLog "Generating Certificate Request"
-        rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 \"Idm User1\" \"IdmUser1\" \"idmuser1@example.org\" \"Engineering\" \"Example.Inc\" "US" "--" "ret_reqstatus" "ret_requestid"" 0 
-	rlLog "To Approve the request we would need CA Admin Cert Nick Name stored in $MY_CERTDB_DIR"
-        rlLog "Approve Certificate requeset"
-        rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action approve 1> $TmpDir/pki-approve-out"
+        rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 \"Idm User1\" \"IdmUser1\" \
+		\"idmuser1@example.org\" \"Engineering\" \"Example.Inc\" "US" "--" "ret_reqstatus" "ret_requestid"" 0 "Generating Certificate Request"
+        rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \"$CA_agentV_user\" ca-cert-request-review $ret_requestid \
+		--action approve 1> $TmpDir/pki-approve-out" 0 "Approve Certificate requeset"
 	rlAssertGrep "Approved certificate request $ret_requestid" "$TmpDir/pki-approve-out"
-	rlRun "valid_serialNumber=`pki cert-request-show $ret_requestid | grep \"Certificate ID\" | sed 's/ //g' | cut -d: -f2`"
+	rlRun "valid_serialNumber=$(pki cert-request-show $ret_requestid | grep \"Certificate ID\" | sed 's/ //g' | cut -d: -f2)"
         rlPhaseEnd
 	
 
-	# pki cert-request-show <valid requestId(decimal) Anonymous>
+	# pki cert-request-show <valid requestId(decimal)
         rlPhaseStartTest "pki_cert_request_show-001: pki cert-request-show < valid requestid >  should show Certificate Request Details"
-        local temp_out1="$TmpDir/cert-request-show1.out"
-        rlLog "Running pki cert-request-show $ret_requestid"
-        rlRun "pki cert-request-show $ret_requestid > $temp_out1" 0 "command pki cert-request-show $requestid"
-	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out1"
-	rlAssertGrep "Type: enrollment" "$temp_out1"
-	rlAssertGrep "Request Status: complete" "$temp_out1"
-	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out1"
-	rlAssertGrep "Certificate ID: $valid_serialNumber" "$temp_out1"
+        rlRun "pki cert-request-show $ret_requestid > $temp_out" 0 "command pki cert-request-show $requestid"
+	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out"
+	rlAssertGrep "Type: enrollment" "$temp_out"
+	rlAssertGrep "Request Status: complete" "$temp_out"
+	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out"
+	rlAssertGrep "Certificate ID: $valid_serialNumber" "$temp_out"
 	rlPhaseEnd
 
-	# pki cert-request-show  <valid requestid(hexadecimal)> Anonymous
+	# pki cert-request-show  <valid requestid(hexadecimal)>
 	rlPhaseStartTest "pki_cert_request_show-002: pki cert-request-show <valid requestid(hexadecimal)> should Show Certificate Request details"
-	local temp_out2="$TmpDir/cert-request-show2.out"
-	local hex_valid_requestid=`printf 0x%x $ret_requestid`
-	rlLog "Running pki cert-request-show $hex_valid_reqid"
-	rlRun "pki cert-request-show $hex_valid_requestid > $temp_out2" 0
-	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out2"
-	rlAssertGrep "Type: enrollment" "$temp_out2" 
-	rlAssertGrep "Request Status: complete" "$temp_out2"
-	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out2"
-	rlAssertGrep "Certificate ID: $valid_serialNumber" "$temp_out2"
+	local hex_valid_requestid=$(printf 0x%x $ret_requestid)
+	rlRun "pki cert-request-show $hex_valid_requestid > $temp_out" 0 "Executing pki cert-request-show $hex_valid_reqid"
+	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out"
+	rlAssertGrep "Type: enrollment" "$temp_out" 
+	rlAssertGrep "Request Status: complete" "$temp_out"
+	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out"
+	rlAssertGrep "Certificate ID: $valid_serialNumber" "$temp_out"
 	rlPhaseEnd
 
-	# pki cert-request-show <invalid requestid(decimal)> Anonymous
+	# pki cert-request-show <invalid requestid(decimal)>
 	rlPhaseStartTest "pki_cert_request_show-003: pki cert-request-show <invalid requestid(decimal)> Should fail to display any Request details"
-	local temp_out3="$TmpDir/cert-request-show3.out"
-	rlLog "Executing pki cert-request-show $invalid_requestid"
-	rlRun "pki cert-request-show $invalid_requestid 2> $temp_out3" 1
-	rlAssertGrep "RequestNotFoundException: Request ID $hex_invalid_requestid not found" "$temp_out3"
+	rlRun "pki cert-request-show $invalid_requestid 2> $temp_out" 1 "Executing pki cert-request-show $invalid_requestid"
+	rlAssertGrep "RequestNotFoundException: Request ID $hex_invalid_requestid not found" "$temp_out"
 	rlPhaseEnd
 
-	#pki cert-request-show <invalid requestid(hexadecimal)> Anonymous
+	#pki cert-request-show <invalid requestid(hexadecimal)>
 	rlPhaseStartTest "pki_cert_request_show-004: pki cert-request-show <invalid requestid(hexadecimal)> Should fail to display any Request details"
-	local temp_out4="$TmpDir/cert-request-show4.out"
-	rlLog "Executing pki cert-request-show $hex_invalid_requestid"
-	rlRun "pki cert-request-show $hex_invalid_requestid 2> $temp_out4" 1
-	rlAssertGrep "RequestNotFoundException: Request ID $hex_invalid_requestid not found" "$temp_out4"
+	rlRun "pki cert-request-show $hex_invalid_requestid 2> $temp_out" 1 "Executing pki cert-request-show $hex_invalid_requestid"
+	rlAssertGrep "RequestNotFoundException: Request ID $hex_invalid_requestid not found" "$temp_out"
 	rlPhaseEnd
 	
-	#pki cert-request-show <junk chracters> Anonymous
+	#pki cert-request-show <junk chracters>
 	rlPhaseStartTest "pki_cert_request_show-005: pki cert-request-show <Junk Characters(decimal)> Should fail to display any Request details"
-	local temp_out5="$TmpDir/cert-request-show5.out"
 	rlLog "Executing pki cert-request-show \"$junk_requestid~!@#$%^&*()_+|\""
-	rlRun "pki cert-request-show \"$junk_requestid~\!@#$%^&*\(\)_+|\" 2> $temp_out5" 255
-	rlAssertGrep "Error: Invalid certificate request ID" "$temp_out5"
+	rlRun "pki cert-request-show \"$junk_requestid~\!@#$%^&*\(\)_+|\" 2> $temp_out" 255
+	rlAssertGrep "Error: Invalid certificate request ID" "$temp_out"
 	rlPhaseEnd
 	
 	#Pki cert-request-show Verify rejected Request Id is displayed correctly 
-	rlPhaseStartTest "pki_cert_request_show-006: Verify rejected RequesId's status is displayed as Rejected"
-	local temp_out6="$TmpDir/cert-request-show6.out"
-	rlLog "Request a New Certificate Request"
-	rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 \"Idm User2\" \"IdmUser2\" \"idmuser2@example.org\" \"MCP Division\" \"Example Org\" "US" "--" "ret_reqstatus" "ret_requestid""
-	rlLog "Reject Certificate requeset"
-	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action reject 1> $temp_cert_out"
+	rlPhaseStartTest "pki_cert_request_show-006: Verify rejected RequestId's status is displayed as Rejected"
+	rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 \
+		 \"Idm User2\" \"IdmUser2\" \"idmuser2@example.org\" \"MCP Division\" \"Example Org\" "US" "--" "ret_reqstatus" "ret_requestid"" \
+		 0 "Request a New Certificate Request"
+	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \"$CA_agentV_user\" \
+		ca-cert-request-review $ret_requestid --action reject 1> $temp_cert_out" 0 "As $CA_AgentV_user Reject Certificate request"
 	rlAssertGrep "Rejected certificate request $ret_requestid" "$temp_cert_out"
-	rlLog "Executing pki cert-request-show $ret_requestid" 
-	rlRun "pki cert-request-show $ret_requestid 1> $temp_out6" 0 
-	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out6"
-	rlAssertGrep "Request ID: $ret_requestid" "$temp_out6"
-	rlAssertGrep "Type: enrollment" "$temp_out6"
-	rlAssertGrep "Request Status: rejected" "$temp_out6"
-	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out6"
+	rlRun "pki cert-request-show $ret_requestid 1> $temp_out" 0 "Executing pki cert-request-show $ret_requestid"
+	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out"
+	rlAssertGrep "Request ID: $ret_requestid" "$temp_out"
+	rlAssertGrep "Type: enrollment" "$temp_out"
+	rlAssertGrep "Request Status: rejected" "$temp_out"
+	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out"
 	rlPhaseEnd
 
 	#Pki cert-request-show Verify canceled Request Id is displayed correctly
-	rlPhaseStartTest "pki_cert_request_show-007: Verify canceled RequesId's status is displayed as canceled"
-	local temp_out7="$TmpDir/cert-request-show7.out"
-	rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 \"Idm User3\" \"IdmUser3\" \"idmuser3@example.org\" \"MAP Division\" \"Example Org\" "US" "--" "ret_reqstatus" "ret_requestid""
-	rlLog "Cancel Certificate request"
-	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action cancel 1> $temp_cert_out"
+
+	rlPhaseStartTest "pki_cert_request_show-007: Verify canceled RequestId's status is displayed as canceled"
+	rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 	\
+		\"Idm User3\" \"IdmUser3\" \"idmuser3@example.org\" \"MAP Division\" \"Example Org\" "US" "--" "ret_reqstatus" "ret_requestid"" \
+		0 "Request for new certificate request"
+	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD \
+		-n \"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action cancel 1> $temp_cert_out" 0 "As $CA_agentV_user Cancel $ret_requestid"
 	rlAssertGrep "Canceled certificate request $ret_requestid" "$temp_cert_out"
-	rlLog "Executing pki cert-request-show $ret_requestid" 
-	rlRun "pki cert-request-show $ret_requestid 1> $temp_out7" 0 
-	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out7"
-	rlAssertGrep "Request ID: $ret_requestid" "$temp_out7"
-	rlAssertGrep "Type: enrollment" "$temp_out7"
-	rlAssertGrep "Request Status: canceled" "$temp_out7"
-	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out7"
+	rlRun "pki cert-request-show $ret_requestid 1> $temp_out" 0 "Executing pki cert-request-show $ret_requestid"
+	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out"
+	rlAssertGrep "Request ID: $ret_requestid" "$temp_out"
+	rlAssertGrep "Type: enrollment" "$temp_out"
+	rlAssertGrep "Request Status: canceled" "$temp_out"
+	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out"
 	rlPhaseEnd
 	
+	#Pki cert-request-show Verify Updated Request Id is displayed as pending
+	rlPhaseStartTest "pki_cert_request_show-008: Verify updated RequestID's status is displayed as pending"
+	rlRun "create_cert_request $TEMP_NSS_DB redhat crmf rsa 2048 \
+		 \"Idm User4\" \"IdmUser4\" \"idmuser4@example.org\" \"MAP Division\" \"Example Org\" "US" "--" "ret_reqstatus" "ret_requestid"" \
+		 0 "Request for new certificate request"	
+	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD \
+		-n \"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action update 1> $temp_cert_out" 0 "As $CA_agentV_user update $ret_requestid"
+	rlAssertGrep "Updated certificate request $ret_requestid" "$temp_cert_out"
+	rlRun "pki cert-request-show $ret_requestid 1> $temp_out" 0 "Executing pki cert-request-show $ret_requestid"
+	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out"
+	rlAssertGrep "Request ID: $ret_requestid" "$temp_out"
+	rlAssertGrep "Type: enrollment" "$temp_out"
+	rlAssertGrep "Request Status: pending" "$temp_out"
+	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out"
+	rlPhaseEnd
+	
+	#Pki cert-request-show Assign a Request Id & Verify request status is displayed as pending
+	rlPhaseStartTest "pki_cert_request_show-009: Assign a Pending Request & Verify Assigned RequestID's status is displayed as pending"
+	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD \
+		-n \"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action assign 1> $temp_cert_out" 0 "As $CA_agentV_user Assign $ret_requestid"
+	rlAssertGrep "Assigned certificate request $ret_requestid" "$temp_cert_out"
+	rlRun "pki cert-request-show $ret_requestid 1> $temp_out" 0 "Executing pki cert-request-show $ret_requestid"
+	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out"
+	rlAssertGrep "Request ID: $ret_requestid" "$temp_out"
+	rlAssertGrep "Type: enrollment" "$temp_out"
+	rlAssertGrep "Request Status: pending" "$temp_out"
+	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out"
+	rlPhaseEnd
+
+	#Pki cert-request-show Un-Assign a Pending Request & Verify request status is displayed as pending
+	rlPhaseStartTest "pki_cert_request_show-0010: Assign a Pending Request & Verify Assigned RequestID's status is displayed as pending"
+	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD \
+		-n \"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action unassign 1> $temp_cert_out" 0 "As $CA_agentV_user Un-Assign $ret_requestid"
+	rlAssertGrep "Unassigned certificate request $ret_requestid" "$temp_cert_out"
+	rlRun "pki cert-request-show $ret_requestid 1> $temp_out" 0 "Executing pki cert-request-show $ret_requestid"
+	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out"
+	rlAssertGrep "Request ID: $ret_requestid" "$temp_out"
+	rlAssertGrep "Type: enrollment" "$temp_out"
+	rlAssertGrep "Request Status: pending" "$temp_out" 
+	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out"
+	rlPhaseEnd
+	
+	#pki cert-request-show Validate a pending request & verify request status is displayed as pending
+	rlPhaseStartTest "pki_cert_request_show-0011: Validate a Pending Request & Verify Modified RequestID's status is displayed as pending"
+	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \
+	\"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action validate 1> $temp_cert_out" 0 "As $CA_agentV_user Validate $ret_requestid"
+	rlAssertGrep "Validated certificate request $ret_requestid" "$temp_cert_out"
+	rlLog "Executing pki cert-request-show $ret_requestid"
+	rlRun "pki cert-request-show $ret_requestid 1> $temp_out" 0
+	rlAssertGrep "Certificate request \"$ret_requestid\"" "$temp_out"
+	rlAssertGrep "Request ID: $ret_requestid" "$temp_out"
+	rlAssertGrep "Type: enrollment" "$temp_out"
+	rlAssertGrep "Request Status: pending" "$temp_out"
+	rlAssertGrep "Operation Result: $ret_reqstatus" "$temp_out"
+	rlPhaseEnd
+
 	#pki cert-request-show Verify SerialNumber Displayed matches with SerialNumber assigned to Approved request
-	rlPhaseStartTest "pki_cert_request_show-008: verify serialNumber displayed matches with serialNumber assigned to Approved request"
-	rlLog "Request a New Certificate Request"
-	rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 \"Idm User4\" \"IdmUser4\" \"idmuser4@example.org\" \"MNP Division\" \"Example Org\" "US" "--" "ret_reqstatus" "ret_requestid""
-	rlLog "Approve the Certificate Request"
-	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action approve 1> $TmpDir/pki-approve-out" 0 
+	rlPhaseStartTest "pki_cert_request_show-0012: Verify serialNumber displayed matches with serialNumber assigned to Approved request"
+	rlRun "create_cert_request $TEMP_NSS_DB redhat pkcs10 rsa 2048 \
+		\"Idm User8\" \"IdmUser8\" \"idmuser8@example.org\" \"MNP Division\" \"Example Org\" "US" "--" "ret_reqstatus" "ret_requestid"" \
+		0 "Request for new certificate request" 
+	rlRun "pki -d $CERTDB_DIR -c $CERTDB_DIR_PASSWORD -n \
+		\"$CA_agentV_user\" ca-cert-request-review $ret_requestid --action approve 1> $TmpDir/pki-approve-out" 0 "As $CA_agentV_user Approve the request"
 	rlAssertGrep "Approved certificate request $ret_requestid" "$TmpDir/pki-approve-out"
-	rlLog "Get the Certificate Serial Number displayed by pki cert-request-show $ret_requestid"
-	rlRun "valid_serialNumber=`pki cert-request-show $ret_requestid | grep \"Certificate ID\" | sed 's/ //g' | cut -d: -f2`"
+	local valid_serialNumber=$(pki cert-request-show $ret_requestid | grep \"Certificate ID\" | sed 's/ //g' | cut -d: -f2) 
 	rlLog "Serial Number Displayed by pki cert-request-show $ret_requestid is $valid_serialNumber"
 	rlLog "Run ldapsearch against CA Database to get the serial Number assigned to the approve Request $ret_requestid"
-	rlRun "sno=`ldapsearch -x -LLL -b \"ou=certificateRepository,ou=ca,O=pki-tomcat-CA\" -D \"$LDAP_ROOTDN\" -w $LDAP_ROOTDNPWD -h $(hostname) -p $CA_LDAP_PORT \"(metainfo=requestID:$ret_requestid)\" cn | grep -v dn | awk -F ": " '{print $2}'`"
-	
-		if [ "$sno" == "`printf %d $valid_serialNumber`" ]; then
-			rlLog "SerialNumber Matches with serialNumber assigned to Approved Request"
-		else
-			rlLog "FAIL :: SerialNumber displayed doesn't match with serialNumber assigned to Approved Request"
-		fi
+	local sno=$(ldapsearch -x -LLL -b \
+		\"ou=certificateRepository,ou=ca,O=pki-tomcat-CA\" \
+		-D \"$LDAP_ROOTDN\" -w $LDAP_ROOTDNPWD \
+		-h $(hostname) -p $CA_LDAP_PORT \"(metainfo=requestID:$ret_requestid)\" cn | grep -v dn | awk -F ": " '{print $2}')
+	if [ "$sno" == "$(printf %d $valid_serialNumber)" ]; then
+		rlLog "SerialNumber Matches with serialNumber assigned to Approved Request"
+	else
+		rlLog "FAIL :: SerialNumber displayed doesn't match with serialNumber assigned to Approved Request"
+	fi
 	rlPhaseEnd
-		
 	
 	rlPhaseStartCleanup "pki cert-request-show cleanup: Delete temp dir"
 	rlRun "popd"
+	rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
     	rlPhaseEnd
-	
 }
