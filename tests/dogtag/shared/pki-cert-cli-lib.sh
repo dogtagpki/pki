baa4933c1e26504fff7e24eebab23053ec7584b7
Niranjan Mallapadi <mrniranjan@redhat.com>
2014-04-06 23:21:21 +0530

    add new functions to create & submit request
    Adding create_new_cert_request to create pkcs10 or crmf request
    adding submit_new_request function to submit the request to
    required CA

--- tests/dogtag/shared/pki-cert-cli-lib.sh
+++ tests/dogtag/shared/pki-cert-cli-lib.sh
@@ -49,13 +49,9 @@ generate_PKCS10()
 }
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 ##############################
-#
 #Function Usage create_cert <Temporary NSS DB Directory> <NSS DB Directory Password> <pkcs10/crmf> <rsa/ec> <cn> <uid> <email> 
 #<organizationalUnit> <organization> <country> <profilename> <return_status> <return_id> <return_dn>
 #return_status, return_id & return_dn are return variables emitting out Status of the request, Request id & Request DN Submitted
-#
-#TODO: Currently we have implemented only for caUserCert profile, Need to extend for other profiles
-#CRMF Request needs to be generated
 #############################
 create_cert_request()
 {
@@ -197,7 +193,7 @@ local rand=$(cat /dev/urandom | tr -dc '0-9' | fold -w 5 | head -n 1)
 	[ "$profilename" != "caDirUserCert" ] || \
 	[ "$profilename" != "caECDirUserCert" ] || \
 	[ "$profilename" != "caAgentServerCert" ] && \
-	[ "$profilename" != "caUserCert" ]; then
+        [ "$profilename" != "caUserCert" ]; then
 		rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='cert_request_type']/Value\" -v \"$request_type\" $xml_profile_file"
 		rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='cert_request']/Value\" -v \"$(cat -v $dir/$cert_request_file)\" $xml_profile_file"
 		rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='sn_cn']/Value\" -v \"$cn\" $xml_profile_file"
@@ -223,5 +219,216 @@ local rand=$(cat /dev/urandom | tr -dc '0-9' | fold -w 5 | head -n 1)
 	rlLog "Request id : $request_id"
 	eval "$cert_subject"="'$subject'"
 	return 0 
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+create_new_cert_request()
+{
+	local dir=$(echo $1|cut -d: -f2)
+	local password=$(echo $2|cut -d: -f2)
+	local request_type=$(echo $3|cut -d: -f2)
+	local algo=$(echo $4|cut -d: -f2)
+	local key_size=$(echo $5|cut -d: -f2)
+	local cn="$(echo $6|cut -d: -f2)"
+	local uid="$(echo $7|cut -d: -f2)"
+	local email="$(echo $8|cut -d: -f2)"
+	local ou="$(echo $9|cut -d: -f2)"
+	local organization="$(echo ${10}|cut -d: -f2)"
+	local country="$(echo ${11}|cut -d: -f2)"
+	local archive="$(echo ${12}|cut -d: -f2)"
+	local cert_request_file="$(echo ${13}|cut -d: -f2)"
+	local cert_subject_file="$(echo ${14}|cut -d: -f2)"
+	local rand=$(cat /dev/urandom | tr -dc '0-9' | fold -w 5 | head -n 1)
+
+#### First we create  NSS Database
+
+	if [ -d "$dir" ]; then
+	
+		rlLog "$dir Directory exists"
+	else
+		rlLog "Creating Security Database"
+		rlRun "pki -d $dir -c $password client-init" 0 "Initializing Security Database"
+		RETVAL=$?
+		if  [ $RETVAL != 0 ]; then
+		  rlLog "FAIL :: NSS Database was not created"
+		  return 1
+		fi
+	fi
+	if [ "$cn" == "" ]; then
+		cn="pkiuser$rand"
+	fi
+	if [ "$uid" == "" ] && [ "$cn" == pkiuser$rand ]; then
+	        uid=$cn
+	fi
+	if [ "$email" == "" ] && [ "$cn" == pkiuser$rand ];then 
+	        email="$cn@example.org"
+	fi
+	if [ "$ou" == "" ]; then
+	        ou="Engineering"
+	fi
+	if [ "$organization" == "" ]; then
+	        organization="Example.Inc"
+	fi
+	if [ "$country" == "" ]; then
+	        country="US"
+	fi
+#### Generate request
+	if [ "$uid" != "" ] && [ "$email" != "" ]; then
+		#local subject="CN=$cn,UID=$uid,E=$email,OU=$ou,O=$organization,C=$country"
+		local subject="UID=$uid,E=$email,CN=$cn,OU=$ou,O=$organization,C=$country"
+	else
+		local subject="CN=$cn,OU=$ou,O=$organization,C=$country"
+	fi
+	
+	if [ "$request_type" == "pkcs10" ];then
+
+		rlRun "PKCS10Client -p $password -d $dir -a $algo -l $key_size -o $cert_request_file -n \"$subject\" 1> $dir/pkcs10.out" 0
+		RETVAL=$?
+		if [ $RETVAL != 0 ]; then
+			rLog "Create of PKCS10 Request failed for $subject"
+			return 1
+		fi
+	fi
+	if [ "$request_type" == "crmf" ] && [ "$archive" != "true" ];then
+		rlRun "set_newjavapath \":./:/usr/lib/java/jss4.jar:/usr/share/java/pki/pki-nsutil.jar:/usr/share/java/pki/pki-cmsutil.jar:/usr/share/java/apache-commons-codec.jar:/usr/share/java/pki/pki-silent.jar:/opt/rhqa_pki/java/generateCRMFRequest.jar:\"" 0 "Setting Java CLASSPATH"
+		rlRun "source /opt/rhqa_pki/env.sh" 0 "Set Environment Variables"
+		rlLog "Execute generateCRMFRequest to generate CRMF Request"
+		rlRun "java -cp $CLASSPATH generateCRMFRequest -client_certdb_dir $dir -client_certdb_pwd $password -debug false -request_subject \"$subject\" -request_keytype $algo -request_keysize $key_size -output_file $cert_request_file 1> $dir/crmf.out" 0 "Execute generateCRMFRequest to generate CRMF Request"
+	fi
+	
+	if [ "$request_type" == "crmf" ] && [ "$archive" == "true" ];then
+		rlLog "Get Transport Cert"
+		rlRun "cat $CA_SERVER_ROOT/conf/CS.cfg | grep ca.connector.KRA.transportCert | awk -F \"=\" '{print \$2}' > transport.txt"
+		rlRun "CRMFPopClient  -d $dir -p $password -o $cert_request_file -n "$subject" -a $algo -l $key_size -u $uid -r $uid 1> $dir/CRMFPopClient.out" 0 "Executing CRMFPopClient"
+		RETVAL=$?
+                if [ $RETVAL != 0 ]; then
+                        rlFail "CRMFPopClient Failed"
+                        return 1
+                fi
+	fi
+#### Strip  headers from request, Note for CRMF requests Our class doesn't generate the headers
+	if [ "$request_type" == "pkcs10" ] || [ "$archive" == "false" ]; then
+
+		rlLog "Stripping headers from the $cert_request_file"
+		rlRun "sed -e '/-----BEGIN NEW CERTIFICATE REQUEST-----/d' -i $cert_request_file"
+		rlRun "sed -e '/-----END NEW CERTIFICATE REQUEST-----/d' -i $cert_request_file"
+	fi
+	
+### use dos2unix to convert the request to unix format 
+
+	rlLog "Converting $cert_request_file to unix format to strip CRLF lines"
+	rlRun "dos2unix $cert_request_file"
+	RETVAL=$?
+	if [ $RETVAL != 0 ]; then
+		rlFail "FAIL :: Convert to UNIX format failed"
+		return 1
+	fi
+	echo -e "RequestType:$request_type" > $cert_subject_file
+	echo -e "CN:$cn" >> $cert_subject_file
+	echo -e "UID:$uid" >> $cert_subject_file
+	echo -e "Email:$email" >> $cert_subject_file
+	echo -e "OU:$ou" >> $cert_subject_file
+	echo -e "Org:$organization" >> $cert_subject_file
+	echo -e "Country:$country" >> $cert_subject_file
+	echo -e "Request_DN:$subject" >> $cert_subject_file
+	rlLog "Certificate Request file is saved in $cert_request_file"
+	rlLog "Subject DN information for Certificate Requeset is saved in $cert_subject_file"
+	return 0
+}	
+
+submit_new_request(){
+	local dir=$(echo $1|cut -d: -f2)
+	local dir_pwd=$(echo $2|cut -d: -f2)
+	local target_host=$(echo $3|cut -d: -f2)
+	local nickname=$(echo $4|cut -d: -f2)
+	local protocol=$(echo $5|cut -d: -f2)
+	local port=$(echo $6|cut -d: -f2)
+	local url=$(echo $7|cut -d: -f2)
+	local username=$(echo $8|cut -d: -f2)
+	local userpwd=$(echo $9|cut -d: -f2)
+	local profilename=$(echo ${10}|cut -d: -f2)
+	local cert_request_file=$(echo ${11}|cut -d: -f2)
+	local subj_request_file=$(echo ${12}|cut -d: -f2)
+	local output_file=$(echo ${13}|cut -d: -f2)
+	local request_type=$(cat $subj_request_file | grep "RequestType" | cut -d: -f2)
+	local uid=$(cat $subj_request_file | grep ^"UID" | cut -d: -f2)
+	local cn=$(cat $subj_request_file | grep ^"CN" | cut -d: -f2)
+	local email=$(cat $subj_request_file | grep ^"Email" | cut -d: -f2)
+	local ou=$(cat $subj_request_file | grep ^"OU" | cut -d: -f2)
+	local organization=$(cat $subj_request_file | grep ^"Org" | cut -d: -f2)
+	rlLog "organization=$organization"
+	local country=$(cat $subj_request_file | grep ^"Country" | cut -d: -f2)
+	local cert_request_dn=$(cat $subj_request_file | grep ^"Request_DN" | cut -d: -f2)
+	local rand=$(cat /dev/urandom | tr -dc '0-9' | fold -w 5 | head -n 1)
+
+
+        if [ "$target_host" == "" ]; then
+                target_host="$(hostname)"
+        fi
+        if [ "$protocol" == "" ]; then
+                protocol="http"
+        fi
+        if [ "$port" == "" ]; then
+                port=8080
+        fi
+        if [ "$profilename" == "" ]; then
+		profilename="caUserCert"
+        fi
+
+	local xml_profile_file=$dir/$profilename-$rand.xml
+
+	rlLog "Getting the $profilename XML file to submit the request"
+	rlRun "pki -d $dir -h $target_host -p $port -c $dir_pwd cert-request-profile-show $profilename --output $xml_profile_file 1> $xml_profile_file-out" 
+	if [ $? != 0 ]; then
+		rlFail "FAIL :: We have some problem getting $profile xml"
+		return 1
+	fi 
+	if [ "$profilename" == "caUserCert" ]  || [ "$profilename" ==  "caUserSMIMEcapCert" ] || [ "$profilename" ==  "caDualCert" ];then
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='cert_request_type']/Value\" -v \"$request_type\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='cert_request']/Value\" -v \"$(cat -v $cert_request_file)\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='sn_uid']/Value\" -v \"$uid\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='sn_e']/Value\" -v \"$email\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='sn_cn']/Value\" -v \"$cn\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='sn_ou']/Value\" -v \"$ou\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='sn_o']/Value\" -v \"$organization\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='sn_c']/Value\" -v \"$country\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='requestor_name']/Value\" -v \"$cn\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='requestor_email']/Value\" -v \"$email\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='requestor_phone']/Value\" -v 123-456-7890 $xml_profile_file"
+	fi
+
+	 if [ "$profilename" != "CaDualCert" ] || \
+        [ "$profilename" != "caDirPinUserCert" ] || \
+        [ "$profilename" != "caDirUserCert" ] || \
+        [ "$profilename" != "caECDirUserCert" ] || \
+        [ "$profilename" != "caAgentServerCert" ] && \
+	[ "$profilename" != "caUserCert" ]; then
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='cert_request_type']/Value\" -v \"$request_type\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='cert_request']/Value\" -v \"$(cat -v $cert_request_file)\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='requestor_name']/Value\" -v \"$cn\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='requestor_email']/Value\" -v \"$email\" $xml_profile_file"
+	rlRun "xmlstarlet ed -L -u \"CertEnrollmentRequest/Input/Attribute[@name='requestor_phone']/Value\" -v 123-456-7890 $xml_profile_file"
+	fi
+	
+#### submit the request to CA 
+
+	rlLog "Submiting  Request $target_host at $port"
+	rlRun "pki -d $dir -c $dir_pwd -h $target_host -P $protocol -p $port cert-request-submit $xml_profile_file 1> $output_file"
+	RETVAL=$?
+        if [ $RETVAL != 0 ]; then
+                rlFail "Could not submit request"
+		return 1
+		
+        fi
+
+        REQUEST_SUBMIT_STATUS=$(cat $output_file | grep "Operation Result" | awk -F ": " '{print $2}')
+        REQUEST_ID=$(cat $output_file  | grep "Request ID" | awk -F ": " '{print $2}')
+
+	echo -e "----------------------------------------" >> $output_file
+	echo -e "REQUEST_ID_RETURNED:$REQUEST_ID" >> $output_file
+	echo -e "REQUEST_SUBMIT_STATUS:$REQUEST_SUBMIT_STATUS" >> $output_file
+	echo -e "REQUEST_DN:$cert_request_dn" >> $output_file
+	echo -e "------------------------------------------" >> $output_file
+	
+	return 0;
 }
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
