e6928a0a20733c6756d16d308edc25c1e8cb0bcf
Matthew Harmsen <mharmsen@redhat.com>
2015-10-22 14:38:07 -0600

    Checking in under the one line trivial change rule.

--- base/kra/src/com/netscape/kra/RecoveryService.java
+++ base/kra/src/com/netscape/kra/RecoveryService.java
@@ -405,7 +405,7 @@ public class RecoveryService implements IService {
                 pubkey = X509Key.parsePublicKey(new DerValue(publicKeyData));
             } catch (Exception e) {
                 CMS.debug("RecoverService: after parsePublicKey:" + e.toString());
-                throw new EKRAException(CMS.getUserMessage("CMS_KRA_RECOVERY_FAILED_1", "pubic key parsing failure"));
+                throw new EKRAException(CMS.getUserMessage("CMS_KRA_RECOVERY_FAILED_1", "public key parsing failure"));
             }
             byte iv[] = { 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1 };
             PrivateKey privKey =
