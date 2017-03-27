8e7653987bf592ae6a5968fc0c5ef6696f13d348
Endi S. Dewata <edewata@redhat.com>
2017-03-28 00:15:28 +0200

    Added audit service and CLI to all subsystems.
    Previously the audit service and CLI were only available on TPS.
    Now they have been added to all subsystems.
    
    Change-Id: I3b472254641eb887289c5122df390c46ccd97d47

--- base/java-tools/src/com/netscape/cmstools/cli/TKSCLI.java
+++ base/java-tools/src/com/netscape/cmstools/cli/TKSCLI.java
@@ -22,6 +22,7 @@ import com.netscape.certsrv.client.PKIClient;
 import com.netscape.certsrv.client.SubsystemClient;
 import com.netscape.certsrv.tks.TKSClient;
 import com.netscape.cmstools.group.GroupCLI;
+import com.netscape.cmstools.logging.AuditCLI;
 import com.netscape.cmstools.selftests.SelfTestCLI;
 import com.netscape.cmstools.system.TPSConnectorCLI;
 import com.netscape.cmstools.user.UserCLI;
@@ -36,6 +37,7 @@ public class TKSCLI extends SubsystemCLI {
     public TKSCLI(CLI parent) {
         super("tks", "TKS management commands", parent);
 
+        addModule(new AuditCLI(this));
         addModule(new GroupCLI(this));
         addModule(new SelfTestCLI(this));
         addModule(new TPSConnectorCLI(this));
