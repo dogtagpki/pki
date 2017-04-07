32e257fccd0707cb4ebd5b2a93a9fd9172870d1d
Ade Lee <alee@redhat.com>
2017-04-07 16:52:31 -0400

    Add CAInfo resource
    This resource (which will be accessed at /ca/rest/info)
    will initially return the mechanism for archival.
    
    This is needed by clients to know how to package secrets when
    archiving.  We may add the transport cert later.
    
    Change-Id: Ib13d52344e38dc9b54c0d2a1645f1211dd84069b

---
+++ base/common/src/org/dogtagpki/common/CAInfo.java
@@ -0,0 +1,119 @@
+// --- BEGIN COPYRIGHT BLOCK ---
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// (C) 2017 Red Hat, Inc.
+// All rights reserved.
+// --- END COPYRIGHT BLOCK ---
+
+package org.dogtagpki.common;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netscape.certsrv.base.ResourceMessage;
+
+/**
+ * @author Ade Lee
+ */
+@XmlRootElement(name="CAInfo")
+public class CAInfo extends ResourceMessage {
+
+    private static Logger logger = LoggerFactory.getLogger(Info.class);
+
+    public static Marshaller marshaller;
+    public static Unmarshaller unmarshaller;
+
+    static {
+        try {
+            marshaller = JAXBContext.newInstance(CAInfo.class).createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            unmarshaller = JAXBContext.newInstance(CAInfo.class).createUnmarshaller();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    String archivalMechanism;
+
+    @XmlElement(name="ArchivalMechanism")
+    public String getArchivalMechanism() {
+        return archivalMechanism;
+    }
+
+    public void setArchivalMechanism(String archivalMechanism) {
+        this.archivalMechanism = archivalMechanism;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((archivalMechanism == null) ? 0 : archivalMechanism.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CAInfo other = (CAInfo) obj;
+        if (archivalMechanism == null) {
+            if (other.archivalMechanism != null)
+                return false;
+        } else if (!archivalMechanism.equals(other.archivalMechanism))
+            return false;
+        return true;
+    }
+
+    public String toString() {
+        try {
+            StringWriter sw = new StringWriter();
+            marshaller.marshal(this, sw);
+            return sw.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static CAInfo valueOf(String string) throws Exception {
+        return (CAInfo)unmarshaller.unmarshal(new StringReader(string));
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        CAInfo before = new CAInfo();
+        before.setArchivalMechanism("encrypt");
+
+        String string = before.toString();
+        System.out.println(string);
+
+        CAInfo after = CAInfo.valueOf(string);
+        System.out.println(before.equals(after));
+    }
+}
+
