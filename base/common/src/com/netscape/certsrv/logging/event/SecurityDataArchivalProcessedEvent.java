ce6d3aa6e1cf9e936bbeaec516f1a6e0cea8d9fc
Ade Lee <alee@redhat.com>
2017-05-16 22:16:30 -0400

    Encapsulate archival processed audit logs
    Encapsulate audit logs for SECURITY_DATA_ARCHIVAL_REQUEST_PROCESSED
    and PRIVATE_KEY_ARCHIVAL_REQUEST_PROCESSED.  We have merged the
    two audit events.
    
    Change-Id: I2abc7edff076495bb62733b92304fecd4f15b2b7

---
+++ base/common/src/com/netscape/certsrv/logging/event/SecurityDataArchivalProcessedEvent.java
@@ -0,0 +1,49 @@
+// --- BEGIN COPYRIGHT BLOCK ---
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// (C) 2017 Red Hat, Inc.
+// All rights reserved.
+// --- END COPYRIGHT BLOCK ---
+package com.netscape.certsrv.logging.event;
+
+import com.netscape.certsrv.logging.AuditEvent;
+
+public class SecurityDataArchivalProcessedEvent extends AuditEvent {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final String LOGGING_PROPERTY = "LOGGING_SIGNED_AUDIT_SECURITY_DATA_ARCHIVAL_REQUEST_PROCESSED";
+
+    public SecurityDataArchivalProcessedEvent(
+            String subjectID,
+            String outcome,
+            String requestID,
+            String clientKeyID,
+            String keyID,
+            String failureReason,
+            String pubkey) {
+
+        super(LOGGING_PROPERTY);
+
+        setParameters(new Object[] {
+                subjectID,
+                outcome,
+                requestID,
+                clientKeyID,
+                keyID,
+                failureReason,
+                pubkey
+        });
+    }
+}
