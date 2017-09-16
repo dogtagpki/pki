e5a0b45e7a409804f71c5949c73d6bf7d6e45967
Endi S. Dewata <edewata@redhat.com>
2017-09-16 05:24:00 +0200

    Refactored KRA connector configuration (part 1).
    The code for configuring KRA connector in CA has been moved into
    KRAInstallerService.
    
    https://pagure.io/dogtagpki/issue/2280
    
    Change-Id: I37a697576d8d5f5bb995cf77379eecb44a40f7b5

--- base/kra/src/org/dogtagpki/server/kra/rest/KRAInstallerService.java
+++ base/kra/src/org/dogtagpki/server/kra/rest/KRAInstallerService.java
@@ -17,13 +17,21 @@
 // --- END COPYRIGHT BLOCK ---
 package org.dogtagpki.server.kra.rest;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+
 import org.dogtagpki.server.rest.SystemConfigService;
 
 import com.netscape.certsrv.apps.CMS;
 import com.netscape.certsrv.base.EBaseException;
+import com.netscape.certsrv.base.IConfigStore;
 import com.netscape.certsrv.base.PKIException;
 import com.netscape.certsrv.system.ConfigurationRequest;
 import com.netscape.cms.servlet.csadmin.ConfigurationUtils;
+import com.netscape.cmsutil.xml.XMLObject;
 
 /**
  * @author alee
@@ -44,7 +52,7 @@ public class KRAInstallerService extends SystemConfigService {
 
             // need to push connector information to the CA
             if (!request.getStandAlone() && !ca_host.equals("")) {
-                ConfigurationUtils.updateConnectorInfo(CMS.getAgentHost(), CMS.getAgentPort());
+                configureKRAConnector(CMS.getAgentHost(), CMS.getAgentPort());
                 ConfigurationUtils.setupClientAuthUser();
             }
 
@@ -61,6 +69,65 @@ public class KRAInstallerService extends SystemConfigService {
         } catch (Exception e) {
             CMS.debug(e);
             throw new PKIException("Errors in updating next serial number ranges in DB: " + e);
+        }
+    }
+
+    public void configureKRAConnector(String kraHost, String kraPort)
+            throws Exception {
+
+        IConfigStore cs = CMS.getConfigStore();
+
+        String caHost = null;
+        int caPort = -1;
+        String transportCert = "";
+
+        String url = cs.getString("preop.ca.url", "");
+        if (!url.equals("")) {
+            caHost = cs.getString("preop.ca.hostname", "");
+            caPort = cs.getInteger("preop.ca.httpsadminport", -1);
+            transportCert = cs.getString("kra.transport.cert", "");
+        }
+
+        if (caHost == null) {
+            CMS.debug("KRAInstallerService: preop.ca.url is not defined. External CA selected. No transport certificate setup is required");
+            return;
+        }
+
+        CMS.debug("KRAInstallerService: "
+                + "Configuring KRA connector in CA at https://" + caHost + ":" + caPort);
+
+        String sessionId = CMS.getConfigSDSessionId();
+
+        MultivaluedMap<String, String> content = new MultivaluedHashMap<String, String>();
+        content.putSingle("ca.connector.KRA.enable", "true");
+        content.putSingle("ca.connector.KRA.local", "false");
+        content.putSingle("ca.connector.KRA.timeout", "30");
+        content.putSingle("ca.connector.KRA.uri", "/kra/agent/kra/connector");
+        content.putSingle("ca.connector.KRA.host", kraHost);
+        content.putSingle("ca.connector.KRA.port", kraPort);
+        content.putSingle("ca.connector.KRA.transportCert", transportCert);
+        content.putSingle("sessionID", sessionId);
+
+        configureKRAConnector(caHost, caPort, true, content);
+    }
+
+    public void configureKRAConnector(String caHost, int caPort, boolean https,
+            MultivaluedMap<String, String> content) throws Exception {
+
+        String c = ConfigurationUtils.post(caHost, caPort, https, "/ca/admin/ca/updateConnector", content, null, null);
+        if (c == null) {
+            return;
+        }
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(c.getBytes());
+        XMLObject parser = new XMLObject(bis);
+
+        String status = parser.getValue("Status");
+        CMS.debug("KRAInstallerService: status: " + status);
+
+        if (!status.equals(ConfigurationUtils.SUCCESS)) {
+            String error = parser.getValue("Error");
+            throw new IOException(error);
         }
     }
 }
