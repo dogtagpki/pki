908cba740156eccf46ebda3e22a10d1aed5d90a8
Endi S. Dewata <edewata@redhat.com>
2017-09-27 09:45:17 +0200

    Refactored LogFile class (part 2).
    The LogFile.log() has been modified such that it handles the
    signed audit events separately from system and transaction events.
    
    https://pagure.io/dogtagpki/issue/2689
    
    Change-Id: I00532139d7ea7779515a58a44ef5f51f867f1416

--- base/server/cms/src/com/netscape/cms/logging/LogFile.java
+++ base/server/cms/src/com/netscape/cms/logging/LogFile.java
@@ -1058,29 +1058,46 @@ public class LogFile implements ILogEventListener, IExtendedPluginInfo {
      * @param ev The event to be logged.
      */
     public void log(ILogEvent ev) throws ELogException {
-        if (ev instanceof AuditEvent) {
-            if (!mType.equals("transaction") || (!mOn) || mLevel > ev.getLevel()) {
-                return;
-            }
-        } else if (ev instanceof SystemEvent) {
-            if (!mType.equals("system") || (!mOn) || mLevel > ev.getLevel()) {
-                return;
-            }
-        } else if (ev instanceof SignedAuditEvent) {
-            if (!mType.equals("signedAudit") || (!mOn) || mLevel > ev.getLevel()) {
-                return;
-            }
+
+        if (!mOn || mLevel > ev.getLevel()) {
+            return;
         }
 
-        // If no selection specified in configuration, then all are selected
-        if (selectedEvents.isEmpty()) {
+        if (mType.equals("transaction")) {
+            if (ev instanceof AuditEvent) {
+                doLog(ev);
+            }
+            return;
+        }
+
+        if (mType.equals("system")) {
+            if (ev instanceof SystemEvent) {
+                doLog(ev);
+            }
+            return;
+        }
+
+        if (mType.equals("signedAudit")) {
+            if (ev instanceof SignedAuditEvent) {
+                logSignedAuditEvent((SignedAuditEvent)ev);
+            }
+            return;
+        }
+
+        CMS.debug("LogFile: logger type not supported: " + mType);
+    }
+
+    public void logSignedAuditEvent(SignedAuditEvent ev) throws ELogException {
+
+        // If no type specified in property file, then treated as selected
+        String type = ev.getEventType();
+        if (type == null) {
             doLog(ev);
             return;
         }
 
-        // If no type specified in propertity file, then treated as selected
-        String type = ev.getEventType();
-        if (type == null) {
+        // If no selection specified in configuration, then all are selected
+        if (selectedEvents.isEmpty()) {
             doLog(ev);
             return;
         }
