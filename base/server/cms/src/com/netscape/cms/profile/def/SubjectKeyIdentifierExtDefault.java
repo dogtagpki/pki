5b08d9d273f896d23e50c507ad57c2ccc08b251a
Jack Magne <jmagne@redhat.com>
2017-10-18 19:04:47 -0700

    Fix: #792 Support SHA256 for SKI
    Provide simple support for this feature.
    Simple mod to the profile default for KeyIdentificationExtDefault to allow
    the user of the console to select a desired hash algorithm for the key info value calculated.
    
    Right now you can choose SHA-1,SHA-256,SHA-384, and SHA-512.
    
    The code defaults to SHA-1 in case of profiles that have not had the hash alg explicity set in the profile for this given extension. The decision should be one made by the user on purpose with full knowledge.
    
    Here is a sample setting that will get set in a given profile for this extension:
    
    policyset.userCertSet.11.constraint.class_id=noConstraintImpl
    policyset.userCertSet.11.constraint.name=No Constraint
    policyset.userCertSet.11.default.class_id=subjectKeyIdentifierExtDefaultImpl
    policyset.userCertSet.11.default.name=Subject Key Identifier Default
    
    policyset.userCertSet.11.default.params.messageDigest=SHA-256
    
    Making this configurable per every profile that may use this extension provides flexibility.
    
    Change-Id: Iebb8f8fb9e8f3be7e2753e92fa73ea61c7fa58b7

--- base/server/cms/src/com/netscape/cms/profile/def/SubjectKeyIdentifierExtDefault.java
+++ base/server/cms/src/com/netscape/cms/profile/def/SubjectKeyIdentifierExtDefault.java
@@ -18,16 +18,7 @@
 package com.netscape.cms.profile.def;
 
 import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
-
-import netscape.security.x509.CertificateX509Key;
-import netscape.security.x509.KeyIdentifier;
-import netscape.security.x509.PKIXExtensions;
-import netscape.security.x509.SubjectKeyIdentifierExtension;
-import netscape.security.x509.X509CertInfo;
-import netscape.security.x509.X509Key;
 
 import com.netscape.certsrv.apps.CMS;
 import com.netscape.certsrv.base.IConfigStore;
@@ -38,6 +29,13 @@ import com.netscape.certsrv.property.EPropertyException;
 import com.netscape.certsrv.property.IDescriptor;
 import com.netscape.certsrv.request.IRequest;
 import com.netscape.cmsutil.crypto.CryptoUtil;
+
+import netscape.security.x509.CertificateX509Key;
+import netscape.security.x509.KeyIdentifier;
+import netscape.security.x509.PKIXExtensions;
+import netscape.security.x509.SubjectKeyIdentifierExtension;
+import netscape.security.x509.X509CertInfo;
+import netscape.security.x509.X509Key;
 
 /**
  * This class implements an enrollment default policy
@@ -52,17 +50,38 @@ public class SubjectKeyIdentifierExtDefault extends EnrollExtDefault {
 
     public static final String VAL_CRITICAL = "critical";
     public static final String VAL_KEY_ID = "keyid";
+    public static final String CONFIG_MD = "messageDigest";
+    public static final String VAL_MD = "messageDigest";
+    public static final String DEF_CONFIG_MDS = "SHA-1,SHA-256,SHA-384,SHA-512";
+    public static final String MD_LABEL="Message digest";
 
     public SubjectKeyIdentifierExtDefault() {
         super();
+
+        CMS.debug("SubjectKeyIdentifierExtDefault: adding config name. " + CONFIG_MD);
+        addConfigName(CONFIG_MD);
+        CMS.debug("SubjectKeyIdentifierExtDefault: done adding config name. " + CONFIG_MD);
         addValueName(VAL_CRITICAL);
         addValueName(VAL_KEY_ID);
+
+        addValueName(VAL_MD);
     }
 
     public void init(IProfile profile, IConfigStore config)
             throws EProfileException {
         super.init(profile, config);
     }
+
+    public IDescriptor getConfigDescriptor(Locale locale, String name) { /* testms */
+        if (name.equals(CONFIG_MD)) {
+            return new Descriptor(IDescriptor.CHOICE, DEF_CONFIG_MDS,
+                    "SHA-1",
+                    MD_LABEL);
+        } else {
+            return null;
+        }
+    }
+
 
     public IDescriptor getValueDescriptor(Locale locale, String name) {
         if (name.equals(VAL_CRITICAL)) {
@@ -75,6 +94,11 @@ public class SubjectKeyIdentifierExtDefault extends EnrollExtDefault {
                     IDescriptor.READONLY,
                     null,
                     CMS.getUserMessage(locale, "CMS_PROFILE_KEY_ID"));
+        } else if (name.equals(VAL_MD)) {
+           return new Descriptor(IDescriptor.STRING,
+                   IDescriptor.READONLY,
+                   null,
+                   MD_LABEL);
         } else {
             return null;
         }
@@ -91,6 +115,9 @@ public class SubjectKeyIdentifierExtDefault extends EnrollExtDefault {
             // read-only; do nothing
         } else if (name.equals(VAL_KEY_ID)) {
             // read-only; do nothing
+        } else if (name.equals(VAL_MD)) {
+            // read-only; do nothing
+            CMS.debug("value: " + value );
         } else {
             throw new EPropertyException(CMS.getUserMessage(
                         locale, "CMS_INVALID_PROPERTY", name));
@@ -154,6 +181,13 @@ public class SubjectKeyIdentifierExtDefault extends EnrollExtDefault {
                                                                   name));
             }
             return toHexString(kid.getIdentifier());
+        } else if (name.equals(VAL_MD)) {
+            String alg = getConfig(CONFIG_MD);
+
+            if(alg == null || alg.length() == 0) {
+                alg = "SHA-1";
+            }
+            return alg;
         } else {
             throw new EPropertyException(CMS.getUserMessage(
                         locale, "CMS_INVALID_PROPERTY", name));
@@ -198,6 +232,10 @@ public class SubjectKeyIdentifierExtDefault extends EnrollExtDefault {
     public KeyIdentifier getKeyIdentifier(X509CertInfo info) {
         String method = "SubjectKeyIdentifierExtDefault: getKeyIdentifier: ";
         try {
+
+            String configHashAlg = getConfig(CONFIG_MD);
+
+            CMS.debug(method + " configured hash alg: " + configHashAlg);
             CertificateX509Key infokey = (CertificateX509Key)
                     info.get(X509CertInfo.KEY);
             X509Key key = (X509Key) infokey.get(CertificateX509Key.KEY);
@@ -206,7 +244,16 @@ public class SubjectKeyIdentifierExtDefault extends EnrollExtDefault {
             // you could specify different algorithm with the alg parameter
             // like this:
             //byte[] hash = CryptoUtil.generateKeyIdentifier(key.getKey(), "SHA-256");
-            byte[] hash = CryptoUtil.generateKeyIdentifier(key.getKey());
+
+            byte[] hash = null;
+
+            if (configHashAlg != null && configHashAlg.length() != 0) {
+                CMS.debug(method + " generating hash with alg: " + configHashAlg);
+                hash = CryptoUtil.generateKeyIdentifier(key.getKey(), configHashAlg);
+            } else {
+                CMS.debug(method + " generating hash with default alg: SHA-1");
+                hash = CryptoUtil.generateKeyIdentifier(key.getKey());
+            }
 
             if (hash == null) {
                 CMS.debug(method +
