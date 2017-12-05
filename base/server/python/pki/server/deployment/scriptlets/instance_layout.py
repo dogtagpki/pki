a60d4d987f1bf9dc9d72031994a6aa2fb7b9f7e4
Endi S. Dewata <edewata@redhat.com>
2017-12-05 01:18:43 +0100

    Refactored instance_layout.py.
    The methods in instance_layout.py has been modified to return early.
    The code has been reformatted to reduce indentations.
    
    https://pagure.io/dogtagpki/issue/2560
    
    Change-Id: Icfac8d4b1af2b09f9df82befbc5714f9d1af38aa

--- base/server/python/pki/server/deployment/scriptlets/instance_layout.py
+++ base/server/python/pki/server/deployment/scriptlets/instance_layout.py
@@ -41,159 +41,162 @@ class PkiScriptlet(pkiscriptlet.AbstractBasePkiScriptlet):
         config.pki_log.info(log.INSTANCE_SPAWN_1, __name__,
                             extra=config.PKI_INDENTATION_LEVEL_1)
 
-        # if this is the first subsystem
-        if len(deployer.instance.tomcat_instance_subsystems()) == 1:
+        # if this is not the first subsystem, skip
+        if len(deployer.instance.tomcat_instance_subsystems()) != 1:
+            return
 
-            # establish instance logs
-            deployer.directory.create(deployer.mdict['pki_instance_log_path'])
+        # establish instance logs
+        deployer.directory.create(deployer.mdict['pki_instance_log_path'])
 
-            # copy /usr/share/pki/server/conf tree into
-            # /var/lib/pki/<instance>/conf
-            # except common ldif files and theme deployment descriptor
-            deployer.directory.copy(
+        # copy /usr/share/pki/server/conf tree into
+        # /var/lib/pki/<instance>/conf
+        # except common ldif files and theme deployment descriptor
+        deployer.directory.copy(
+            deployer.mdict['pki_source_server_path'],
+            deployer.mdict['pki_instance_configuration_path'],
+            ignore_cb=file_ignore_callback_src_server)
+
+        # Link /etc/pki/<instance>/logging.properties
+        # to /usr/share/pki/server/conf/logging.properties.
+        deployer.symlink.create(
+            os.path.join(deployer.mdict['pki_source_server_path'], "logging.properties"),
+            os.path.join(deployer.mdict['pki_instance_configuration_path'],
+                         "logging.properties"))
+
+        # create /etc/sysconfig/<instance>
+        deployer.file.copy_with_slot_substitution(
+            deployer.mdict['pki_source_tomcat_conf'],
+            deployer.mdict['pki_target_tomcat_conf_instance_id'],
+            uid=0, gid=0, overwrite_flag=True)
+
+        # create /var/lib/pki/<instance>/conf/tomcat.conf
+        deployer.file.copy_with_slot_substitution(
+            deployer.mdict['pki_source_tomcat_conf'],
+            deployer.mdict['pki_target_tomcat_conf'],
+            overwrite_flag=True)
+
+        # Deploy ROOT web application
+        deployer.deploy_webapp(
+            "ROOT",
+            os.path.join(
+                config.PKI_DEPLOYMENT_SOURCE_ROOT,
+                "server",
+                "webapps",
+                "ROOT"),
+            os.path.join(
                 deployer.mdict['pki_source_server_path'],
-                deployer.mdict['pki_instance_configuration_path'],
-                ignore_cb=file_ignore_callback_src_server)
+                "Catalina",
+                "localhost",
+                "ROOT.xml"))
 
-            # Link /etc/pki/<instance>/logging.properties
-            # to /usr/share/pki/server/conf/logging.properties.
+        # Deploy pki web application which includes themes,
+        # admin templates, and JS libraries
+        deployer.deploy_webapp(
+            "pki",
+            os.path.join(
+                config.PKI_DEPLOYMENT_SOURCE_ROOT,
+                "server",
+                "webapps",
+                "pki"),
+            os.path.join(
+                deployer.mdict['pki_source_server_path'],
+                "Catalina",
+                "localhost",
+                "pki.xml"))
+
+        # Create Tomcat instance library
+        deployer.directory.create(deployer.mdict['pki_instance_lib'])
+        for name in os.listdir(deployer.mdict['pki_tomcat_lib_path']):
             deployer.symlink.create(
-                os.path.join(deployer.mdict['pki_source_server_path'], "logging.properties"),
-                os.path.join(deployer.mdict['pki_instance_configuration_path'],
-                             "logging.properties"))
-
-            # create /etc/sysconfig/<instance>
-            deployer.file.copy_with_slot_substitution(
-                deployer.mdict['pki_source_tomcat_conf'],
-                deployer.mdict['pki_target_tomcat_conf_instance_id'],
-                uid=0, gid=0, overwrite_flag=True)
-
-            # create /var/lib/pki/<instance>/conf/tomcat.conf
-            deployer.file.copy_with_slot_substitution(
-                deployer.mdict['pki_source_tomcat_conf'],
-                deployer.mdict['pki_target_tomcat_conf'],
-                overwrite_flag=True)
-
-            # Deploy ROOT web application
-            deployer.deploy_webapp(
-                "ROOT",
                 os.path.join(
-                    config.PKI_DEPLOYMENT_SOURCE_ROOT,
-                    "server",
-                    "webapps",
-                    "ROOT"),
+                    deployer.mdict['pki_tomcat_lib_path'],
+                    name),
                 os.path.join(
-                    deployer.mdict['pki_source_server_path'],
-                    "Catalina",
-                    "localhost",
-                    "ROOT.xml"))
+                    deployer.mdict['pki_instance_lib'],
+                    name))
 
-            # Deploy pki web application which includes themes,
-            # admin templates, and JS libraries
-            deployer.deploy_webapp(
-                "pki",
-                os.path.join(
-                    config.PKI_DEPLOYMENT_SOURCE_ROOT,
-                    "server",
-                    "webapps",
-                    "pki"),
-                os.path.join(
-                    deployer.mdict['pki_source_server_path'],
-                    "Catalina",
-                    "localhost",
-                    "pki.xml"))
+        # Link /var/lib/pki/<instance>/lib/log4j.properties
+        # to /usr/share/pki/server/conf/log4j.properties.
+        deployer.symlink.create(
+            os.path.join(deployer.mdict['pki_source_server_path'],
+                         "log4j.properties"),
+            deployer.mdict['pki_instance_lib_log4j_properties'])
 
-            # Create Tomcat instance library
-            deployer.directory.create(deployer.mdict['pki_instance_lib'])
-            for name in os.listdir(deployer.mdict['pki_tomcat_lib_path']):
-                deployer.symlink.create(
-                    os.path.join(
-                        deployer.mdict['pki_tomcat_lib_path'],
-                        name),
-                    os.path.join(
-                        deployer.mdict['pki_instance_lib'],
-                        name))
+        # Link /var/lib/pki/<instance>/common to /usr/share/pki/server/common
+        deployer.symlink.create(
+            '/usr/share/pki/server/common',
+            deployer.mdict['pki_tomcat_common_path'])
 
-            # Link /var/lib/pki/<instance>/lib/log4j.properties
-            # to /usr/share/pki/server/conf/log4j.properties.
-            deployer.symlink.create(
-                os.path.join(deployer.mdict['pki_source_server_path'],
-                             "log4j.properties"),
-                deployer.mdict['pki_instance_lib_log4j_properties'])
+        deployer.directory.create(deployer.mdict['pki_tomcat_tmpdir_path'])
 
-            # Link /var/lib/pki/<instance>/common to /usr/share/pki/server/common
-            deployer.symlink.create(
-                '/usr/share/pki/server/common',
-                deployer.mdict['pki_tomcat_common_path'])
+        deployer.directory.create(deployer.mdict['pki_tomcat_work_path'])
+        deployer.directory.create(
+            deployer.mdict['pki_tomcat_work_catalina_path'])
+        deployer.directory.create(
+            deployer.mdict['pki_tomcat_work_catalina_host_path'])
+        deployer.directory.create(
+            deployer.mdict['pki_tomcat_work_catalina_host_run_path'])
+        deployer.directory.create(
+            deployer.mdict['pki_tomcat_work_catalina_host_subsystem_path'])
+        # establish Tomcat instance logs
+        # establish Tomcat instance registry
+        # establish Tomcat instance convenience symbolic links
+        deployer.symlink.create(
+            deployer.mdict['pki_tomcat_bin_path'],
+            deployer.mdict['pki_tomcat_bin_link'])
+        deployer.symlink.create(
+            deployer.mdict['pki_tomcat_systemd'],
+            deployer.mdict['pki_instance_systemd_link'],
+            uid=0, gid=0)
 
-            deployer.directory.create(deployer.mdict['pki_tomcat_tmpdir_path'])
+        # establish shared NSS security databases for this instance
+        deployer.directory.create(deployer.mdict['pki_database_path'])
+        # establish instance convenience symbolic links
+        deployer.symlink.create(
+            deployer.mdict['pki_database_path'],
+            deployer.mdict['pki_instance_database_link'])
+        deployer.symlink.create(
+            deployer.mdict['pki_instance_configuration_path'],
+            deployer.mdict['pki_instance_conf_link'])
+        deployer.symlink.create(
+            deployer.mdict['pki_instance_log_path'],
+            deployer.mdict['pki_instance_logs_link'])
 
-            deployer.directory.create(deployer.mdict['pki_tomcat_work_path'])
-            deployer.directory.create(
-                deployer.mdict['pki_tomcat_work_catalina_path'])
-            deployer.directory.create(
-                deployer.mdict['pki_tomcat_work_catalina_host_path'])
-            deployer.directory.create(
-                deployer.mdict['pki_tomcat_work_catalina_host_run_path'])
-            deployer.directory.create(
-                deployer.mdict['pki_tomcat_work_catalina_host_subsystem_path'])
-            # establish Tomcat instance logs
-            # establish Tomcat instance registry
-            # establish Tomcat instance convenience symbolic links
-            deployer.symlink.create(
-                deployer.mdict['pki_tomcat_bin_path'],
-                deployer.mdict['pki_tomcat_bin_link'])
-            deployer.symlink.create(
-                deployer.mdict['pki_tomcat_systemd'],
-                deployer.mdict['pki_instance_systemd_link'],
-                uid=0, gid=0)
-
-            # establish shared NSS security databases for this instance
-            deployer.directory.create(deployer.mdict['pki_database_path'])
-            # establish instance convenience symbolic links
-            deployer.symlink.create(
-                deployer.mdict['pki_database_path'],
-                deployer.mdict['pki_instance_database_link'])
-            deployer.symlink.create(
-                deployer.mdict['pki_instance_configuration_path'],
-                deployer.mdict['pki_instance_conf_link'])
-            deployer.symlink.create(
-                deployer.mdict['pki_instance_log_path'],
-                deployer.mdict['pki_instance_logs_link'])
-
-            # create Tomcat instance systemd service link
-            deployer.symlink.create(deployer.mdict['pki_systemd_service'],
-                                    deployer.mdict['pki_systemd_service_link'])
+        # create Tomcat instance systemd service link
+        deployer.symlink.create(deployer.mdict['pki_systemd_service'],
+                                deployer.mdict['pki_systemd_service_link'])
 
     def destroy(self, deployer):
 
         config.pki_log.info(log.INSTANCE_DESTROY_1, __name__,
                             extra=config.PKI_INDENTATION_LEVEL_1)
 
-        if len(deployer.instance.tomcat_instance_subsystems()) == 0:
+        # if this is not the last subsystem, skip
+        if len(deployer.instance.tomcat_instance_subsystems()) != 0:
+            return
 
-            # remove Tomcat instance systemd service link
-            deployer.symlink.delete(deployer.mdict['pki_systemd_service_link'])
+        # remove Tomcat instance systemd service link
+        deployer.symlink.delete(deployer.mdict['pki_systemd_service_link'])
 
-            # remove Tomcat instance base
-            deployer.directory.delete(deployer.mdict['pki_instance_path'])
-            # remove Tomcat instance logs
-            deployer.directory.delete(deployer.mdict['pki_instance_log_path'])
-            # remove shared NSS security database path for this instance
-            deployer.directory.delete(deployer.mdict['pki_database_path'])
-            # remove Tomcat instance configuration
+        # remove Tomcat instance base
+        deployer.directory.delete(deployer.mdict['pki_instance_path'])
+        # remove Tomcat instance logs
+        deployer.directory.delete(deployer.mdict['pki_instance_log_path'])
+        # remove shared NSS security database path for this instance
+        deployer.directory.delete(deployer.mdict['pki_database_path'])
+        # remove Tomcat instance configuration
+        deployer.directory.delete(
+            deployer.mdict['pki_instance_configuration_path'])
+        # remove PKI 'tomcat.conf' instance file
+        deployer.file.delete(
+            deployer.mdict['pki_target_tomcat_conf_instance_id'])
+        # remove Tomcat instance registry
+        deployer.directory.delete(
+            deployer.mdict['pki_instance_registry_path'])
+        # remove Tomcat PKI registry (if empty)
+        if deployer.instance.tomcat_instances() == 0:
             deployer.directory.delete(
-                deployer.mdict['pki_instance_configuration_path'])
-            # remove PKI 'tomcat.conf' instance file
-            deployer.file.delete(
-                deployer.mdict['pki_target_tomcat_conf_instance_id'])
-            # remove Tomcat instance registry
-            deployer.directory.delete(
-                deployer.mdict['pki_instance_registry_path'])
-            # remove Tomcat PKI registry (if empty)
-            if deployer.instance.tomcat_instances() == 0:
-                deployer.directory.delete(
-                    deployer.mdict['pki_instance_type_registry_path'])
+                deployer.mdict['pki_instance_type_registry_path'])
 
 
 # Callback only when the /usr/share/pki/server/conf directory
