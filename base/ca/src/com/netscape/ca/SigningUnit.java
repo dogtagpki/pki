2922cdaab12bf2e42183623d67c0f8f16163eca0
Endi S. Dewata <edewata@redhat.com>
2018-01-12 20:20:59 +0100

    Removed redundant constants in CA's SigningUnit.
    Some constants in CA's SigningUnit have been removed since they
    are already defined in ISigningUnit.
    
    https://pagure.io/dogtagpki/issue/2901
    
    Change-Id: I130bb22eb09fb59b8ce30a2f0bac8d4024daad7d

--- base/ca/src/com/netscape/ca/SigningUnit.java
+++ base/ca/src/com/netscape/ca/SigningUnit.java
@@ -59,13 +59,6 @@ import netscape.security.x509.X509Key;
  */
 
 public final class SigningUnit implements ISigningUnit {
-    public static final String PROP_DEFAULT_SIGNALG = "defaultSigningAlgorithm";
-    public static final String PROP_CERT_NICKNAME = "cacertnickname";
-    // This signing unit is being used in OCSP and CRL also. So
-    // it is better to have a more generic name
-    public static final String PROP_RENAMED_CERT_NICKNAME = "certnickname";
-    public static final String PROP_TOKEN_NAME = "tokenname";
-    public static final String PROP_NEW_NICKNAME = "newNickname";
 
     private CryptoManager mManager = null;
     private CryptoToken mToken = null;
