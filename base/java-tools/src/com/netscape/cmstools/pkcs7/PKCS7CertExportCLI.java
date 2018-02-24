64340154405cd5c00bf7b85ae9e346e8d984c6af
Endi S. Dewata <edewata@redhat.com>
2018-02-24 01:17:14 +0100

    Refactored logger level configuration.
    The code that configures logger level for PKI classes has been
    consolidated into PKILogger.setLevel().
    
    https://pagure.io/dogtagpki/issue/195
    
    Change-Id: Iff9bb9ad4ff843e37c3e2d6d53ce7b7a2f564823

--- base/java-tools/src/com/netscape/cmstools/pkcs7/PKCS7CertExportCLI.java
+++ base/java-tools/src/com/netscape/cmstools/pkcs7/PKCS7CertExportCLI.java
@@ -23,11 +23,11 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.cert.X509Certificate;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
+import org.dogtagpki.util.logger.PKILogger;
 
 import com.netscape.cmstools.cli.CLI;
 import com.netscape.cmsutil.crypto.CryptoUtil;
@@ -78,14 +78,10 @@ public class PKCS7CertExportCLI extends CLI {
         }
 
         if (cmd.hasOption("verbose")) {
-            Logger.getLogger("org.dogtagpki").setLevel(Level.INFO);
-            Logger.getLogger("com.netscape").setLevel(Level.INFO);
-            Logger.getLogger("netscape").setLevel(Level.INFO);
+            PKILogger.setLevel(PKILogger.Level.INFO);
 
         } else if (cmd.hasOption("debug")) {
-            Logger.getLogger("org.dogtagpki").setLevel(Level.FINE);
-            Logger.getLogger("com.netscape").setLevel(Level.FINE);
-            Logger.getLogger("netscape").setLevel(Level.FINE);
+            PKILogger.setLevel(PKILogger.Level.DEBUG);
         }
 
         String filename = cmd.getOptionValue("pkcs7-file");
