d9ec2bee47b4d4fbd5d93e46c0d7f54a0299ba9a
Endi S. Dewata <edewata@redhat.com>
2018-02-25 20:30:23 +0100

    Added separate logging for each subsystem.
    The links to SLF4J libraries have been moved into each subsystem
    to allow separate logging. A new logging.properties files has been
    added to each subsystem as well.
    
    The pki.policy has been modified to allow Tomcat JULI to read the
    logging.properties and write log files for each subsystem.
    
    https://pagure.io/dogtagpki/issue/195
    
    Change-Id: I6c8b6e6744408e21d620ca1983b2be18c353de73

---
+++ base/server/tomcat/src/org/dogtagpki/tomcat/PKIFormatter.java
@@ -0,0 +1,49 @@
+// --- BEGIN COPYRIGHT BLOCK ---
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// (C) 2018 Red Hat, Inc.
+// All rights reserved.
+// --- END COPYRIGHT BLOCK ---
+
+package org.dogtagpki.tomcat;
+
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+public class PKIFormatter extends Formatter {
+
+    public String format(LogRecord record) {
+
+        // 2018-02-23 10:18:51 [main] INFO: Log message
+
+        return String.format(
+                "%1$tF %1$tT [%2$s] %3$s: %4$s%n",
+                new Date(record.getMillis()),
+                Thread.currentThread().getName(),
+                record.getLevel(),
+                formatMessage(record));
+    }
+
+    public static void main(String[] args) {
+
+        PKIFormatter formatter = new PKIFormatter();
+
+        LogRecord record = new LogRecord(Level.INFO, "Log message");
+        record.setSourceClassName(PKIFormatter.class.getName());
+
+        System.out.println(formatter.format(record));
+    }
+}
