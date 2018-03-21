d515d6ceb6b1ce42bd45ae4f74e97ed63b9444cd
Amol Kahat <akahat@redhat.com>
2018-03-22 01:14:27 +0530

    Added code for audit event add, mod, del CLI. New cli introduced:   pki-server <subsystem>-audit-event-add   pki-server <subsystem>-audit-event-mod   pki-server <subsystem>-audit-event-del
    Pagure: https://pagure.io/dogtagpki/issue/2914
    
    Change-Id: Ifc97f7b0155cd266cb44df8301df77768dc360a0
    Signed-off-by: Amol Kahat <akahat@redhat.com>

--- base/server/python/pki/server/cli/audit.py
+++ base/server/python/pki/server/cli/audit.py
@@ -18,8 +18,8 @@
 # All rights reserved.
 #
 
-from __future__ import absolute_import
-from __future__ import print_function
+from __future__ import absolute_import, print_function
+
 import getopt
 import os
 import shutil
@@ -38,6 +38,9 @@ class AuditCLI(pki.cli.CLI):
 
         self.parent = parent
         self.add_module(AuditEventFindCLI(self))
+        self.add_module(AuditEventEnableCLI(self))
+        self.add_module(AuditEventModCLI(self))
+        self.add_module(AuditEventDisableCLI(self))
         self.add_module(AuditFileFindCLI(self))
         self.add_module(AuditFileVerifyCLI(self))
 
@@ -122,6 +125,221 @@ class AuditEventFindCLI(pki.cli.CLI):
             print('  Event Name: %s' % event.get('name'))
             print('  Enabled: %s' % event.get('enabled'))
             print('  Filter: %s' % event.get('filter'))
+
+
+class AuditEventEnableCLI(pki.cli.CLI):
+
+    def __init__(self, parent):
+        super(AuditEventEnableCLI, self).__init__(
+            'event-enable', 'Enable audit event configurations')
+
+        self.parent = parent
+
+    def print_help(self):
+        print('Usage: pki-server %s-audit-event-enable <event_name> [OPTIONS]'
+              % self.parent.parent.name)
+        print()
+        print('  -i, --instance <instance ID>       Instance ID (default: pki-tomcat).')
+        print('  -v, --verbose                      Run in verbose mode.')
+        print('      --help                         Show help message.')
+        print()
+
+    def execute(self, argv):
+
+        try:
+            opts, args = getopt.gnu_getopt(argv, 'i:v', [
+                'instance=', 'verbose', 'help'])
+
+        except getopt.GetoptError as e:
+            print('ERROR: ' + str(e))
+            self.print_help()
+            sys.exit(1)
+
+        instance_name = 'pki-tomcat'
+
+        for o, a in opts:
+            if o in ('-i', '--instance'):
+                instance_name = a
+
+            elif o in ('-v', '--verbose'):
+                self.set_verbose(True)
+
+            elif o == '--help':
+                self.print_help()
+                sys.exit()
+
+            else:
+                print('ERROR: unknown option ' + o)
+                self.print_help()
+                sys.exit(1)
+
+        if len(args) == 0:
+            raise getopt.GetoptError("Missing event name.")
+        if len(args) > 1:
+            raise getopt.GetoptError("Too much arguments specified.")
+        event_name = args[0]
+
+        instance = pki.server.PKIInstance(instance_name)
+        if not instance.is_valid():
+            print('ERROR: Invalid instance %s.' % instance_name)
+            sys.exit(1)
+
+        instance.load()
+
+        subsystem_name = self.parent.parent.name
+        subsystem = instance.get_subsystem(subsystem_name)
+        if not subsystem:
+            print('ERROR: No %s subsystem in instance %s.'
+                  % (subsystem_name.upper(), instance_name))
+            sys.exit(1)
+
+        subsystem.enable_audit_event(event_name)
+
+
+class AuditEventModCLI(pki.cli.CLI):
+
+    def __init__(self, parent):
+        super(AuditEventModCLI, self).__init__(
+            'event-modify', 'Mod audit event configurations')
+
+        self.parent = parent
+
+    def print_help(self):
+        print('Usage: pki-server %s-audit-event-modify <event_name> '
+              '[OPTIONS]' % self.parent.parent.name)
+        print()
+        print('  -i, --instance <instance ID>       Instance ID (default: pki-tomcat).')
+        print('  -f, --filter <event filter>        Event Filter (Ex: (Outcome=Failure)).')
+        print('  -v, --verbose                      Run in verbose mode.')
+        print('      --help                         Show help message.')
+        print()
+
+    def execute(self, argv):
+
+        try:
+            opts, args = getopt.gnu_getopt(argv, 'i:f:v',
+                                           ['instance=', 'filter=', 'verbose',
+                                            'help'])
+
+        except getopt.GetoptError as e:
+            print('ERROR: ' + str(e))
+            self.print_help()
+            sys.exit(1)
+
+        instance_name = 'pki-tomcat'
+        filter_name = None
+
+        for o, a in opts:
+            if o in ('-i', '--instance'):
+                instance_name = a
+
+            elif o in ('-f', '--filter'):
+                filter_name = a
+
+            elif o in ('-v', '--verbose'):
+                self.set_verbose(True)
+
+            elif o == '--help':
+                self.print_help()
+                sys.exit()
+
+            else:
+                print('ERROR: unknown option ' + o)
+                self.print_help()
+                sys.exit(1)
+
+        if len(args) == 0:
+            raise getopt.GetoptError("Missing event name.")
+        if len(args) > 1:
+            raise getopt.GetoptError("Too much arguments specified.")
+
+        event_name = args[0]
+
+        instance = pki.server.PKIInstance(instance_name)
+        if not instance.is_valid():
+            print('ERROR: Invalid instance %s.' % instance_name)
+            sys.exit(1)
+
+        instance.load()
+
+        subsystem_name = self.parent.parent.name
+        subsystem = instance.get_subsystem(subsystem_name)
+        if not subsystem:
+            print('ERROR: No %s subsystem in instance %s.'
+                  % (subsystem_name.upper(), instance_name))
+            sys.exit(1)
+
+        subsystem.update_audit_event_filter(event_name, filter_name)
+
+
+class AuditEventDisableCLI(pki.cli.CLI):
+
+    def __init__(self, parent):
+        super(AuditEventDisableCLI, self).__init__(
+            'event-disable', 'Disable audit event configurations')
+
+        self.parent = parent
+
+    def print_help(self):
+        print('Usage: pki-server %s-audit-event-disable <event_name> [OPTIONS]'
+              % self.parent.parent.name)
+        print()
+        print('  -i, --instance <instance ID>       Instance ID (default: pki-tomcat).')
+        print('  -v, --verbose                      Run in verbose mode.')
+        print('      --help                         Show help message.')
+        print()
+
+    def execute(self, argv):
+
+        try:
+            opts, args = getopt.gnu_getopt(argv, 'i:v', [
+                'instance=', 'verbose', 'help'])
+
+        except getopt.GetoptError as e:
+            print('ERROR: ' + str(e))
+            self.print_help()
+            sys.exit(1)
+
+        instance_name = 'pki-tomcat'
+
+        for o, a in opts:
+            if o in ('-i', '--instance'):
+                instance_name = a
+
+            elif o in ('-v', '--verbose'):
+                self.set_verbose(True)
+
+            elif o == '--help':
+                self.print_help()
+                sys.exit()
+
+            else:
+                print('ERROR: unknown option ' + o)
+                self.print_help()
+                sys.exit(1)
+
+        if len(args) == 0:
+            raise getopt.GetoptError("Missing event name.")
+        if len(args) > 1:
+            raise getopt.GetoptError("Too much arguments specified.")
+
+        event_name = args[0]
+
+        instance = pki.server.PKIInstance(instance_name)
+        if not instance.is_valid():
+            print('ERROR: Invalid instance %s.' % instance_name)
+            sys.exit(1)
+
+        instance.load()
+
+        subsystem_name = self.parent.parent.name
+        subsystem = instance.get_subsystem(subsystem_name)
+        if not subsystem:
+            print('ERROR: No %s subsystem in instance %s.'
+                  % (subsystem_name.upper(), instance_name))
+            sys.exit(1)
+
+        subsystem.disable_audit_event(event_name)
 
 
 class AuditFileFindCLI(pki.cli.CLI):
