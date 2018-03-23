520ad5092cf0b1542e2fd1c69247c70d2b8d21ae
Dinesh Prasanth M K <dmoluguw@redhat.com>
2018-03-23 13:53:15 -0400

    Redesigned CI infrastructure
    The Travis CI infrastructure now directly sets Gerrit
    labels instead of the TravisPy library being used.
    
    - This avoids dependency on 3rd party libraries.
    - Logs are posted directly as comments in case of failure
    
    Change-Id: Ie399f612655f3172c065ccd626a9dd41b588ee59

--- .travis/run_task.sh
+++ .travis/run_task.sh
@@ -19,9 +19,9 @@
 # Copyright (C) 2017 Red Hat, Inc.
 # All rights reserved.
 #
+set -e
 
 PYTHON="/usr/bin/python${TRAVIS_PYTHON_VERSION}"
-TEST_RUNNER_CONFIG=".test_runner_config.yaml"
 
 test_set="test_caacl_plugin.py test_caacl_profile_enforcement.py test_cert_plugin.py test_certprofile_plugin.py test_vault_plugin.py"
 developer_mode_opt="--developer-mode"
@@ -40,9 +40,9 @@ function truncate_log_to_test_failures() {
 }
 
 
-if [[ "$TASK_TO_RUN" == "run-ipa-test" ]]
+if [[ "$TASK_TO_RUN" == "ipa-test" ]]
 then
-    docker pull ${IPA_TEST_RUNNER_IMAGE}
+    docker pull ${IPA_IMAGE}
 
     # Copy the built RPMS to host machine
     mkdir -p ${DOGTAG_PKI_RPMS}
@@ -59,22 +59,38 @@ then
     echo ${cert_test_file_loc}
 
     ipa-docker-test-runner -l ${CI_RESULTS_LOG} \
-        -c ${TEST_RUNNER_CONFIG} \
+        -c .travis/ipa-test.yaml \
         $developer_mode_opt \
         --container-environment "PYTHON=$PYTHON" \
-        --container-image ${IPA_TEST_RUNNER_IMAGE} \
+        --container-image ${IPA_IMAGE} \
         --git-repo ${TRAVIS_BUILD_DIR} \
         run-tests ${cert_test_file_loc}
 
-elif [[ "$TASK_TO_RUN" == "run-dogtag-build" ]]
+elif [[ "$TASK_TO_RUN" == "pki-test" ]]
 then
-   docker exec -i ${CONTAINER} ${SCRIPTDIR}/20-install-rpms
+   # First install pki-core packges as it's the dependency for other packages
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/20-install-rpms || exit $?
+
+   # Install deps, generate RPMS for all other packages and install them (in order)
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/01-install-dependencies dogtag-pki-theme   # meta
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/01-install-dependencies pki-console
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/01-install-dependencies dogtag-pki
+
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/10-compose-rpms compose_dogtag_pki_theme_packages
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/20-install-rpms || exit $?
+
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/10-compose-rpms compose_pki_console_packages
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/20-install-rpms || exit $?
+
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/10-compose-rpms compose_dogtag_pki_meta_packages
+   docker exec -i ${CONTAINER} ${SCRIPTDIR}/20-install-rpms || exit $?
+
    docker exec -i ${CONTAINER} ${SCRIPTDIR}/30-setup-389ds
    # Test whether pki subsystem works correctly
    docker exec -i ${CONTAINER} ${SCRIPTDIR}/40-spawn-ca
    docker exec -i ${CONTAINER} ${SCRIPTDIR}/50-spawn-kra
    docker exec -i ${CONTAINER} ${SCRIPTDIR}/99-destroy
-   # copy pki.server for Python 3 and rewrite pkispawn/pkidestroy shebang
+   # copy pki.server for Python 3
    docker exec -i ${CONTAINER} ${SCRIPTDIR}/py3rewrite
    docker exec -i ${CONTAINER} ${SCRIPTDIR}/30-setup-389ds
    docker exec -i ${CONTAINER} ${SCRIPTDIR}/40-spawn-ca
@@ -86,7 +102,7 @@ fi
 
 exit_status="$?"
 
-if [[ "$exit_status" -ne 0 && "$TASK_TO_RUN" == "run-ipa-test" ]]
+if [[ "$exit_status" -ne 0 && "$TASK_TO_RUN" == "ipa-test" ]]
 then
     truncate_log_to_test_failures
 fi
