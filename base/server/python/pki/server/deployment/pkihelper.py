dogtagpki/pki

commit 0a05eab2483e6248a3e14a97c214e531828cd9be

Refactored wait_for_startup() (part 2).
The wait_for_startup() method in pkihelper.py has been modified
to handle the exceptions thrown by get_instance_status(). If it
is an SSLError, the method will terminate immediately. If it's a
ConnectionError, it will wait for the server to start.

https://pagure.io/dogtagpki/issue/203

Change-Id: I70f97c08b1ff3dbf54e6ee5657fcf1af1605ccaa

base/server/python/pki/server/deployment/pkihelper.py
@@ -1020,28 +1020,17 @@ class Instance:
 
     def get_instance_status(self, connection, timeout=None):
 
-        # catching all exceptions because we do not want to break if underlying
-        # requests or urllib3 use a different exception.
-        # If the connection fails, we will time out in any case
-        # pylint: disable=W0703
-        try:
-            client = pki.system.SystemStatusClient(connection)
-            response = client.get_status(timeout=timeout)
-            config.pki_log.debug(
-                response,
-                extra=config.PKI_INDENTATION_LEVEL_3)
+        client = pki.system.SystemStatusClient(connection)
+        response = client.get_status(timeout=timeout)
 
-            root = ET.fromstring(response)
-            status = root.findtext("Status")
-            return status
-        except Exception as exc:
-            config.pki_log.debug(
-                "No connection - server may still be down",
-                extra=config.PKI_INDENTATION_LEVEL_3)
-            config.pki_log.debug(
-                "No connection - exception thrown: %s", exc,
-                extra=config.PKI_INDENTATION_LEVEL_3)
-            return None
+        config.pki_log.debug(
+            response,
+            extra=config.PKI_INDENTATION_LEVEL_3)
+
+        root = ET.fromstring(response)
+        status = root.findtext("Status")
+
+        return status
 
     def wait_for_startup(
         self,
@@ -1081,20 +1070,45 @@ class Instance:
 
         start_time = datetime.today()
         status = None
+        counter = 0
 
         while status != "running":
+            try:
+                time.sleep(1)
 
-            time.sleep(1)
+                status = self.get_instance_status(
+                    connection=connection,
+                    timeout=request_timeout,
+                )
 
-            status = self.get_instance_status(
-                connection=connection,
-                timeout=request_timeout,
-            )
-
-            stop_time = datetime.today()
-
-            if (stop_time - start_time).total_seconds() >= timeout:
+            except requests.exceptions.SSLError as exc:
+                max_retry_error = exc.args[0]
+                reason = getattr(max_retry_error, 'reason')
+                config.pki_log.error(
+                    "server unreachable due to SSL error: %s", reason,
+                    extra=config.PKI_INDENTATION_LEVEL_3)
                 break
+
+            except requests.exceptions.ConnectionError as exc:
+
+                stop_time = datetime.today()
+                counter = (stop_time - start_time).total_seconds()
+
+                if counter >= timeout:
+
+                    config.pki_log.error(
+                        "server did not start after %ds",
+                        timeout,
+                        extra=config.PKI_INDENTATION_LEVEL_3)
+
+                    break
+
+                config.pki_log.info(
+                    "waiting for server to start (%ds)",
+                    int(round(counter)),
+                    extra=config.PKI_INDENTATION_LEVEL_3)
+
+                continue
 
         return status
 

commit 353de17f540a12107509f4b06f207d5d000ff4dc

Retry request on timeout error
Commit 0a05eab2 introduced a regression in wait_for_startup. The routine
now catches and retries timeout errors along with connection errors. A
timeout error may occur while a server is starting but is not ready to
respond to a request fast enough.

Change-Id: Ica2ccea4a5b5feb92a46a69284950bf8cfd06258
Closes: https://pagure.io/freeipa/issue/7425
Closes: https://pagure.io/dogtagpki/issue/2973
Signed-off-by: Christian Heimes <cheimes@redhat.com>

base/server/python/pki/server/deployment/pkihelper.py
@@ -66,6 +66,12 @@ if selinux.is_selinux_enabled():
         if sys.version_info.major == 2:
             raise
 
+# Retry-able connection errors, see https://pagure.io/dogtagpki/issue/2973
+RETRYABLE_EXCEPTIONS = (
+    requests.exceptions.ConnectionError,  # connection failed
+    requests.exceptions.Timeout,  # connection or read time out
+)
+
 
 class Identity:
     """PKI Deployment Identity Class"""
@@ -1089,7 +1095,7 @@ class Instance:
                     extra=config.PKI_INDENTATION_LEVEL_3)
                 break
 
-            except requests.exceptions.ConnectionError as exc:
+            except RETRYABLE_EXCEPTIONS as exc:
 
                 stop_time = datetime.today()
                 counter = (stop_time - start_time).total_seconds()

base/server/python/pki/server/pkispawn.py
@@ -38,6 +38,7 @@ try:
     import pki
     from pki.server.deployment import pkiconfig as config
     from pki.server.deployment import pkimanifest as manifest
+    from pki.server.deployment.pkihelper import RETRYABLE_EXCEPTIONS
     from pki.server.deployment.pkiparser import PKIConfigParser
     from pki.server.deployment import pkilogging
     from pki.server.deployment import pkimessages as log
@@ -357,7 +358,7 @@ def main(argv):
                                             'pki_security_domain_name',
                                             info.name)
                         break
-                    except requests.exceptions.ConnectionError as e:
+                    except RETRYABLE_EXCEPTIONS as e:
                         parser.print_text('ERROR: ' + str(e))
 
                 while True:
@@ -388,7 +389,7 @@ def main(argv):
                         if status == 'running':
                             break
                         parser.print_text('ERROR: CA is not running')
-                    except requests.exceptions.ConnectionError as e:
+                    except RETRYABLE_EXCEPTIONS as e:
                         parser.print_text('ERROR: ' + str(e))
 
                 while True:
@@ -400,7 +401,7 @@ def main(argv):
                         if status == 'running':
                             break
                         parser.print_text('ERROR: TKS is not running')
-                    except requests.exceptions.ConnectionError as e:
+                    except RETRYABLE_EXCEPTIONS as e:
                         parser.print_text('ERROR: ' + str(e))
 
                 while True:
@@ -423,7 +424,7 @@ def main(argv):
                             if status == 'running':
                                 break
                             parser.print_text('ERROR: KRA is not running')
-                        except requests.exceptions.ConnectionError as e:
+                        except RETRYABLE_EXCEPTIONS as e:
                             parser.print_text('ERROR: ' + str(e))
                     else:
                         parser.set_property(deployer.subsystem_name,
@@ -709,11 +710,7 @@ def check_security_domain(parser):
                                     info.name)
                 parser.sd_authenticate()
 
-        except requests.exceptions.ConnectionError as e:
-            print(('ERROR:  Unable to access security domain: ' + str(e)))
-            sys.exit(1)
-
-        except requests.exceptions.HTTPError as e:
+        except requests.exceptions.RequestException as e:
             print(('ERROR:  Unable to access security domain: ' + str(e)))
             sys.exit(1)
 