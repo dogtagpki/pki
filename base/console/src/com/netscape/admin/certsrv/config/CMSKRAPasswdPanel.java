f2b2fffe3eb4d9b9f10974f945c3c56bb707f7b4
Endi S. Dewata <edewata@redhat.com>
2018-04-26 23:34:40 +0200

    Removed warnings in CMSKRAPasswdPanel
    Change-Id: Iec29d4469fe857223735c03300bd3b0f54e2be8f

--- base/console/src/com/netscape/admin/certsrv/config/CMSKRAPasswdPanel.java
+++ base/console/src/com/netscape/admin/certsrv/config/CMSKRAPasswdPanel.java
@@ -17,14 +17,36 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.admin.certsrv.config;
 
-import com.netscape.admin.certsrv.*;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.StringTokenizer;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+
+import com.netscape.admin.certsrv.CMSAdminResources;
+import com.netscape.admin.certsrv.CMSAdminUtil;
+import com.netscape.admin.certsrv.CMSBaseResourceModel;
+import com.netscape.admin.certsrv.EAdminException;
 import com.netscape.admin.certsrv.connection.AdminConnection;
-import com.netscape.certsrv.common.*;
-import com.netscape.admin.certsrv.ug.*;
-import javax.swing.*;
-import java.awt.*;
-import java.util.*;
-import java.awt.event.*;
+import com.netscape.admin.certsrv.ug.CMSBaseUGTab;
+import com.netscape.admin.certsrv.ug.CMSUGTabPanel;
+import com.netscape.certsrv.common.Constants;
+import com.netscape.certsrv.common.DestDef;
+import com.netscape.certsrv.common.NameValuePairs;
+import com.netscape.certsrv.common.ScopeDef;
 
 /**
  * KRA password management tab
@@ -33,12 +55,15 @@ import java.awt.event.*;
  * @version $Revision$, $Date$
  */
 public class CMSKRAPasswdPanel extends CMSBaseUGTab {
+
+    private static final long serialVersionUID = 1L;
+
     private static String PANEL_NAME = "KRAPASSWD";
     private CMSBaseResourceModel mModel;
     private JButton mPwdBtn;
     private AdminConnection mAdmin;
-    private JList mAgentList;
-    private DefaultListModel mAgentModel;
+    private JList<JLabel> mAgentList;
+    private DefaultListModel<JLabel> mAgentModel;
     private Icon mUserIcon;
     private JButton mRefresh, mHelp;
     private static final String HELPINDEX =
@@ -127,19 +152,19 @@ public class CMSKRAPasswdPanel extends CMSBaseUGTab {
         listPanel.setBorder(makeTitledBorder("RECOVERYLIST"));
 
         CMSAdminUtil.resetGBC(gbc);
-        gbc.anchor = gbc.NORTH;
+        gbc.anchor = GridBagConstraints.NORTH;
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
-        gbc.gridheight = gbc.REMAINDER;
-        gbc.gridwidth = gbc.REMAINDER;
+        gbc.gridheight = GridBagConstraints.REMAINDER;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
         gb3.setConstraints(listPanel, gbc);
         mainPanel.add(listPanel);
 
         // label for table
         JLabel tablelbl = makeJLabel("RECOVERYLIST");
         CMSAdminUtil.resetGBC(gbc);
-        gbc.anchor = gbc.NORTHWEST;
-        gbc.fill = gbc.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 0;
         //gbc.weightx = 1.0;
@@ -148,7 +173,7 @@ public class CMSKRAPasswdPanel extends CMSBaseUGTab {
         listPanel.add(tablelbl);
 
         // agent table
-        mAgentModel = new DefaultListModel();
+        mAgentModel = new DefaultListModel<>();
         mAgentList = makeJList(mAgentModel, 10);
         mAgentList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         MouseListener mouseListener = new MouseAdapter() {
@@ -165,27 +190,27 @@ public class CMSKRAPasswdPanel extends CMSBaseUGTab {
         JScrollPane scrollPane = createScrollPane(mAgentList);
 
         CMSAdminUtil.resetGBC(gbc);
-        gbc.anchor = gbc.NORTHWEST;
-        gbc.fill = gbc.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
         gbc.gridx = 0;
         gbc.gridy = 1;
-        gbc.gridheight = gbc.REMAINDER;
+        gbc.gridheight = GridBagConstraints.REMAINDER;
         gb.setConstraints(scrollPane, gbc);
         listPanel.add(scrollPane);
 
         // change password button
         mPwdBtn = makeJButton("CHANGEPWD");
         CMSAdminUtil.resetGBC(gbc);
-        gbc.anchor = gbc.NORTHEAST;
-        gbc.fill = gbc.NONE;
+        gbc.anchor = GridBagConstraints.NORTHEAST;
+        gbc.fill = GridBagConstraints.NONE;
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
         gbc.gridx = 1;
         gbc.gridy = 1;
-        gbc.gridheight = gbc.REMAINDER;
-        gbc.gridwidth = gbc.REMAINDER;
+        gbc.gridheight = GridBagConstraints.REMAINDER;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
         gb.setConstraints(mPwdBtn, gbc);
         listPanel.add(mPwdBtn);
 
@@ -194,7 +219,7 @@ public class CMSKRAPasswdPanel extends CMSBaseUGTab {
         return mainPanel;
     }
 
-    private JScrollPane createScrollPane(JList listbox) {
+    private JScrollPane createScrollPane(JList<JLabel> listbox) {
 
         JScrollPane scrollPane = new JScrollPane(listbox,
             JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
@@ -230,14 +255,14 @@ public class CMSKRAPasswdPanel extends CMSBaseUGTab {
 			int s = mAgentList.getModel().getSize();
 			boolean foundID = false;
 			for (int i = 0; i < s; i++) {
-				JLabel l = (JLabel)mAgentList.getModel().getElementAt(i);
+				JLabel l = mAgentList.getModel().getElementAt(i);
 				if (str.equals(l.getText())) {
 					foundID = true;
 					break;
 				}
 			}
 			if (foundID) {
-            	JDialog pwdDialog = new CMSPasswordDialog(mModel.getFrame(),
+                new CMSPasswordDialog(mModel.getFrame(),
               		mAdmin, str);
 			} else {
             	showErrorDialog("Invalid ID");
