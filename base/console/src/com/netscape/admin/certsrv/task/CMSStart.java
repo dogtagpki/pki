5edf033321ebcaa119df3c56d4d3455b58b14ee0
Endi S. Dewata <edewata@redhat.com>
2018-04-27 06:02:34 +0200

    Removed warnings in CMSStart
    Change-Id: Ic78afc514a3dc02ed9e7ab6c16155fb9bf874d81

--- base/console/src/com/netscape/admin/certsrv/task/CMSStart.java
+++ base/console/src/com/netscape/admin/certsrv/task/CMSStart.java
@@ -17,15 +17,23 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.admin.certsrv.task;
 
-import java.util.*;
-import javax.swing.*;
-import com.netscape.management.client.*;
-import com.netscape.management.client.console.*;
-import com.netscape.admin.certsrv.*;
-import com.netscape.admin.certsrv.connection.*;
-import com.netscape.certsrv.common.*;
-import com.netscape.management.client.util.*;
-import netscape.ldap.*;
+import java.util.Hashtable;
+import java.util.StringTokenizer;
+
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+
+import com.netscape.admin.certsrv.CMSAdminResources;
+import com.netscape.admin.certsrv.CMSAdminUtil;
+import com.netscape.management.client.IPage;
+import com.netscape.management.client.console.ConsoleInfo;
+import com.netscape.management.client.util.Debug;
+import com.netscape.management.client.util.LDAPUtil;
+import com.netscape.management.client.util.UtilConsoleGlobals;
+
+import netscape.ldap.LDAPConnection;
+import netscape.ldap.LDAPEntry;
+import netscape.ldap.LDAPException;
 
 /**
  * Start the server
@@ -35,13 +43,13 @@ import netscape.ldap.*;
  */
 public class CMSStart extends CGITask
 {
+    private static final long serialVersionUID = 1L;
+
     /*==========================================================
      * variables
      *==========================================================*/
     private static final String PREFIX = "TASKSTART";
     public static final String START_TASK_CGI = "Tasks/Operation/start";
-    private Hashtable mCgiResponse = null;
-    private String mCgiTask = null;
 
 	/*==========================================================
      * constructors
@@ -70,7 +78,7 @@ public class CMSStart extends CGITask
         if (dialog.isCancel())
             return false;
 
-        Hashtable configParams = new Hashtable();
+        Hashtable<String, Object> configParams = new Hashtable<>();
         configParams.put("serverRoot",_consoleInfo.get("serverRoot"));
         String servid = (String)_consoleInfo.get("servid");
         int index = servid.indexOf("-");
