eb5b163c96b54283fcf5bee3771ec867915f81fc
Endi S. Dewata <edewata@redhat.com>
2018-05-14 19:49:33 -0500

    Removed duplicate CI tests
    The pki-test.sh has been modified to remove duplicate tests.
    
    https://pagure.io/dogtagpki/issue/2882
    
    Change-Id: I776cd848a0214be6bc03cb010e373dd13e3b27d4

--- travis/pki-test.sh
+++ travis/pki-test.sh
@@ -24,11 +24,12 @@ set -e
 # First install pki-core packges as it's the dependency for other packages
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/20-install-rpms || exit $?
 
-# Install deps, generate RPMS for all other packages and install them (in order)
-docker exec -i ${CONTAINER} ${SCRIPTDIR}/01-install-dependencies dogtag-pki-theme   # meta
+# Install PKI build dependencies
+docker exec -i ${CONTAINER} ${SCRIPTDIR}/01-install-dependencies dogtag-pki-theme
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/01-install-dependencies pki-console
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/01-install-dependencies dogtag-pki
 
+# Build and install PKI packages
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/10-compose-rpms compose_dogtag_pki_theme_packages
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/20-install-rpms || exit $?
 
@@ -38,13 +39,7 @@ docker exec -i ${CONTAINER} ${SCRIPTDIR}/20-install-rpms || exit $?
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/10-compose-rpms compose_dogtag_pki_meta_packages
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/20-install-rpms || exit $?
 
-docker exec -i ${CONTAINER} ${SCRIPTDIR}/30-setup-389ds
-# Test whether pki subsystem works correctly
-docker exec -i ${CONTAINER} ${SCRIPTDIR}/40-spawn-ca
-docker exec -i ${CONTAINER} ${SCRIPTDIR}/50-spawn-kra
-docker exec -i ${CONTAINER} ${SCRIPTDIR}/99-destroy
-# copy pki.server for Python 3
-docker exec -i ${CONTAINER} ${SCRIPTDIR}/py3rewrite
+# Test basic PKI installations
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/30-setup-389ds
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/40-spawn-ca
 docker exec -i ${CONTAINER} ${SCRIPTDIR}/50-spawn-kra
