01f0122639ac49d8c5a66a167fcab726af03fb07
Endi S. Dewata <edewata@redhat.com>
2018-05-14 20:36:11 -0500

    Added basic OCSP installation test
    Change-Id: I2837dce498d70822795e4de6d847a5b4c6efccb1

--- travis/remove-all.sh
+++ travis/remove-all.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ -d /etc/pki/pkitest/kra ]; then
-    pkidestroy -v -i pkitest -s KRA
-fi
-
+pkidestroy -v -i pkitest -s OCSP
+pkidestroy -v -i pkitest -s KRA
 pkidestroy -v -i pkitest -s CA
 
 remove-ds.pl -f -i slapd-pkitest
-
