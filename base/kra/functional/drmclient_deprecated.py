ba497148506981500fab5717d296eb7554514626
Endi S. Dewata <edewata@redhat.com>
2018-05-16 21:21:11 -0500

    Renamed COPYING to LICENSE
    Change-Id: I21de12b9aac61e7277a3163ce4c4bcef24825455

--- base/kra/functional/drmclient_deprecated.py
+++ base/kra/functional/drmclient_deprecated.py
@@ -2,7 +2,7 @@
 #   Ade Lee <alee@redhat.com>
 #
 # Copyright (C) 2012  Red Hat
-# see file 'COPYING' for use and warranty information
+# see file 'LICENSE' for use and warranty information
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
