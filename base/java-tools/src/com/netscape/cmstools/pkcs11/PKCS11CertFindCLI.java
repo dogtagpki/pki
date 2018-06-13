3bb33d5e04fffd80f21e9f7ca20de5e487fef55b
Endi S. Dewata <edewata@redhat.com>
2018-06-13 05:49:40 +0200

    Added pki pkcs11-cert-find
    A new pki pkcs11-cert-find CLI has been added to list the certs in
    PKCS #11 keystore.
    
    Change-Id: I718fa72a5b11de046f110f70c7b286e7df8eaf83

---
+++ base/java-tools/src/com/netscape/cmstools/pkcs11/PKCS11CertFindCLI.java
@@ -0,0 +1,98 @@
+// --- BEGIN COPYRIGHT BLOCK ---
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// (C) 2018 Red Hat, Inc.
+// All rights reserved.
+// --- END COPYRIGHT BLOCK ---
+
+package com.netscape.cmstools.pkcs11;
+
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.util.Enumeration;
+
+import org.apache.commons.cli.CommandLine;
+import org.dogtagpki.util.logging.PKILogger;
+import org.mozilla.jss.crypto.CryptoToken;
+import org.mozilla.jss.provider.java.security.JSSLoadStoreParameter;
+
+import com.netscape.cmstools.cli.CLI;
+import com.netscape.cmsutil.crypto.CryptoUtil;
+
+/**
+ * @author Endi S. Dewata
+ */
+public class PKCS11CertFindCLI extends CLI {
+
+    public PKCS11CertFindCLI(PKCS11CertCLI parent) {
+        super("find", "Find PKCS #11 certificates", parent);
+
+        createOptions();
+    }
+
+    public void printHelp() {
+        formatter.printHelp(getFullName() + " [OPTIONS...]", options);
+    }
+
+    public void createOptions() {
+        options.addOption("v", "verbose", false, "Run in verbose mode.");
+        options.addOption(null, "debug", false, "Run in debug mode.");
+        options.addOption(null, "help", false, "Show help message.");
+    }
+
+    public void execute(String[] args) throws Exception {
+
+        CommandLine cmd = parser.parse(options, args);
+
+        if (cmd.hasOption("help")) {
+            printHelp();
+            return;
+        }
+
+        if (cmd.hasOption("verbose")) {
+            PKILogger.setLevel(PKILogger.Level.INFO);
+
+        } else if (cmd.hasOption("debug")) {
+            PKILogger.setLevel(PKILogger.Level.DEBUG);
+        }
+
+        String tokenName = getConfig().getTokenName();
+        CryptoToken token = CryptoUtil.getKeyStorageToken(tokenName);
+
+        KeyStore ks = KeyStore.getInstance("pkcs11");
+        ks.load(new JSSLoadStoreParameter(token));
+
+        Enumeration<String> aliases = ks.aliases();
+
+        boolean first = true;
+
+        while (aliases.hasMoreElements()) {
+
+            String alias = aliases.nextElement();
+
+            Certificate cert = ks.getCertificate(alias);
+            if (cert == null) {
+                continue;
+            }
+
+            if (first) {
+                first = false;
+            } else {
+                System.out.println();
+            }
+
+            PKCS11CertCLI.printCertInfo(alias, cert);
+        }
+    }
+}
