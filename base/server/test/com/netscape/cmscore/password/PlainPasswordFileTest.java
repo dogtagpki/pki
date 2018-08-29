d527937fae4ec1c86a001a4316a87bb4ad488633
Dinesh Prasanth M K <dmoluguw@redhat.com>
2018-08-29 16:08:26 -0400

    Fixed jUnit Test Case
    - test matrix updated
    - helper methods added for readability
    
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- base/server/test/com/netscape/cmscore/password/PlainPasswordFileTest.java
+++ base/server/test/com/netscape/cmscore/password/PlainPasswordFileTest.java
@@ -39,44 +39,25 @@ public class PlainPasswordFileTest {
 
     PlainPasswordFile pwdFile;
     File createdFile;
+    private final int TESTCASE_ENTRY = 0;
+    private final int TESTCASE_KEY = 1;
+    private final int TESTCASE_VALUE = 2;
 
     // Successful cases
-    // Expected key-value pair
-    String[] refString1 = { "Truth", "Beauty" };
-    String[] testString1 = {
-            "Truth=Beauty", "Truth= Beauty", "Truth=Beauty ", "Truth= Beauty ",
-            "Truth =Beauty", "Truth = Beauty", "Truth =Beauty ", "Truth = Beauty ",
-            " Truth=Beauty", " Truth= Beauty", " Truth=Beauty ", " Truth= Beauty ",
-            " Truth =Beauty", " Truth = Beauty", " Truth =Beauty ", " Truth = Beauty "
-    };
-
-    String[] refString2 = { "Welcome Message", "Hello World" };
-    String[] testString2 = {
-            "Welcome Message=Hello World", "Welcome Message= Hello World",
-            "Welcome Message=Hello World ", "Welcome Message= Hello World ",
-
-            "Welcome Message =Hello World", "Welcome Message = Hello World",
-            "Welcome Message =Hello World ", "Welcome Message = Hello World ",
-
-            " Welcome Message=Hello World", " Welcome Message= Hello World",
-            " Welcome Message=Hello World ", " Welcome Message= Hello World ",
-
-            " Welcome Message =Hello World", " Welcome Message = Hello World",
-            " Welcome Message =Hello World ", " Welcome Message = Hello World "
-    };
-
-    String[] testString3 = { " \n", "# Ignored line 1", "   # Ignored line 2" };
-
-    // Negative cases
-    String[] testString4 = {
-            "hello",
-            "hello:world",
-            "hello world"
+    String[][] testCases = {
+            { "Truth=Beauty", "Truth", "Beauty" }, // No spaces
+            { " Truth = Beauty ", "Truth", "Beauty" }, // Check for spaces
+            { "Welcome Message=Hello World", "Welcome Message", "Hello World" }, // No spaces
+            { " Welcome Message = Hello World ", "Welcome Message", "Hello World" },
+            { "\n" }, // Empty line
+            { "# Ignored line 1" }, // Commented line
+            { "hello" },
+            { "hello:world" },
+            { "hello world" }
     };
 
     @Before
-    public void preTestSetup() throws IOException
-    {
+    public void preTestSetup() throws IOException {
         pwdFile = new PlainPasswordFile();
         createdFile = folder.newFile("tempPasswordFile");
     }
@@ -85,45 +66,70 @@ public class PlainPasswordFileTest {
     public TemporaryFolder folder = new TemporaryFolder();
 
     @Rule
-    public ExpectedException expectedException= ExpectedException.none();
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void testInitNormal() throws IOException {
-        writeToFileAndInit(createdFile, testString1);
-        // Since all all keys and values are equivalent, only 1 value should be loaded
-        Assert.assertEquals(1, pwdFile.getSize());
-        // Check whether the value is correctly stored and retrieved
-        Assert.assertEquals(refString1[1], pwdFile.getPassword(refString1[0], 0));
-
+        testHelper(0, false);
     }
 
     @Test
-    public void testInitWithSpace() throws IOException {
-        writeToFileAndInit(createdFile, testString2);
-        Assert.assertEquals(1, pwdFile.getSize());
-        Assert.assertEquals(refString2[1], pwdFile.getPassword(refString2[0], 0));
+    public void testInitNormalWithSpace() throws IOException {
+        testHelper(1, false);
+    }
+
+    @Test
+    public void testInitWithSpaceInside() throws IOException {
+        testHelper(2, false);
+    }
+
+    @Test
+    public void testInitWithSpaceInsideAndOutside() throws IOException {
+        testHelper(3, false);
+    }
+
+    @Test
+    public void testEmptyLine() throws IOException {
+        testHelper(4, true);
 
     }
 
     @Test
     public void testComments() throws IOException {
-        writeToFileAndInit(createdFile, testString3);
-        // There should be no values present since the file contains only comments
-        // and newline
-        Assert.assertEquals(0, pwdFile.getSize());
+        testHelper(5, true);
     }
 
     @Test
-    public void testException() throws IOException {
+    public void testNoValue() throws IOException {
         expectedException.expect(IOException.class);
-        writeToFileAndInit(createdFile, testString4);
+        testHelper(6, false);
     }
 
-    private void writeToFileAndInit(File file, String[] strArray) throws IOException {
-        for (String str : strArray) {
-            FileUtils.writeStringToFile(file, str + "\n", (Charset) null, true);
-        }
+    @Test
+    public void testWrongDelimiter() throws IOException {
+        expectedException.expect(IOException.class);
+        testHelper(7,  false);
+    }
+
+    @Test
+    public void testSpaceDelimiter() throws IOException {
+        expectedException.expect(IOException.class);
+        testHelper(8, false);
+    }
+
+    private void writeToFileAndInit(File file, String string) throws IOException {
+        FileUtils.writeStringToFile(file, string + "\n", (Charset) null, true);
         pwdFile.init(createdFile.getAbsolutePath());
+    }
+
+    private void testHelper(int testCaseId, boolean isEmpty) throws IOException {
+        writeToFileAndInit(createdFile, testCases[testCaseId][TESTCASE_ENTRY]);
+        if (isEmpty) {
+            Assert.assertEquals(0, pwdFile.getSize());
+        } else {
+            Assert.assertEquals(testCases[testCaseId][TESTCASE_VALUE],
+                    pwdFile.getPassword(testCases[testCaseId][TESTCASE_KEY], 0));
+        }
     }
 
 }
