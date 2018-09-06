bbe588e4f41a945c50483c5db34b77cb0aad7089
Dinesh Prasanth M K <dmoluguw@redhat.com>
2018-09-06 10:30:11 -0400

    Revert "Ticket 2960 and 3027"
    - This reverts commits:
    	fdda6408e6d5e7d350567902898a418a1136d5be
    	04ddc823762b5400f22409bbaceac1a8344708ca

--- base/server/cms/src/com/netscape/cms/profile/common/CACertCAEnrollProfile.java
+++ base/server/cms/src/com/netscape/cms/profile/common/CACertCAEnrollProfile.java
@@ -76,7 +76,7 @@ public class CACertCAEnrollProfile extends CAEnrollProfile
         IConfigStore defConfig4 = def4.getConfigStore();
         defConfig4.putString("params.signingAlg", "-");
         defConfig4.putString("params.signingAlgsAllowed",
-                "SHA1withRSA,SHA256withRSA,SHA512withRSA,SHA256withEC,SHA384withEC,SHA512withEC");
+                "SHA1withRSA,SHA256withRSA,SHA512withRSA,MD5withRSA,MD2withRSA,SHA256withEC,SHA384withEC,SHA512withEC");
 
         // extensions
         IProfilePolicy policy5 =
