2dcc2d5673bb154d11a8e764007e1c92a58369af
Endi S. Dewata <edewata@redhat.com>
2018-09-25 22:15:07 +0200

    Fixed pki-server tps-clone-prepare
    The pki-server tps-clone-prepare has been modified not to export
    'signing' certificate since TPS doesn't have such certificate.

--- base/server/python/pki/server/cli/tps.py
+++ base/server/python/pki/server/cli/tps.py
@@ -149,8 +149,6 @@ class TPSClonePrepareCLI(pki.cli.CLI):
             subsystem.export_system_cert(
                 'subsystem', pkcs12_file, pkcs12_password_file)
             subsystem.export_system_cert(
-                'signing', pkcs12_file, pkcs12_password_file, append=True)
-            subsystem.export_system_cert(
                 'audit_signing', pkcs12_file, pkcs12_password_file, append=True)
             instance.export_external_certs(
                 pkcs12_file, pkcs12_password_file, append=True)
