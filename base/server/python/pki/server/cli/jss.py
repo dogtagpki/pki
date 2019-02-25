ae70dad4f00f26bcc7675c4d25e15f936c5e4c34
Endi S. Dewata <edewata@redhat.com>
2019-02-25 18:54:29 +0100

    Updated pki-server jss-enable
    The pki-server jss-enable has been modified to install JSS
    libraries as well.

--- base/server/python/pki/server/cli/jss.py
+++ base/server/python/pki/server/cli/jss.py
@@ -100,40 +100,7 @@ class JSSInstallCLI(pki.cli.CLI):
             print("ERROR: Invalid instance: %s" % instance_name)
             sys.exit(1)
 
-        instance.symlink(
-            '/usr/share/java/commons-lang.jar',
-            os.path.join(instance.lib_dir, 'commons-lang.jar'),
-            force)
-
-        instance.symlink(
-            '/usr/share/java/commons-codec.jar',
-            os.path.join(instance.lib_dir, 'commons-codec.jar'),
-            force)
-
-        instance.symlink(
-            '/usr/share/java/slf4j/slf4j-api.jar',
-            os.path.join(instance.lib_dir, 'slf4j-api.jar'),
-            force)
-
-        instance.symlink(
-            '/usr/share/java/slf4j/slf4j-jdk14.jar',
-            os.path.join(instance.lib_dir, 'slf4j-jdk14.jar'),
-            force)
-
-        instance.symlink(
-            '/usr/share/java/jaxb-api.jar',
-            os.path.join(instance.lib_dir, 'jaxb-api.jar'),
-            force)
-
-        instance.symlink(
-            '/usr/lib/java/jss4.jar',
-            os.path.join(instance.lib_dir, 'jss4.jar'),
-            force)
-
-        instance.symlink(
-            '/usr/share/java/tomcatjss.jar',
-            os.path.join(instance.lib_dir, 'tomcatjss.jar'),
-            force)
+        instance.install_jss_lib(force=force)
 
 
 class JSSUninstallCLI(pki.cli.CLI):
@@ -195,13 +162,7 @@ class JSSUninstallCLI(pki.cli.CLI):
             print("ERROR: Invalid instance: %s" % instance_name)
             sys.exit(1)
 
-        pki.util.unlink(os.path.join(instance.lib_dir, 'tomcatjss.jar'), force)
-        pki.util.unlink(os.path.join(instance.lib_dir, 'jss4.jar'), force)
-        pki.util.unlink(os.path.join(instance.lib_dir, 'jaxb-api.jar'), force)
-        pki.util.unlink(os.path.join(instance.lib_dir, 'slf4j-jdk14.jar'), force)
-        pki.util.unlink(os.path.join(instance.lib_dir, 'slf4j-api.jar'), force)
-        pki.util.unlink(os.path.join(instance.lib_dir, 'commons-codec.jar'), force)
-        pki.util.unlink(os.path.join(instance.lib_dir, 'commons-lang.jar'), force)
+        instance.uninstall_jss_lib(force=force)
 
 
 class JSSEnableCLI(pki.cli.CLI):
@@ -214,6 +175,7 @@ class JSSEnableCLI(pki.cli.CLI):
         print('Usage: pki-server jss-enable [OPTIONS]')
         print()
         print('  -i, --instance <instance ID>       Instance ID (default: pki-tomcat).')
+        print('      --force                        Force enablement.')
         print('  -v, --verbose                      Run in verbose mode.')
         print('      --debug                        Run in debug mode.')
         print('      --help                         Show help message.')
@@ -224,6 +186,7 @@ class JSSEnableCLI(pki.cli.CLI):
         try:
             opts, _ = getopt.gnu_getopt(argv, 'i:v', [
                 'instance=',
+                'force',
                 'verbose', 'debug', 'help'])
 
         except getopt.GetoptError as e:
@@ -232,10 +195,14 @@ class JSSEnableCLI(pki.cli.CLI):
             sys.exit(1)
 
         instance_name = 'pki-tomcat'
+        force = False
 
         for o, a in opts:
             if o in ('-i', '--instance'):
                 instance_name = a
+
+            elif o == '--force':
+                force = True
 
             elif o in ('-v', '--verbose'):
                 logging.getLogger().setLevel(logging.INFO)
@@ -257,6 +224,8 @@ class JSSEnableCLI(pki.cli.CLI):
         if not instance.is_valid():
             print("ERROR: Invalid instance: %s" % instance_name)
             sys.exit(1)
+
+        instance.install_jss_lib(force=force)
 
         jss_config = instance.load_jss_config()
 
