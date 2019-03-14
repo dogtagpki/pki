6afa6e29c2d754bfec737b43b86b6b600d5c799d
Endi S. Dewata <edewata@redhat.com>
2019-03-14 16:54:37 -0500

    Updated loggers in SubjectInfoAccessExtDefault

--- base/server/cms/src/com/netscape/cms/profile/def/SubjectInfoAccessExtDefault.java
+++ base/server/cms/src/com/netscape/cms/profile/def/SubjectInfoAccessExtDefault.java
@@ -48,6 +48,8 @@ import com.netscape.cmscore.apps.CMSEngine;
  */
 public class SubjectInfoAccessExtDefault extends EnrollExtDefault {
 
+    public static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(SubjectInfoAccessExtDefault.class);
+
     public static final String CONFIG_CRITICAL = "subjInfoAccessCritical";
     public static final String CONFIG_NUM_ADS = "subjInfoAccessNumADs";
     public static final String CONFIG_AD_ENABLE = "subjInfoAccessADEnable_";
@@ -261,7 +263,7 @@ public class SubjectInfoAccessExtDefault extends EnrollExtDefault {
                             try {
                                 ext.addAccessDescription(new ObjectIdentifier(method), gn);
                             } catch (NumberFormatException ee) {
-                                CMS.debug("SubjectInfoAccessExtDefault: " + ee.toString());
+                                logger.error("SubjectInfoAccessExtDefault: " + ee.getMessage(), ee);
                                 throw new EPropertyException(CMS.getUserMessage(
                                         locale, "CMS_PROFILE_DEF_SIA_OID", method));
                             }
@@ -275,11 +277,11 @@ public class SubjectInfoAccessExtDefault extends EnrollExtDefault {
 
             replaceExtension(ext.getExtensionId().toString(), ext, info);
         } catch (IOException e) {
-            CMS.debug("SubjectInfoAccessExtDefault: " + e.toString());
+            logger.error("SubjectInfoAccessExtDefault: " + e.getMessage(), e);
             throw new EPropertyException(CMS.getUserMessage(
                         locale, "CMS_INVALID_PROPERTY", name));
         } catch (EProfileException e) {
-            CMS.debug("SubjectInfoAccessExtDefault: " + e.toString());
+            logger.error("SubjectInfoAccessExtDefault: " + e.getMessage(), e);
             throw new EPropertyException(CMS.getUserMessage(
                         locale, "CMS_INVALID_PROPERTY", name));
         }
@@ -306,7 +308,7 @@ public class SubjectInfoAccessExtDefault extends EnrollExtDefault {
                 populate(null, info);
 
             } catch (EProfileException e) {
-                CMS.debug("SubjectInfoAccessExtDefault: getValue " + e.toString());
+                logger.error("SubjectInfoAccessExtDefault: getValue " + e.getMessage(), e);
                 throw new EPropertyException(CMS.getUserMessage(
                         locale, "CMS_INVALID_PROPERTY", name));
             }
@@ -335,7 +337,7 @@ public class SubjectInfoAccessExtDefault extends EnrollExtDefault {
 
             int num = getNumAds();
 
-            CMS.debug("SubjectInfoAccess num=" + num);
+            logger.debug("SubjectInfoAccess num=" + num);
             Vector<NameValuePairs> recs = new Vector<NameValuePairs>();
 
             for (int i = 0; i < num; i++) {
@@ -415,7 +417,7 @@ public class SubjectInfoAccessExtDefault extends EnrollExtDefault {
             for (int i = 0; i < num; i++) {
                 String enable = getConfig(CONFIG_AD_ENABLE + i);
                 if (enable != null && enable.equals("true")) {
-                    CMS.debug("SubjectInfoAccess: createExtension i=" + i);
+                    logger.debug("SubjectInfoAccess: createExtension i=" + i);
                     String method = getConfig(CONFIG_AD_METHOD + i);
                     String locationType = getConfig(CONFIG_AD_LOCATIONTYPE + i);
                     if (locationType == null || locationType.length() == 0)
@@ -441,8 +443,7 @@ public class SubjectInfoAccessExtDefault extends EnrollExtDefault {
                 }
             }
         } catch (Exception e) {
-            CMS.debug("SubjectInfoAccessExtDefault: createExtension " +
-                    e.toString());
+            logger.warn("SubjectInfoAccessExtDefault: createExtension " + e.getMessage(), e);
         }
 
         return ext;
