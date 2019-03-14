a1681007d88eb2cbd8dbd43d7c2fd2146fb1d313
Fraser Tweedale <ftweedal@redhat.com>
2019-03-15 10:27:08 +1100

    PKIInstance: add store_passwords method
    Manual partial backport of 768e5bc05b92e4b773c6184a2357ea29079b5772,
    which is needed by cert-fix.
    
    Part of: https://pagure.io/dogtagpki/issue/2776

--- base/server/python/pki/server/__init__.py
+++ base/server/python/pki/server/__init__.py
@@ -1272,11 +1272,7 @@ class PKIInstance(object):
                     self.group = m.group(1)
                     self.gid = grp.getgrnam(self.group).gr_gid
 
-        # load passwords
-        self.passwords.clear()
-        if os.path.exists(self.password_conf):
-            logger.info('Loading password config: %s', self.password_conf)
-            pki.util.load_properties(self.password_conf, self.passwords)
+        self.load_passwords()
 
         self.load_external_certs(self.external_certs_conf)
 
@@ -1447,6 +1443,19 @@ class PKIInstance(object):
 
             finally:
                 shutil.rmtree(tmpdir)
+
+    def load_passwords(self):
+
+        self.passwords.clear()
+
+        if os.path.exists(self.password_conf):
+            logger.info('Loading password config: %s', self.password_conf)
+            pki.util.load_properties(self.password_conf, self.passwords)
+
+    def store_passwords(self):
+
+        pki.util.store_properties(self.password_conf, self.passwords)
+        pki.util.chown(self.password_conf, self.uid, self.gid)
 
     def get_server_config(self):
         server_config = ServerConfiguration(self.server_xml)
