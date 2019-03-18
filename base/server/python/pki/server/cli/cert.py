12d2cdbd3950610db65b33dd2118ed438a3efa8b
Fraser Tweedale <ftweedal@redhat.com>
2019-03-18 14:50:37 +1100

    cert-fix: target CA subsystem when extra-certs specified
    If _only_ specifying --extra-certs, no subsystems are targeted and
    Dogtag database configuration changes are not applied.  Explicitly
    target the CA subsystem in this scenario.
    
    Part of: https://pagure.io/dogtagpki/issue/2776

--- base/server/python/pki/server/cli/cert.py
+++ base/server/python/pki/server/cli/cert.py
@@ -1145,6 +1145,9 @@ class CertFixCLI(pki.cli.CLI):
 
                     target_subsys.add(subsystem)
 
+            if len(extra_certs) > 0:
+                target_subsys.add(ca_subsystem)
+
             # Generate new password for agent account
             agent_pass = gen_random_password()
 
