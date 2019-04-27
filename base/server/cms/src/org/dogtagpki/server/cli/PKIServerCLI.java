98719cbcd9819388650b226794adc7b6554eb7f5
Endi S. Dewata <edewata@redhat.com>
2019-04-26 19:46:31 -0500

    Replaced PKIServer with PKIServerCLI
    The PKIServer class has been replaced with PKIServerCLI for
    running Java-based pki-server commands.

---
+++ base/server/cms/src/org/dogtagpki/server/cli/PKIServerCLI.java
@@ -0,0 +1,90 @@
+// --- BEGIN COPYRIGHT BLOCK ---
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// (C) 2019 Red Hat, Inc.
+// All rights reserved.
+// --- END COPYRIGHT BLOCK ---
+
+package org.dogtagpki.server.cli;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.dogtagpki.cli.CLI;
+import org.dogtagpki.util.logging.PKILogger;
+import org.dogtagpki.util.logging.PKILogger.Level;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PKIServerCLI extends CLI {
+
+    public static Logger logger = LoggerFactory.getLogger(PKIServerCLI.class);
+
+    public PKIServerCLI() throws Exception {
+        super("pki-server", "PKI server management commands");
+
+        createOptions();
+    }
+
+    public String getFullModuleName(String moduleName) {
+        return moduleName;
+    }
+
+    public void createOptions() throws Exception {
+
+        Option option = new Option("d", true, "NSS database location");
+        option.setArgName("database");
+        options.addOption(option);
+
+        option = new Option("f", true, "NSS database password configuration");
+        option.setArgName("password config");
+        options.addOption(option);
+
+        options.addOption("v", "verbose", false, "Run in verbose mode.");
+        options.addOption(null, "debug", false, "Run in debug mode.");
+        options.addOption(null, "help", false, "Show help message.");
+    }
+
+    public void printHelp() {
+
+        formatter.printHelp(name + " [OPTIONS..] <command> [ARGS..]", options);
+        System.out.println();
+
+        super.printHelp();
+    }
+
+    public void execute(String[] args) throws Exception {
+
+        CommandLine cmd = parser.parse(options, args, true);
+
+        if (cmd.hasOption("verbose")) {
+            PKILogger.setLevel(Level.INFO);
+            verbose = true;
+
+        } else if (cmd.hasOption("debug")) {
+            PKILogger.setLevel(PKILogger.Level.DEBUG);
+            verbose = true;
+        }
+
+        String[] cmdArgs = cmd.getArgs();
+        logger.info("Command: " + String.join(" ", cmdArgs));
+
+        super.execute(cmdArgs);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        PKIServerCLI cli = new PKIServerCLI();
+        cli.execute(args);
+    }
+}
