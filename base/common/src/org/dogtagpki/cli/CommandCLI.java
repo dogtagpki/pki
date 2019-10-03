36c0bd4839af0299a87d8c595cd5e1c82eec1482
Endi S. Dewata <edewata@redhat.com>
2019-10-02 20:53:50 -0500

    Added CommandCLI
    The CommandCLI has been added as a base class for all commands.

---
+++ base/common/src/org/dogtagpki/cli/CommandCLI.java
@@ -0,0 +1,62 @@
+// --- BEGIN COPYRIGHT BLOCK ---
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// (C) 2019 Red Hat, Inc.
+// All rights reserved.
+// --- END COPYRIGHT BLOCK ---
+
+package org.dogtagpki.cli;
+
+import org.apache.commons.cli.CommandLine;
+import org.dogtagpki.util.logging.PKILogger;
+import org.dogtagpki.util.logging.PKILogger.Level;
+
+/**
+ * @author Endi S. Dewata
+ */
+public class CommandCLI extends CLI {
+
+    public static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CommandCLI.class);
+
+    public CommandCLI(String name, String description, CLI parent) {
+        super(name, description, parent);
+
+        createOptions();
+    }
+
+    public void createOptions() {
+    }
+
+    public void execute(String[] args) throws Exception {
+
+        CommandLine cmd = parser.parse(options, args);
+
+        if (cmd.hasOption("help")) {
+            printHelp();
+            return;
+        }
+
+        if (cmd.hasOption("debug")) {
+            PKILogger.setLevel(PKILogger.Level.DEBUG);
+
+        } else if (cmd.hasOption("verbose")) {
+            PKILogger.setLevel(Level.INFO);
+        }
+
+        execute(cmd);
+    }
+
+    public void execute(CommandLine cmd) throws Exception {
+    }
+}
