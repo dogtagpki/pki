c10c00382b7407a99c4fc0c4480fb1cc9e9f3413
Endi S. Dewata <edewata@redhat.com>
2019-10-14 07:30:42 -0500

    Added upgrade script to remove pki.policy

---
+++ base/server/upgrade/10.8.0/05-RemovePKIPolicy.py
@@ -0,0 +1,38 @@
+# Authors:
+#     Endi S. Dewata <edewata@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright (C) 2019 Red Hat, Inc.
+# All rights reserved.
+
+from __future__ import absolute_import
+import os
+
+import pki
+
+
+class RemovePKIPolicy(pki.server.upgrade.PKIServerUpgradeScriptlet):
+
+    def __init__(self):
+        super(RemovePKIPolicy, self).__init__()
+        self.message = 'Remove pki.policy from instance folder'
+
+    def upgrade_instance(self, instance):
+
+        pki_policy = os.path.join(instance.conf_dir, 'pki.policy')
+
+        if os.path.exists(pki_policy):
+            self.backup(pki_policy)
+            pki.util.remove(pki_policy)
