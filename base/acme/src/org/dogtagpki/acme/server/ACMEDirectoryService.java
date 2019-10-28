2b58fabcfd15fa9122a3bc3f5e703c83df34df18
Endi S. Dewata <edewata@redhat.com>
2019-10-28 09:43:50 -0500

    Added ACMEDirectoryService
    The ACMEDirectoryService has been added to list the services
    provided by the ACME application.

---
+++ base/acme/src/org/dogtagpki/acme/server/ACMEDirectoryService.java
@@ -0,0 +1,48 @@
+//
+// Copyright Red Hat, Inc.
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+package org.dogtagpki.acme.server;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import org.dogtagpki.acme.ACMEDirectory;
+
+/**
+ * @author Endi S. Dewata
+ */
+@Path("directory")
+public class ACMEDirectoryService {
+
+    public static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ACMEDirectoryService.class);
+
+    @Context
+    UriInfo uriInfo;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getDirectory() throws Exception {
+
+        logger.info("Creating directory");
+
+        ACMEDirectory directory = new ACMEDirectory();
+
+        ACMEEngine engine = ACMEEngine.getInstance();
+        directory.setMetadata(engine.getMetadata());
+
+        logger.info("Directory:\n" + directory);
+
+        ResponseBuilder builder = Response.ok();
+        builder.entity(directory);
+
+        return builder.build();
+    }
+}
