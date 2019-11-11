91cac97f72d61c7ffcc2326176dc092a7b3708b5
Endi S. Dewata <edewata@redhat.com>
2019-11-11 11:00:47 -0600

    Added ACMEOrderService
    The ACMEOrderService has been added to return the requested
    order object.

--- base/acme/src/org/dogtagpki/acme/server/ACMEApplication.java
+++ base/acme/src/org/dogtagpki/acme/server/ACMEApplication.java
@@ -33,6 +33,7 @@ public class ACMEApplication extends Application {
         classes.add(ACMEAuthorizationService.class);
         classes.add(ACMEChallengeService.class);
         classes.add(ACMEFinalizeOrderService.class);
+        classes.add(ACMEOrderService.class);
     }
 
     public Set<Class<?>> getClasses() {
---
+++ base/acme/src/org/dogtagpki/acme/server/ACMEOrderService.java
@@ -0,0 +1,72 @@
+//
+// Copyright Red Hat, Inc.
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+package org.dogtagpki.acme.server;
+
+import java.net.URI;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import org.dogtagpki.acme.ACMEAccount;
+import org.dogtagpki.acme.ACMEHeader;
+import org.dogtagpki.acme.ACMENonce;
+import org.dogtagpki.acme.ACMEOrder;
+import org.dogtagpki.acme.JWS;
+
+/**
+ * @author Endi S. Dewata
+ */
+@Path("order/{id}")
+public class ACMEOrderService {
+
+    public static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ACMEOrderService.class);
+
+    @Context
+    UriInfo uriInfo;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response handlePOST(@PathParam("id") String orderID, JWS jws) throws Exception {
+
+        logger.info("Retrieving order " + orderID);
+
+        String protectedHeader = new String(jws.getProtectedHeaderAsBytes(), "UTF-8");
+        logger.info("Header: " + protectedHeader);
+        ACMEHeader header = ACMEHeader.fromJSON(protectedHeader);
+
+        ACMEEngine engine = ACMEEngine.getInstance();
+        engine.validateNonce(header.getNonce());
+
+        URI kid = header.getKid();
+        String kidPath = kid.getPath();
+        String accountID = kidPath.substring(kidPath.lastIndexOf('/') + 1);
+        logger.info("Account ID: " + accountID);
+
+        ACMEAccount account = engine.getAccount(accountID);
+        engine.validateJWS(jws, header.getAlg(), account.getJWK());
+
+        ACMEOrder order = engine.getOrder(account, orderID);
+
+        ResponseBuilder builder = Response.ok();
+
+        ACMENonce nonce = engine.createNonce();
+        builder.header("Replay-Nonce", nonce.getValue());
+
+        URI index = uriInfo.getBaseUriBuilder().path("directory").build();
+        builder.link(index, "index");
+
+        builder.entity(order);
+
+        return builder.build();
+    }
+}
