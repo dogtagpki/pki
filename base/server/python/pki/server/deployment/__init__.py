dogtagpki/pki

commit 08ea62892a894553d8ceae200618c6fa8d7f0585

Refactored security domain methods in PKIConfigParser
The security domain methods in PKIConfigParser have been
moved into PKIDeployer class.

base/server/python/pki/server/deployment/__init__.py
@@ -27,6 +27,10 @@ import subprocess
 import time
 from time import strftime as date
 
+import pki.account
+import pki.client
+import pki.system
+
 from . import pkiconfig as config
 from . import pkihelper as util
 from . import pkimanifest as manifest
@@ -94,6 +98,7 @@ class PKIDeployer:
             self.dns_domainname = self.hostname
 
         self.ds_connection = None
+        self.sd_connection = None
 
     def set_property(self, key, value, section=None):
 
@@ -214,3 +219,37 @@ class PKIDeployer:
 
     def ds_close(self):
         self.ds_connection.unbind_s()
+
+    def sd_connect(self):
+
+        sd_url = self.mdict['pki_security_domain_uri']
+        sd_hostname = self.mdict['pki_security_domain_hostname']
+        sd_port = self.mdict['pki_security_domain_https_port']
+
+        logger.info('Connecting to security domain at %s', sd_url)
+
+        self.sd_connection = pki.client.PKIConnection(
+            protocol='https',
+            hostname=sd_hostname,
+            port=sd_port,
+            trust_env=False)
+
+        sd_user = self.mdict['pki_security_domain_user']
+        sd_password = self.mdict['pki_security_domain_password']
+
+        self.sd_connection.authenticate(sd_user, sd_password)
+
+    def get_domain_info(self):
+
+        logger.info('Getting security domain info')
+
+        sd_client = pki.system.SecurityDomainClient(self.sd_connection)
+        return sd_client.get_security_domain_info()
+
+    def sd_login(self):
+        account = pki.account.AccountClient(self.sd_connection, subsystem='ca')
+        account.login()
+
+    def sd_logout(self):
+        account = pki.account.AccountClient(self.sd_connection, subsystem='ca')
+        account.logout()

base/server/python/pki/server/deployment/pkiparser.py
@@ -27,7 +27,6 @@ import ldap
 import logging
 import os
 import random
-import requests.exceptions
 import string
 import xml.etree.ElementTree as ET
 
@@ -185,7 +184,6 @@ class PKIConfigParser:
             help='display verbose information (details below)')
 
         self.indent = 0
-        self.sd_connection = None
         self.authdb_connection = None
 
         # Master and Slot dictionaries
@@ -506,39 +504,6 @@ class PKIConfigParser:
                 message = '%s Use \'%s\' instead.' % (message, new_param)
 
             print('WARNING: %s' % message)
-
-    def sd_connect(self):
-        self.sd_connection = pki.client.PKIConnection(
-            protocol='https',
-            hostname=self.mdict['pki_security_domain_hostname'],
-            port=self.mdict['pki_security_domain_https_port'],
-            trust_env=False)
-
-    def sd_get_info(self):
-        sd = pki.system.SecurityDomainClient(self.sd_connection)
-        try:
-            info = sd.get_security_domain_info()
-        except requests.exceptions.HTTPError as e:
-            logger.warning('Unable to get security domain info: %s', e)
-            logger.info('Trying older interface.')
-            info = sd.get_old_security_domain_info()
-        return info
-
-    def sd_authenticate(self):
-        self.sd_connection.authenticate(
-            self.mdict['pki_security_domain_user'],
-            self.mdict['pki_security_domain_password'])
-
-        account = pki.account.AccountClient(self.sd_connection, subsystem='ca')
-        try:
-            account.login()
-            account.logout()
-        except requests.exceptions.HTTPError as e:
-            code = e.response.status_code
-            if code == 404 or code == 501:
-                logger.warning('Unable to authenticate against security domain: %s', e)
-            else:
-                raise
 
     def authdb_connect(self):
 

base/server/python/pki/server/pkispawn.py
@@ -381,8 +381,8 @@ def main(argv):
                                      'pki_security_domain_https_port')
 
                     try:
-                        parser.sd_connect()
-                        info = parser.sd_get_info()
+                        deployer.sd_connect()
+                        info = deployer.get_domain_info()
                         parser.print_text('Name: ' + info.name)
                         deployer.set_property('pki_security_domain_name', info.name)
                         break
@@ -398,7 +398,8 @@ def main(argv):
                                          'pki_security_domain_password')
 
                     try:
-                        parser.sd_authenticate()
+                        deployer.sd_login()
+                        deployer.sd_logout()
                         break
                     except requests.exceptions.HTTPError as e:
                         parser.print_text('ERROR: ' + str(e))
@@ -716,10 +717,11 @@ def check_security_domain(parser):
                 sys.exit(1)
 
             if not config.str2bool(parser.mdict['pki_skip_sd_verify']):
-                parser.sd_connect()
-                info = parser.sd_get_info()
+                deployer.sd_connect()
+                info = deployer.get_domain_info()
                 deployer.set_property('pki_security_domain_name', info.name)
-                parser.sd_authenticate()
+                deployer.sd_login()
+                deployer.sd_logout()
 
         except requests.exceptions.RequestException as e:
             print(('ERROR:  Unable to access security domain: ' + str(e)))

commit 590232433ca8590213cfc1487eee8654e109f31e

Fix interactive installation for subsystems other than CA
When doing an interactive installation, the pkispawn script tries
to connect to Security domain via `sd_connect` and attaches user
credentials. At this point the user has not been prompted for any
credentials. So, the authentication happens with empty strings. As
a result the interactive installation fails.

This was not observed in non-interactive installation because all the info
is provided via cfg file and is available in the dictionary at the time
of execution.

This patch moves the authentication logic from `sd_connect()`
to `sd_login()` (ie) authenticate before trying to log in

The bug was introduced in commit: 08ea62892a894553d8ceae200618c6fa8d7f0585

Resolves: BZ#1795215

Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

base/server/python/pki/server/deployment/__init__.py
@@ -253,11 +253,6 @@ class PKIDeployer:
             port=sd_port,
             trust_env=False)
 
-        sd_user = self.mdict['pki_security_domain_user']
-        sd_password = self.mdict['pki_security_domain_password']
-
-        self.sd_connection.authenticate(sd_user, sd_password)
-
         return self.sd_connection
 
     def get_domain_info(self):
@@ -275,6 +270,12 @@ class PKIDeployer:
         return self.domain_info
 
     def sd_login(self):
+
+        sd_user = self.mdict['pki_security_domain_user']
+        sd_password = self.mdict['pki_security_domain_password']
+
+        self.sd_connection.authenticate(sd_user, sd_password)
+
         account = pki.account.AccountClient(self.sd_connection, subsystem='ca')
         account.login()
 