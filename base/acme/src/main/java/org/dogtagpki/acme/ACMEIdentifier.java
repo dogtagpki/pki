05431ba1aab83a2509abd183d70afdd109b7346f
Endi S. Dewata <edewata@redhat.com>
2020-03-17 11:44:49 -0500

    Added revocation with authorization records
    The ACMEEngine.validateRevocation() has been modified to support
    validating revocation request using the account's authorization
    records.

--- base/acme/src/main/java/org/dogtagpki/acme/ACMEIdentifier.java
+++ base/acme/src/main/java/org/dogtagpki/acme/ACMEIdentifier.java
@@ -44,6 +44,37 @@ public class ACMEIdentifier {
         this.value = value;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ACMEIdentifier other = (ACMEIdentifier) obj;
+        if (type == null) {
+            if (other.type != null)
+                return false;
+        } else if (!type.equals(other.type))
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+
     public String toJSON() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(this);
