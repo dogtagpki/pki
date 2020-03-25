6cd2ce6ab81798854b0dd44a807cc7c041d49305
Dinesh Prasanth M K <SilleBille@users.noreply.github.com>
2020-03-25 16:02:15 -0400

    Patch ECAdminCertProfile upgrade script (#355)
    * Patch ECAdminCertProfile upgrade script
    
    The caECAdminCert profile was added 2 years ago but was never patched
    to be added to the CS.cfg. Hence, when a user tries to upgrade, the path
    did not exist and so, the upgrade failed. This patch adds the missing
    attribute to ensure smooth upgradation process
    
    Resolves: BZ#1814242
    Upstream: https://pagure.io/dogtagpki/issue/3168
    
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>
    
    * Addressing comments in PR
    
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- base/server/upgrade/10.8.2/01-FixECAdminCertProfile.py
+++ base/server/upgrade/10.8.2/01-FixECAdminCertProfile.py
@@ -28,12 +28,28 @@ class FixECAdminCertProfile(pki.server.upgrade.PKIServerUpgradeScriptlet):
         self.backup(subsystem.cs_conf)
 
         path = subsystem.config.get('profile.caECAdminCert.config')
-        logger.info('Current path: %s', path)
+        if path is None:
+            # Add missing path
+            logger.info('Missing profile.caECAdminCert.config')
 
-        dirname = os.path.dirname(path)
+            path = "{0}/profiles/{1}/caECAdminCert.cfg".format(
+                subsystem.base_dir, subsystem.name)
 
-        path = os.path.join(dirname, 'caECAdminCert.cfg')
+        else:
+            # Fix existing path
+            logger.info("Fixing profile.caECAdminCert.config")
+            dirname = os.path.dirname(path)
+            path = os.path.join(dirname, 'caECAdminCert.cfg')
+
         logger.info('New path: %s', path)
-
         subsystem.config['profile.caECAdminCert.config'] = path
+
+        subsystem.config['profile.caECAdminCert.class_id'] = 'caEnrollImpl'
+
+        # check if caECAdminCert is part of profile.list
+        profile_list = subsystem.config['profile.list'].split(',')
+        if 'caECAdminCert' not in profile_list:
+            profile_list.append('caECAdminCert')
+            subsystem.config['profile.list'] = ','.join(profile_list)
+
         subsystem.save()
