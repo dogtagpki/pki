c40e78344b199e4f9c537c5c175c68eacd4979db
Endi S. Dewata <edewata@redhat.com>
2020-04-01 14:41:41 -0500

    Added handlers to capture CI install logs

--- travis/tps-create.sh
+++ travis/tps-create.sh
@@ -1,3 +1,36 @@
 #!/bin/bash -ex
 
+set -o pipefail
+
+exit_handler() {
+
+    if [ $? -eq 0 ]
+    then
+        return
+    fi
+
+    # display logs for quick troubleshooting
+
+    for DEBUG_LOG in `ls /var/log/pki/pkitest/tps/debug.*`
+    do
+        tail -n 1000 $DEBUG_LOG
+    done
+
+    SYSTEMD_LOG=/tmp/pkitest-systemd.log
+    journalctl -u pki-tomcatd@pkitest.service --no-pager > $SYSTEMD_LOG
+    tail -n 1000 $SYSTEMD_LOG
+
+    # upload logs for further investigation
+
+    for DEBUG_LOG in `ls /var/log/pki/pkitest/tps/debug.*`
+    do
+        filename=`basename $DEBUG_LOG`
+        curl -k -w "\n" --upload-file $DEBUG_LOG https://transfer.sh/$filename
+    done
+
+    filename=`basename $SYSTEMD_LOG`
+    curl -k -w "\n" --upload-file $SYSTEMD_LOG https://transfer.sh/$filename
+}
+
+trap "exit_handler" EXIT
 pkispawn -v -f ${BUILDDIR}/pki/travis/pki.cfg -s TPS
