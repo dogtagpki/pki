6deb112dbf8ec3901c47712f42887c9f37b6bb03
Alexander Scheel <ascheel@redhat.com>
2020-04-02 12:48:27 -0400

    Temporarily disable PKI COPR repo for SSLEngine
    Having a SSLContext implementation is confusing Tomcat, resulting in PKI
    CI failing. Temporarily disable PKI's COPR repo, preventing a newer JSS
    from being pulled in.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- travis/pki-init.sh
+++ travis/pki-init.sh
@@ -42,7 +42,7 @@ then
 fi
 
 # Enable pki related COPR repo
-dnf copr enable -y ${COPR_REPO}
+# dnf copr enable -y ${COPR_REPO}
 
 # update, container might be outdated
 dnf update -y --best --allowerasing
