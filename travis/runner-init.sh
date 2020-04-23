3544aef734ad1bba7e437f7ae5b0272b39275b11
Dinesh Prasanth M K <dmoluguw@redhat.com>
2020-04-23 15:58:53 -0400

    Clean up GH actions code
    Update self executed container to ensure
    we run with latest set of packages
    
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- travis/runner-init.sh
+++ travis/runner-init.sh
@@ -38,3 +38,11 @@ docker run \
 
 # Check whether the container is up
 docker ps -a
+
+# Pause 5 seconds to let the container start up.
+# The container uses /usr/sbin/init as its entrypoint which requires few seconds.
+# to startup. This avoids the following error:
+# [Errno 2] No such file or directory: '/var/cache/dnf/metadata_lock.pid'
+sleep 5
+
+docker exec -i ${CONTAINER} dnf update -y
