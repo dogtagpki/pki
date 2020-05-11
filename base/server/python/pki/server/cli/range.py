999610002b2381155e75998cdca364fce78448c8
Endi S. Dewata <edewata@redhat.com>
2020-05-11 10:58:05 -0500

    Added pki-server ca/kra-range-update
    The pki-server ca/kra-range-update has been added to update
    the request ID, serial number, and replica ID ranges.

--- base/server/python/pki/server/cli/range.py
+++ base/server/python/pki/server/cli/range.py
@@ -23,6 +23,7 @@ class RangeCLI(pki.cli.CLI):
 
         self.parent = parent
         self.add_module(RangeShowCLI(self))
+        self.add_module(RangeUpdateCLI(self))
 
 
 class RangeShowCLI(pki.cli.CLI):
@@ -101,3 +102,88 @@ class RangeShowCLI(pki.cli.CLI):
 
         print('  Enable serial management: %s' %
               subsystem.config.get('dbs.enableSerialManagement'))
+
+
+class RangeUpdateCLI(pki.cli.CLI):
+
+    def __init__(self, parent):
+        super(RangeUpdateCLI, self).__init__(
+            'update',
+            'Update %s range configuration' % parent.parent.name.upper())
+
+        self.parent = parent
+
+    def print_help(self):
+        print('Usage: pki-server %s-range-update [OPTIONS]' % self.parent.parent.name)
+        print()
+        print('  -i, --instance <instance ID>       Instance ID (default: pki-tomcat).')
+        print('      --master <URL>                 Master URL.')
+        print('      --session <ID>                 Session ID.')
+        print('  -v, --verbose                      Run in verbose mode.')
+        print('      --debug                        Run in debug mode.')
+        print('      --help                         Show help message.')
+        print()
+
+    def execute(self, argv):
+        try:
+            opts, _ = getopt.gnu_getopt(argv, 'i:v', [
+                'instance=',
+                'master=', 'session=',
+                'verbose', 'debug', 'help'])
+
+        except getopt.GetoptError as e:
+            logger.error(e)
+            self.print_help()
+            sys.exit(1)
+
+        instance_name = 'pki-tomcat'
+        subsystem_name = self.parent.parent.name
+        master_url = None
+        session_id = None
+
+        for o, a in opts:
+            if o in ('-i', '--instance'):
+                instance_name = a
+
+            elif o == '--master':
+                master_url = a
+
+            elif o == '--session':
+                session_id = a
+
+            elif o in ('-v', '--verbose'):
+                logging.getLogger().setLevel(logging.INFO)
+
+            elif o == '--debug':
+                logging.getLogger().setLevel(logging.DEBUG)
+
+            elif o == '--help':
+                self.print_help()
+                sys.exit()
+
+            else:
+                logger.error('Invalid option: %s', o)
+                self.print_help()
+                sys.exit(1)
+
+        if not master_url:
+            raise Exception('Missing master URL')
+
+        if not session_id:
+            raise Exception('Missing session ID')
+
+        instance = pki.server.instance.PKIInstance(instance_name)
+        if not instance.exists():
+            logger.error('Invalid instance: %s', instance_name)
+            sys.exit(1)
+
+        instance.load()
+
+        subsystem = instance.get_subsystem(subsystem_name)
+
+        if not subsystem:
+            logger.error('No %s subsystem in instance %s',
+                         subsystem_name.upper(), instance_name)
+            sys.exit(1)
+
+        subsystem.update_ranges(master_url, session_id)
