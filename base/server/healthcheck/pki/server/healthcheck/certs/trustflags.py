f80a04f000c54d2b8743ed8252966a7fa2317004
Dinesh Prasanth M K <dmoluguw@redhat.com>
2020-06-04 15:36:06 -0400

    Add new healthcheck for TKS system cert trust flags
    This patch adds a new healthcheck to check the trust
    flag of TKS's System certs with known good value
    
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- base/server/healthcheck/pki/server/healthcheck/certs/trustflags.py
+++ base/server/healthcheck/pki/server/healthcheck/certs/trustflags.py
@@ -209,6 +209,68 @@ class OCSPSystemCertTrustFlagCheck(CertsPlugin):
                              nickname=cert['nickname'])
 
 
+@registry
+class TKSSystemCertTrustFlagCheck(CertsPlugin):
+    """
+    Compare the NSS trust for the TKS certs to a known good value
+    """
+    @duration
+    def check(self):
+
+        if not self.instance.exists():
+            logger.debug('Invalid instance: %s', self.instance.name)
+            yield Result(self, constants.CRITICAL,
+                         msg='Invalid PKI instance: %s' % self.instance.name)
+            return
+
+        self.instance.load()
+
+        # Make a list of known good trust flags for ALL system certs
+        expected_trust = {
+            'sslserver': 'u,u,u',
+            'subsystem': 'u,u,u',
+            'audit_signing': 'u,u,Pu'
+        }
+
+        tks = self.instance.get_subsystem('tks')
+
+        if not tks:
+            logger.debug("No TKS configured, skipping TKS System Cert Trust Flag check")
+            return
+
+        certs = tks.find_system_certs()
+
+        # Iterate on TKS's all system certificate to check with list of expected trust flags
+        for cert in certs:
+            cert_id = cert['id']
+
+            # Load cert trust from NSSDB
+            with nssdb_connection(self.instance) as nssdb:
+                try:
+                    cert_trust = nssdb.get_trust(
+                        nickname=cert['nickname'],
+                        token=cert['token']
+                    )
+                except Exception as e:  # pylint: disable=broad-except
+                    logger.debug('Unable to load cert from NSSDB: %s', str(e))
+                    yield Result(self, constants.ERROR,
+                                 key=cert_id,
+                                 nssdbDir=self.instance.nssdb_dir,
+                                 msg='Unable to load cert from NSSDB: %s' % str(e))
+                    continue
+            if cert_trust != expected_trust[cert_id]:
+                yield Result(self, constants.ERROR,
+                             cert_id=cert_id,
+                             nickname=cert['nickname'],
+                             token=cert['token'],
+                             cert_trust=cert_trust,
+                             msg='Incorrect NSS trust for %s. Got %s expected %s'
+                                 % (cert['nickname'], cert_trust, expected_trust[cert_id]))
+            else:
+                yield Result(self, constants.SUCCESS,
+                             cert_id=cert_id,
+                             nickname=cert['nickname'])
+
 @contextmanager
 def nssdb_connection(instance):
     """Open a connection to nssdb containing System Certificates"""
