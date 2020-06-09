76820004f231bcc4291cb63699a89746bd78c8e7
Alexander Scheel <ascheel@redhat.com>
2020-06-09 12:21:24 -0400

    Introduce pki_ajp_secret configuration parameter
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- base/server/python/pki/server/deployment/pkiparser.py
+++ base/server/python/pki/server/deployment/pkiparser.py
@@ -713,6 +713,8 @@ class PKIConfigParser:
                 self.mdict['pki_ajp_port']
             self.mdict['PKI_AJP_HOST_SLOT'] = \
                 self.mdict['pki_ajp_host']
+            self.mdict['PKI_AJP_SECRET_SLOT'] = \
+                self.mdict['pki_ajp_secret']
             self.mdict['PKI_AJP_REDIRECT_PORT_SLOT'] = \
                 self.mdict['pki_https_port']
             self.mdict['PKI_CA_HOSTNAME_SLOT'] = \
