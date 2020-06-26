93732b527a9083987f6fd16960e42a02433ee7f3
Dinesh Prasanth M K <dmoluguw@redhat.com>
2020-06-25 21:06:40 -0400

    Healthcheck: Add method to load dogtag specific config values
    This patch adds a reusable method to load dogtag specific values
    specified in the config file. Note that each registry calls this
    method but, the values are read only once. The registry initialization
    is handled by the underlying 'pkg_resources' library and there was no
    particular order.
    
    TODO: This is a temporary patch and the parsing method should be
    moved into the ipa-healthcheck-core library
    
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- base/server/healthcheck/pki/server/healthcheck/certs/plugin.py
+++ base/server/healthcheck/pki/server/healthcheck/certs/plugin.py
@@ -9,6 +9,8 @@
 from ipahealthcheck.core.plugin import Plugin, Registry
 from pki.server.instance import PKIInstance
 
+from pki.server.healthcheck.core.main import merge_dogtag_config
+
 import logging
 
 # Temporary workaround to skip VERBOSE data. Fix already pushed to upstream
@@ -26,6 +28,11 @@ class CertsPlugin(Plugin):
 
 class CertsRegistry(Registry):
     def initialize(self, framework, config):
+        # Read dogtag specific config values and merge with already existing config
+        # before adding it to registry
+        merge_dogtag_config(config)
+
+        super(CertsRegistry, self).initialize(framework, config)
         pass
 
 
--- base/server/healthcheck/pki/server/healthcheck/core/main.py
+++ base/server/healthcheck/pki/server/healthcheck/core/main.py
@@ -11,12 +11,61 @@ import logging
 import sys
 
 from ipahealthcheck.core.core import RunChecks
+from configparser import ConfigParser, ParsingError
 
 logging.basicConfig(format='%(message)s')
 logger = logging.getLogger()
 
+DOGTAG_CONFIG_FILE = "/etc/pki/healthcheck.conf"
+DOGTAG_CONFIG_SECTION = "dogtag"
+DOGTAG_DEFAULT_INSTANCE = "pki-tomcat"
+
+dogtag_config_parsed = False
+
 
 def main():
     checks = RunChecks(['pkihealthcheck.registry'],
-                       '/etc/pki/healthcheck.conf')
+                       DOGTAG_CONFIG_FILE)
     sys.exit(checks.run_healthcheck())
+
+
+def merge_dogtag_config(config):
+    # TODO: Move this method into ipa-healthcheck-core library
+    """
+    Merge "dogtag" specific config values into the provided `config` object.
+    If "dogtag" section is missing in config, appends the default instance
+    name "pki-tomcat"
+
+    :param config: config object containing prefilled values
+    :return: None
+    """
+    global dogtag_config_parsed
+
+    if not dogtag_config_parsed:
+        logger.info("Reading Dogtag specific config values")
+        parser = ConfigParser()
+
+        try:
+            parser.read(DOGTAG_CONFIG_FILE)
+        except ParsingError as e:
+            logger.error("Unable to parse {}: {}".format(DOGTAG_CONFIG_FILE, e))
+            return
+
+        if not parser.has_section(DOGTAG_CONFIG_SECTION):
+            # If "dogtag" section is missing, initialize with default instance name
+            config['ca_instance_name'] = DOGTAG_DEFAULT_INSTANCE
+            config['kra_instance_name'] = DOGTAG_DEFAULT_INSTANCE
+            config['ocsp_instance_name'] = DOGTAG_DEFAULT_INSTANCE
+            config['tks_instance_name'] = DOGTAG_DEFAULT_INSTANCE
+            config['tps_instance_name'] = DOGTAG_DEFAULT_INSTANCE
+
+            # There is no point in re-reading the config file.
+            dogtag_config_parsed = True
+            return
+
+        items = parser.items(DOGTAG_CONFIG_SECTION)
+
+        for (key, value) in items:
+            config[key] = value
+
+        dogtag_config_parsed = True
