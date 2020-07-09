8cb34a7723758c1af8f63b30afd9465eb53cff41
Endi S. Dewata <edewata@redhat.com>
2020-07-08 19:23:49 -0500

    Added ACMEMaintenanceTask
    The ACMEMaintenanceTask has been added to clean up ACME
    database. Initially it is used to clean up expired nonces
    every 5 minutes.

---
+++ base/acme/src/main/java/org/dogtagpki/acme/scheduler/ACMEMaintenanceTask.java
@@ -0,0 +1,31 @@
+//
+// Copyright Red Hat, Inc.
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+package org.dogtagpki.acme.scheduler;
+
+import java.util.Date;
+
+import org.dogtagpki.acme.database.ACMEDatabase;
+import org.dogtagpki.acme.server.ACMEEngine;
+
+/**
+ * @author Endi S. Dewata
+ */
+public class ACMEMaintenanceTask extends ACMETask {
+
+    public static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ACMEMaintenanceTask.class);
+
+    public void run() throws Exception {
+
+        logger.info("Running ACME maintenance");
+
+        Date currentTime = new Date();
+
+        ACMEEngine engine = ACMEEngine.getInstance();
+        ACMEDatabase database = engine.getDatabase();
+
+        database.removeExpiredNonces(currentTime);
+    }
+}
