665db6fcae353c830c38bdadba3af1e89ba50e91
Shalini Khandelwal <skhandel@redhat.com>
2020-07-29 20:11:26 +0530

    Code cleanup of osp_provision.py
    Signed-off-by: Shalini Khandelwal <skhandel@redhat.com>

--- tests/dogtag/pytest-ansible/provision/osp_provision.py
+++ tests/dogtag/pytest-ansible/provision/osp_provision.py
@@ -15,7 +15,6 @@ CI_FLAVOR_NAME = os.getenv('CI_FLAVOR_NAME')
 DOMAIN_NAME = os.getenv('DOMAIN_NAME')
 OS_PASSWORD = os.getenv('OS_PASSWORD')
 OS_NETWORK_ID = os.getenv('OS_NETWORK_ID').split(',')
-# OS_NETWORK_ID = random.choice(OS_NETWORK_ID)
 
 inventory_file = "ansible_inventory"
 instance_record = '/tmp/instance_record.txt'
@@ -46,26 +45,21 @@ def spawn_server(conn, image_name, server_type='master'):
         server_name = 'OSP_API_Client_{}'.format(random.randint(999, 99999))
     image = conn.compute.find_image(image_name)
     flavor = conn.compute.find_flavor(CI_FLAVOR_NAME)
-    try:
-        server = conn.compute.create_server(name=server_name, image_id=image.id,
-                                            flavor_id=flavor.id, networks=[{"uuid": OS_NETWORK_ID}],
-                                            key_name=OS_KEYPAIR_NAME)
+    server = conn.compute.create_server(name=server_name, image_id=image.id,
+                                        flavor_id=flavor.id, networks=[{"uuid": OS_NETWORK_ID}],
+                                        key_name=OS_KEYPAIR_NAME)
 
+    # Writing server details in the inventory file which is used at the time of teardown.
+    if os.path.isfile(instance_record):
+        with open(instance_record, 'a') as f:
+            f.write('{}\n'.format(server.name))
+    else:
+        with open(instance_record, 'w') as f:
+            f.write('{}\n'.format(server.name))
 
-        # Writing server details in the inventory file which is used at the time of teardown.
-        if os.path.isfile(instance_record):
-            with open(instance_record, 'a') as f:
-                f.write('{}\n'.format(server.name))
-        else:
-            with open(instance_record, 'w') as f:
-                f.write('{}\n'.format(server.name))
-
-        # Wait time for VM to come active is 10 min
-        server = conn.compute.wait_for_server(server, status='ACTIVE', failures='ERROR', interval=2, wait=600)
-        return server
-    except Exception as e:
-        print(e)
-        sys.exit(1)
+    # Wait time for VM to come active is 10 min
+    server = conn.compute.wait_for_server(server, status='ACTIVE', failures='ERROR', interval=2, wait=600)
+    return server
 
 
 def delete_server(conn, server_name):
@@ -118,9 +112,8 @@ def cli():
 @click.option('-v', '--verbose', is_flag=True, help="Verbose")
 def up(inventory, image, image_type, server_type, verbose):
 
-    python_interpreter = ''
     if not image:
-        image_name = 'Fedora 31'
+        image_name = 'Fedora-Cloud-Base-32'
     else:
         image_name = image
 
@@ -134,69 +127,68 @@ def up(inventory, image, image_type, server_type, verbose):
     image_list = list_images(conn, image)
 
     network = None
-    if image_name in image_list:
-        print("Using Image: {}".format(image_name))
-        server = spawn_server(conn, image_name, server_type=server_type)
-        print(server)
-        if server:
-            for key in server.addresses.keys():
-                network = server.addresses[key][0]
-                print("OSP Machine name: {}".format(server.name))
-                print("Server ip: {}".format(network['addr']))
-
-            data = ["[all]\n"]
-            if os.path.isfile(inventory):
-                with open(inventory, 'r') as f:
-                    data = f.readlines()
-
-            data = [i.strip() for i in data]
-            index = data.index("[all]")
-            data.insert(index + 1, "{} hostname={} {}".format(network['addr'], network['addr'],
-                                                              python_interpreter))
-            if "[{}]".format(server_type) in data:
-                index = data.index("[{}]".format(server_type))
-                data.insert(index + 1, "{} hostname={} {}".format(network['addr'], network['addr'],
-                                                                  python_interpreter))
-            else:
-                data.extend(["\n[{}]".format(server_type),
-                             "{} hostname={} {}".format(network['addr'], network['addr'],
-                                                        python_interpreter)])
-            with open(inventory, 'w') as f:
-                print(data)
-                file_data = "\n".join(data)
-                f.write(file_data)
-        else:
-            print("ERROR: Unable to provision machine")
-            sys.exit(1)
-
-    else:
+    if image_name not in image_list:
         print("ERROR: No image found.")
         sys.exit(1)
+
+    print("Using Image: {}".format(image_name))
+    server = spawn_server(conn, image_name, server_type=server_type)
+
+    if not server:
+        print("ERROR: Unable to provision machine")
+        sys.exit(1)
+
+    for key in server.addresses.keys():
+        network = server.addresses[key][0]
+        print("OSP Machine name: {}".format(server.name))
+        print("Server ip: {}".format(network['addr']))
+
+    data = ["[all]\n"]
+    if os.path.isfile(inventory):
+        with open(inventory, 'r') as f:
+            data = f.readlines()
+
+    data = [i.strip() for i in data]
+    index = data.index("[all]")
+    data.insert(index + 1, "{} hostname={}".format(network['addr'], network['addr']))
+    if "[{}]".format(server_type) in data:
+        index = data.index("[{}]".format(server_type))
+        data.insert(index + 1, "{} hostname={}".format(network['addr'], network['addr']))
+    else:
+        data.extend(["\n[{}]".format(server_type),
+                     "{} hostname={} {}".format(network['addr'], network['addr'])])
+    with open(inventory, 'w') as f:
+        print(data)
+        file_data = "\n".join(data)
+        f.write(file_data)
 
 
 @cli.command()
 @click.option('--inventory', default='ansible_inventory', help='Inventory file path')
 def down(inventory):
+
     name = None
     if not inventory:
         inventory = inventory_file
-    if os.path.isfile(instance_record):
-        conn = create_connection(OS_AUTH_URL, OS_REGION_NAME,
-                                 OS_PROJECT_NAME, OS_USERNAME,
-                                 OS_PASSWORD)
-        vm_list = open(instance_record, 'r').read()
-        for record in vm_list.split("\n"):
-            if record.strip():
-                name = record.split()[0].strip()
-                print("Removing Machine: {}".format(name))
-                delete_server(conn, name)
-            print("Removed Machine: {}".format(name))
-    else:
+
+    if not os.path.isfile(instance_record):
         print("ERROR: No record file found. Delete instances manually.")
-    print("Removed record.")
+        os.remove(inventory)
+        print("Removed inventory file.")
+        sys.exit(1)
+
+    conn = create_connection(OS_AUTH_URL, OS_REGION_NAME,
+                             OS_PROJECT_NAME, OS_USERNAME,
+                             OS_PASSWORD)
+    vm_list = open(instance_record, 'r').read()
+    for record in vm_list.split("\n"):
+        if record.strip():
+            name = record.split()[0].strip()
+            print("Removing Machine: {}".format(name))
+            delete_server(conn, name)
+        print("Removed Machine: {}".format(name))
     os.remove(instance_record)
-    print("Removed inventory.")
-    os.remove(inventory)
+    print("Removed record.")
 
 
 cli.add_command(up)
