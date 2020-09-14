f1b8e6b80f87820e907128710af2c13ee705195c
Endi S. Dewata <edewata@redhat.com>
2020-09-14 08:38:00 -0500

    Add pki-server ca-profile-import

--- base/server/python/pki/server/cli/ca.py
+++ base/server/python/pki/server/cli/ca.py
@@ -48,6 +48,7 @@ class CACLI(pki.cli.CLI):
         self.add_module(pki.server.cli.audit.AuditCLI(self))
         self.add_module(CACertCLI())
         self.add_module(CACloneCLI())
+        self.add_module(CAProfileCLI())
         self.add_module(pki.server.cli.config.SubsystemConfigCLI(self))
         self.add_module(pki.server.cli.db.SubsystemDBCLI(self))
         self.add_module(pki.server.cli.range.RangeCLI(self))
@@ -476,3 +477,88 @@ class CAClonePrepareCLI(pki.cli.CLI):
 
         finally:
             shutil.rmtree(tmpdir)
+
+
+class CAProfileCLI(pki.cli.CLI):
+
+    def __init__(self):
+        super(CAProfileCLI, self).__init__(
+            'profile', 'CA profile management commands')
+
+        self.add_module(CAProfileImportCLI())
+
+
+class CAProfileImportCLI(pki.cli.CLI):
+
+    def __init__(self):
+        super(CAProfileImportCLI, self).__init__(
+            'import', 'Import CA profiles')
+
+    def print_help(self):
+        print('Usage: pki-server ca-profile-import [OPTIONS]')
+        print()
+        print('  -i, --instance <instance ID>       Instance ID (default: pki-tomcat).')
+        print('      --input-folder <path>          Input folder.')
+        print('      --as-current-user              Run as current user.')
+        print('  -v, --verbose                      Run in verbose mode.')
+        print('      --debug                        Run in debug mode.')
+        print('      --help                         Show help message.')
+        print()
+
+    def execute(self, argv):
+
+        try:
+            opts, _ = getopt.gnu_getopt(argv, 'i:v', [
+                'instance=',
+                'input-folder=',
+                'verbose', 'debug', 'help'])
+
+        except getopt.GetoptError as e:
+            logger.error(e)
+            self.print_help()
+            sys.exit(1)
+
+        instance_name = 'pki-tomcat'
+        input_folder = '/usr/share/pki/ca/profiles/ca'
+        as_current_user = False
+
+        for o, a in opts:
+            if o in ('-i', '--instance'):
+                instance_name = a
+
+            elif o == '--input-folder':
+                input_folder = a
+
+            elif o == '--as-current-user':
+                as_current_user = True
+
+            elif o in ('-v', '--verbose'):
+                logging.getLogger().setLevel(logging.INFO)
+
+            elif o == '--debug':
+                logging.getLogger().setLevel(logging.DEBUG)
+
+            elif o == '--help':
+                self.print_help()
+                sys.exit()
+
+            else:
+                logger.error('Invalid option: %s', o)
+                self.print_help()
+                sys.exit(1)
+
+        instance = pki.server.instance.PKIInstance(instance_name)
+        if not instance.exists():
+            logger.error('Invalid instance: %s', instance_name)
+            sys.exit(1)
+
+        instance.load()
+
+        subsystem = instance.get_subsystem('ca')
+        if not subsystem:
+            logger.error('No CA subsystem in instance %s', instance_name)
+            sys.exit(1)
+
+        subsystem.import_profiles(
+            input_folder=input_folder,
+            as_current_user=as_current_user)
