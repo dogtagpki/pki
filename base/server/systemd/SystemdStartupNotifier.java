33fa47bafef4e4eeee4ae834c0922afd494e9422
Fraser Tweedale <ftweedal@redhat.com>
2020-09-29 23:18:34 +1000

    startup notification: complete systemd notifier
    Complete the implementation of SystemdStartupNotifier.  We use JNA
    to bind to libsystemd.  The dependency on 'jna' package only occurs
    when %{with sdnotify}.
    
    The systemd unit template file is left alone, retaining Type=simple.
    In order to enable systemd startup notification, you can override
    the Type in the "drop-in" directory. For example, if the instance
    name is 'pki-tomcat', write to the file:
    
      /etc/systemd/system/pki-tomcatd@pki-tomcat.service.d/notify.conf
    
    the content:
    
      [Service]
      Type=notify
    
    See systemd.unit(5) for more details.
    
    Fixes: https://pagure.io/dogtagpki/issue/1233

--- base/server/systemd/SystemdStartupNotifier.java
+++ base/server/systemd/SystemdStartupNotifier.java
@@ -5,10 +5,42 @@
 //
 package com.netscape.cmscore.systemd;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
 import com.netscape.cmscore.apps.StartupNotifier;
 
 public class SystemdStartupNotifier implements StartupNotifier {
+    interface Systemd extends Library {
+        public int sd_booted();
+        public int sd_notify(int unset_environment, String state);
+    }
+
+    /* loadLibrary is deprecated in jna >= 5; replaced with load()
+     * which avoids the cast.  But RHEL 8 has jna 4.5 so we must put
+     * up with deprecation warnings on Fedora. */
+    static Systemd systemd = (Systemd) Native.loadLibrary("systemd", Systemd.class);
+
+    static boolean isNotifyService = hasNotifySocket() && systemdBooted();
+
+    private static boolean systemdBooted() {
+        return systemd.sd_booted() > 0;
+    }
+
+    private static boolean hasNotifySocket() {
+        return System.getenv("NOTIFY_SOCKET") != null;
+    }
+
+    private static void notify(String status) {
+        if (isNotifyService) {
+            System.out.println("Notifying systemd:\n" + status);
+            systemd.sd_notify(0 /* unset_environment */, status);
+        } else {
+            System.err.println("Failed to notify systemd (isNotifyService = false)");
+        }
+    }
+
     public void notifyReady() {
-        System.out.println("Notifying systemd... not!");
+        notify("READY=1");
     }
 }
