6f778fb9d16dba6d7777d14b53c589c31cb576b1
Endi S. Dewata <edewata@redhat.com>
2021-02-03 11:29:32 -0600

    Update log messages in test_cert_enrollment.py
    The test_cert_enrollment.py has been modified to provide
    a --verbose and a --debug options to show the test progress
    and some debugging information.

--- tests/dogtag/pytest-ansible/pytest/performance_test/test_cert_enrollment.py
+++ tests/dogtag/pytest-ansible/pytest/performance_test/test_cert_enrollment.py
@@ -40,11 +40,17 @@ parser.add_argument("--number-of-tests-per-client", "--tests-per-client",
                     help="Number of tests per client",
                     default=1,
                     type=int)
-parser.add_argument("--ca-cert-path",
+parser.add_argument("--ca-cert-path", "--ca-cert",
                     help="Path to CA signing certificate in PEM format")
+parser.add_argument("-v", "--verbose",
+                    help="Run in verbose mode.",
+                    action="store_true")
+parser.add_argument("--debug",
+                    help="Run in debug mode.",
+                    action="store_true")
 
-log = logging.getLogger()
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(levelname)s: %(message)s')
 
 args = parser.parse_args()
 
@@ -65,7 +71,7 @@ class cert_enroll(object):
     #This class will enroll the certificate.
     """
 
-    def __init__(self, sn_uid):
+    def __init__(self, id, sn_uid):
         """
         Constructor
         """
@@ -88,6 +94,7 @@ class cert_enroll(object):
         inputs['sn_e'] = 'example@redhat.com'
         inputs['sn_cn'] = 'MyTestUser'
 
+        logger.debug("Client %s: Submitting enrollment request", id)
         enrollment_results = cert_client.enroll_cert('caUserCert', inputs)
 
         if enrollment_results.__len__() == 0:
@@ -100,13 +107,16 @@ class cert_enroll(object):
                 raise Exception("Cert enrollment failed : {}".format(cert_data))
             else:
                 self.cert_serial_number = cert_data.serial_number
+                logger.debug("Client %s: Certificate: %s", id, cert_data)
 
 
-def run_test(sn_uid, number_of_tests_per_thread):
+def run_test(id, number_of_tests_per_thread):
+    sn_uid = "testuser{}".format(id)
     # execute the specified number of tests sequentially
-    for i in range(number_of_tests_per_thread):
+    for i in range(1, number_of_tests_per_thread + 1):
+        logger.info("Client %s: Enrolling cert %s of %s", id, i, number_of_tests_per_thread)
         start = timer()
-        serial_num = cert_enroll(sn_uid)
+        serial_num = cert_enroll(id, sn_uid)
         end = timer()
         issuance_times.append(end - start)
         cert_list.append(serial_num.cert_serial_number)
@@ -114,17 +124,31 @@ def run_test(sn_uid, number_of_tests_per_thread):
 
 
 if __name__ == "__main__":
-    # get test parameters from CLI parameters
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    elif args.verbose:
+        logger.setLevel(logging.INFO)
+
+    print("Test parameters:")
+    print("- target: https://%s:%s" % (args.hostname, args.port))
+
     number_of_threads = args.number_of_clients
+    print("- number of clients: %s" % number_of_threads)
+
     number_of_tests_per_thread = args.number_of_tests_per_client
+    print("- number of tests per client: %s" % number_of_tests_per_thread)
 
     # create the specified number of threads
     threads = []
     cert_list = []
     issuance_times = []
     start = timer()
-    for t in range(number_of_threads):
-        t1 = threading.Thread(target=run_test, args=("testuser{}".format(t), number_of_tests_per_thread))
+
+    for t in range(1, number_of_threads + 1):
+        logger.info("Starting client %s" % t)
+        t1 = threading.Thread(target=run_test, args=(t, number_of_tests_per_thread))
         t1.start()
         threads.append(t1)
 
@@ -143,9 +167,9 @@ if __name__ == "__main__":
     T = end - start
     N = number_of_threads * number_of_tests_per_thread
 
-    log.info("Number of certs enrolled (N)={}".format(N))
-    log.info("Minimum execution time (T)={}".format(min(issuance_times)))
-    log.info("Maximum execution time (T)={}".format(max(issuance_times)))
-    log.info("Average execution time (T)={}".format(sum(issuance_times)/len(issuance_times)))
-    log.info("Test execution time (T)={}".format(T))
-    log.info("Throughput (V = N/T)={}".format(N / T))
+    print("Number of certs enrolled (N): {}".format(N))
+    print("Minimum execution time (T): {}".format(min(issuance_times)))
+    print("Maximum execution time (T): {}".format(max(issuance_times)))
+    print("Average execution time (T): {}".format(sum(issuance_times)/len(issuance_times)))
+    print("Test execution time (T): {}".format(T))
+    print("Throughput (V = N/T): {}".format(N / T))
