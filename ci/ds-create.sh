7a536cabde7ae2ba08e6e427c9f24c2290d4fbcf
Endi S. Dewata <edewata@redhat.com>
2021-03-22 14:28:27 -0500

    Add test for installing CA with secure DS

--- ci/ds-create.sh
+++ ci/ds-create.sh
@@ -2,13 +2,14 @@
 
 # This command needs to be executed as it pulls the machine name
 # dynamically.
-dscreate create-template ds.tmp
+dscreate create-template ds.inf
 
-sed \
-    -e 's/;root_password = .*/root_password = Secret.123/g' \
-    -e 's/;suffix = .*/suffix = dc=example,dc=com/g' \
-    -e 's/;instance_name = .*/instance_name = localhost/g' \
-    ds.tmp > ds.inf
+sed -i \
+    -e "s/;instance_name = .*/instance_name = localhost/g" \
+    -e "s/;root_password = .*/root_password = Secret.123/g" \
+    -e "s/;suffix = .*/suffix = dc=example,dc=com/g" \
+    -e "s/;self_sign_cert = .*/self_sign_cert = False/g" \
+    ds.inf
 
 dscreate from-file ds.inf
 
