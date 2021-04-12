bbf31086b5ec7efb6c0e0cdee6af7cd476407593
Endi S. Dewata <edewata@redhat.com>
2021-04-12 10:29:45 -0500

    Clean up IPA test artifacts
    The IPA tests have been modified to store IPA, PKI, and DS
    config and log files into a single tarball.

---
+++ ci/ipa-artifacts-save.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONTAINER=$1
+ARTIFACTS=/tmp/artifacts/$CONTAINER
+
+mkdir -p $ARTIFACTS/var/log
+
+docker exec $CONTAINER ls -la /var/log
+docker cp $CONTAINER:/var/log/ipaclient-install.log $ARTIFACTS/var/log
+docker cp $CONTAINER:/var/log/ipareplica-install.log $ARTIFACTS/var/log
+docker cp $CONTAINER:/var/log/ipareplica-conncheck.log $ARTIFACTS/var/log
+docker cp $CONTAINER:/var/log/ipaserver-install.log $ARTIFACTS/var/log
+docker cp $CONTAINER:/var/log/ipa $ARTIFACTS/var/log
+docker cp $CONTAINER:/var/log/httpd $ARTIFACTS/var/log
