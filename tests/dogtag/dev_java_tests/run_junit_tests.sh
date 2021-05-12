747f63e43eb4ce769414e9a75f65be862c2f0d81
Chris Kelley <ckelley@redhat.com>
2021-05-12 14:48:49 +0100

    Remove references to xml-commons.api.jar
    This JAR contains packages/classes that are present in OpenJDK 11. As of
    Java 9 it is a compile time failure for packages/classes to be available
    from multiple modules. In order to be Java 11 compatible this JAR needs
    to be removed and the multiply-sourced code sourced from the JDK.
    
    References to the JAR in scripts, build tools etc have also been
    removed.

--- tests/dogtag/dev_java_tests/run_junit_tests.sh
+++ tests/dogtag/dev_java_tests/run_junit_tests.sh
@@ -59,7 +59,6 @@ run_dev_junit_tests() {
     CLASSPATH=$CLASSPATH:/usr/share/java/istack-commons-runtime.jar
     CLASSPATH=$CLASSPATH:/usr/share/java/servlet.jar
     CLASSPATH=$CLASSPATH:/usr/share/java/xerces-j2.jar
-    CLASSPATH=$CLASSPATH:/usr/share/java/xml-commons-apis.jar
     CLASSPATH=$CLASSPATH:/usr/share/java/tomcat/catalina.jar
     CLASSPATH=$CLASSPATH:/usr/share/java/tomcat/tomcat-util.jar
     CLASSPATH=$CLASSPATH:/usr/share/java/commons-io.jar
