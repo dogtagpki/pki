9969a03b07c73eb34cb9aa79af7b4507ebb298f9
Chris Kelley <ckelley@redhat.com>
2021-05-13 11:24:53 +0100

    Remove Link class references from DataCollection

--- base/common/src/main/java/com/netscape/certsrv/base/DataCollection.java
+++ base/common/src/main/java/com/netscape/certsrv/base/DataCollection.java
@@ -21,9 +21,6 @@ package com.netscape.certsrv.base;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.xml.bind.annotation.XmlElement;
-
-import org.jboss.resteasy.plugins.providers.atom.Link;
 
 /**
  * @author Endi S. Dewata
@@ -32,7 +29,6 @@ public class DataCollection<E> {
 
     int total;
     Collection<E> entries = new ArrayList<E>();
-    Collection<Link> links = new ArrayList<Link>();
 
     public int getTotal() {
         return total;
@@ -60,31 +56,11 @@ public class DataCollection<E> {
         entries.remove(entry);
     }
 
-    @XmlElement(name="Link")
-    public Collection<Link> getLinks() {
-        return links;
-    }
-
-    public void setLinks(Collection<Link> links) {
-        this.links.clear();
-        if (links == null) return;
-        this.links.addAll(links);
-    }
-
-    public void addLink(Link link) {
-        links.add(link);
-    }
-
-    public void removeLink(Link link) {
-        links.remove(link);
-    }
-
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((entries == null) ? 0 : entries.hashCode());
-        result = prime * result + ((links == null) ? 0 : links.hashCode());
         result = prime * result + total;
         return result;
     }
@@ -102,11 +78,6 @@ public class DataCollection<E> {
             if (other.entries != null)
                 return false;
         } else if (!entries.equals(other.entries))
-            return false;
-        if (links == null) {
-            if (other.links != null)
-                return false;
-        } else if (!links.equals(other.links))
             return false;
         if (total != other.total)
             return false;
