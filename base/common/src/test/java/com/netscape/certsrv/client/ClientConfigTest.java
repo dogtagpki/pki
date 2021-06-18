963883e24af8416b922884630f1c6663d37fea95
Chris Kelley <ckelley@redhat.com>
2021-06-18 23:33:32 +0100

    Add JSON mapper for ClientConfig

---
+++ base/common/src/test/java/com/netscape/certsrv/client/ClientConfigTest.java
@@ -0,0 +1,51 @@
+package com.netscape.certsrv.client;
+
+import java.net.MalformedURLException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClientConfigTest {
+
+    private static ClientConfig before = new ClientConfig();
+
+    @Before
+    public void setUpBefore() throws MalformedURLException {
+        before.setServerURL("http://localhost:8080");
+        before.setNSSDatabase("certs");
+        before.setNSSPassword("12345");
+        before.setNSSPassword("internal", "12345");
+        before.setNSSPassword("hsm", "12345");
+        before.setCertNickname("caadmin");
+        before.setUsername("caadmin");
+        before.setPassword("12345");
+    }
+
+    @Test
+    public void testXML() throws Exception {
+        // Act
+        String xml = before.toXML();
+        System.out.println("XML (before): " + xml);
+
+        ClientConfig afterXML = ClientConfig.fromXML(xml);
+        System.out.println("XML (after): " + afterXML.toXML());
+
+        // Assert
+        Assert.assertEquals(before, afterXML);
+    }
+
+    @Test
+    public void testJSON() throws Exception {
+        // Act
+        String json = before.toJSON();
+        System.out.println("JSON (before): " + json);
+
+        ClientConfig afterJSON = ClientConfig.fromJSON(json);
+        System.out.println("JSON (after): " + afterJSON.toJSON());
+
+        // Assert
+        Assert.assertEquals(before, afterJSON);
+    }
+
+}
