f565e9c866e6cbcf607ef00467861734a55ed67f
Chris Kelley <ckelley@redhat.com>
2021-06-22 15:47:18 +0100

    Add JSON wrappers for classes in com.netscape.certsrv.request

---
+++ base/common/src/test/java/com/netscape/certsrv/request/CMSRequestInfoTest.java
@@ -0,0 +1,43 @@
+package com.netscape.certsrv.request;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CMSRequestInfoTest {
+
+    private static CMSRequestInfo before = new CMSRequestInfo();
+
+    @Before
+    public void setUpBefore() {
+        before.setRequestType("securityDataEnrollment");
+        before.setRequestStatus(RequestStatus.COMPLETE);
+    }
+
+    @Test
+    public void testXML() throws Exception {
+        // Act
+        String xml = before.toXML();
+        System.out.println("XML (before): " + xml);
+
+        CMSRequestInfo afterXML = CMSRequestInfo.fromXML(xml);
+        System.out.println("XML (after): " + afterXML.toXML());
+
+        // Assert
+        Assert.assertEquals(before, afterXML);
+    }
+
+    @Test
+    public void testJSON() throws Exception {
+        // Act
+        String json = before.toJSON();
+        System.out.println("JSON (before): " + json);
+
+        CMSRequestInfo afterJSON = CMSRequestInfo.fromJSON(json);
+        System.out.println("JSON (after): " + afterJSON.toJSON());
+
+        // Assert
+        Assert.assertEquals(before, afterJSON);
+    }
+
+}
