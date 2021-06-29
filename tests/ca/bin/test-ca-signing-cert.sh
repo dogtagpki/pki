0768aff10c4931fa01bf7dd0e8471764ca98e9d6
Endi S. Dewata <edewata@redhat.com>
2021-06-29 16:17:56 -0500

    Add test for CA certs

---
+++ tests/ca/bin/test-ca-signing-cert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+# get CA signing cert using certutil
+certutil -L -d /etc/pki/pki-tomcat/alias -n ca_signing -r > /tmp/ca_signing.crt
+
+# get CA signing cert using pki ca-cert-signing-export
+pki ca-cert-signing-export > /tmp/ca_signing.pem
+openssl x509 -outform der -in /tmp/ca_signing.pem -out /tmp/ca_signing.der
+
+# the certs should be identical
+diff /tmp/ca_signing.crt /tmp/ca_signing.der
