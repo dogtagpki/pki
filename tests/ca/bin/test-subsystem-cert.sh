131850d967bc65a4d4d0b6dc3aab6cded2dd6ce9
Endi S. Dewata <edewata@redhat.com>
2021-06-29 16:17:56 -0500

    Add test for CA certs

---
+++ tests/ca/bin/test-subsystem-cert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+# get subsystem cert using certutil
+certutil -L -d /etc/pki/pki-tomcat/alias -n subsystem -r > /tmp/subsystem.crt
+
+# get subsystem cert using pki ca-cert-subsystem-export
+pki ca-cert-subsystem-export > /tmp/subsystem.pem
+openssl x509 -outform der -in /tmp/subsystem.pem -out /tmp/subsystem.der
+
+# the certs should be identical
+diff /tmp/subsystem.crt /tmp/subsystem.der
