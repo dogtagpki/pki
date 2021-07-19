98adff6418a0a3e646fcc969a55524c08bb5e764
Chris Kelley <ckelley@redhat.com>
2021-07-19 10:20:54 +0100

    Remove XML mappers from CertRequestInfo{s}

--- base/common/src/test/java/com/netscape/certsrv/cert/CertRequestInfoTest.java
+++ base/common/src/test/java/com/netscape/certsrv/cert/CertRequestInfoTest.java
@@ -21,19 +21,6 @@ public class CertRequestInfoTest {
     }
 
     @Test
-    public void testXML() throws Exception {
-        // Act
-        String xml = before.toXML();
-        System.out.println("XML (before): " + xml);
-
-        CertRequestInfo afterXML = CertRequestInfo.fromXML(xml);
-        System.out.println("XML (after): " + afterXML.toXML());
-
-        // Assert
-        Assert.assertEquals(before, afterXML);
-    }
-
-    @Test
     public void testJSON() throws Exception {
         // Act
         String json = before.toJSON();
