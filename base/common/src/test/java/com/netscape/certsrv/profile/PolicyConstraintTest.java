fb2af11238af99956c9d277feccef38a725a264c
Chris Kelley <ckelley@redhat.com>
2021-07-23 23:40:26 +0100

    Remove XML tagging from PolicyContraint{Value}

--- base/common/src/test/java/com/netscape/certsrv/profile/PolicyConstraintTest.java
+++ base/common/src/test/java/com/netscape/certsrv/profile/PolicyConstraintTest.java
@@ -21,19 +21,6 @@ public class PolicyConstraintTest {
     }
 
     @Test
-    public void testXML() throws Exception {
-        // Act
-        String xml = before.toXML();
-        System.out.println("XML (before): " + xml);
-
-        PolicyConstraint afterXML = PolicyConstraint.fromXML(xml);
-        System.out.println("XML (after): " + afterXML.toXML());
-
-        // Assert
-        Assert.assertEquals(before, afterXML);
-    }
-
-    @Test
     public void testJSON() throws Exception {
         // Act
         String json = before.toJSON();
