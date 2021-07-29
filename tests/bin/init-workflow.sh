eef1f62d8dbed5a6692c2b2b8c0faa4c393134fb
Endi S. Dewata <edewata@redhat.com>
2021-07-29 17:28:56 -0500

    Update default test matrix
    The init-workflow.sh has been modified to test
    against the latest Fedora version by default.

--- tests/bin/init-workflow.sh
+++ tests/bin/init-workflow.sh
@@ -2,9 +2,7 @@
 
 if [ "$BASE64_MATRIX" == "" ]
 then
-    latest=$(cat /etc/fedora-release | awk '{ print $3 }')
-    previous=$(cat /etc/fedora-release | awk '{ print $3 - 1 }')
-    MATRIX="{\"os\":[\"$previous\", \"$latest\"]}"
+    MATRIX="{\"os\":[\"latest\"]}"
 else
     MATRIX=$(echo "$BASE64_MATRIX" | base64 -d)
 fi
