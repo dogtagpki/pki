317b135b7ad503530a7409ec32e80b0a8e58089d
Endi S. Dewata <edewata@redhat.com>
2021-08-17 10:25:55 -0500

    Add DOM mapping for CertDataInfos

--- base/common/src/main/java/com/netscape/certsrv/cert/CertDataInfos.java
+++ base/common/src/main/java/com/netscape/certsrv/cert/CertDataInfos.java
@@ -22,10 +22,15 @@ import java.util.Collection;
 import javax.xml.bind.annotation.XmlElementRef;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.netscape.certsrv.base.DataCollection;
+import com.netscape.certsrv.base.Link;
 
 @XmlRootElement(name = "CertDataInfos")
 @JsonInclude(Include.NON_NULL)
@@ -36,5 +41,56 @@ public class CertDataInfos extends DataCollection<CertDataInfo> {
     @XmlElementRef
     public Collection<CertDataInfo> getEntries() {
         return super.getEntries();
+    }
+
+    public Element toDOM(Document document) {
+
+        Element infosElement = document.createElement("CertDataInfos");
+        document.appendChild(infosElement);
+
+        Element totalElement = document.createElement("total");
+        totalElement.appendChild(document.createTextNode(Integer.toString(total)));
+        infosElement.appendChild(totalElement);
+
+        for (CertDataInfo certDataInfo : getEntries()) {
+            Element infoElement = certDataInfo.toDOM(document);
+            infosElement.appendChild(infoElement);
+        }
+
+        for (Link link : getLinks()) {
+            Element infoElement = link.toDOM(document);
+            infosElement.appendChild(infoElement);
+        }
+
+        return infosElement;
+    }
+
+    public static CertDataInfos fromDOM(Element infosElement) {
+
+        CertDataInfos infos = new CertDataInfos();
+
+        NodeList totalList = infosElement.getElementsByTagName("total");
+        if (totalList.getLength() > 0) {
+            String value = totalList.item(0).getTextContent();
+            infos.setTotal(Integer.parseInt(value));
+        }
+
+        NodeList infoList = infosElement.getElementsByTagName("CertDataInfo");
+        int infoCount = infoList.getLength();
+        for (int i=0; i<infoCount; i++) {
+           Element infoElement = (Element) infoList.item(i);
+           CertDataInfo info = CertDataInfo.fromDOM(infoElement);
+           infos.addEntry(info);
+        }
+
+        NodeList linkList = infosElement.getElementsByTagName("Link");
+        int linkCount = linkList.getLength();
+        for (int i=0; i<linkCount; i++) {
+           Element linkElement = (Element) linkList.item(i);
+           Link link = Link.fromDOM(linkElement);
+           infos.addLink(link);
+        }
+
+        return infos;
     }
 }
