070c45f8d33e5a7118636757515beb7e9befc78f
Chris Kelley <ckelley@redhat.com>
2021-08-18 15:02:09 +0100

    Modify PolicyConstraintTest to produce more accurate XML output
    So we can see whether the DOM replacement for jaxb produces equivalent

--- base/common/src/test/java/com/netscape/certsrv/profile/PolicyConstraintTest.java
+++ base/common/src/test/java/com/netscape/certsrv/profile/PolicyConstraintTest.java
@@ -1,21 +1,40 @@
 package com.netscape.certsrv.profile;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.netscape.certsrv.property.Descriptor;
+import com.netscape.certsrv.property.IDescriptor;
 import com.netscape.certsrv.util.JSONSerializer;
 
 public class PolicyConstraintTest {
 
     private static PolicyConstraint before = new PolicyConstraint();
+    private static PolicyConstraintValue pcv1 = new PolicyConstraintValue();
+    private static PolicyConstraintValue pcv2 = new PolicyConstraintValue();
+    private static Descriptor descriptor = new Descriptor(
+            IDescriptor.CHOICE,
+            "true,false,-",
+            "-",
+            "CMS_PROFILE_CRITICAL");
+    private static List<PolicyConstraintValue> constraints = new ArrayList<>();
 
-    @Before
-    public void setUpBefore() {
+    @BeforeClass
+    public static void setUpBefore() {
         before.setClassId("foo");
-        before.setConstraints(new ArrayList<>());
+        pcv1.setDescriptor(descriptor);
+        pcv1.setName("foo1");
+        pcv1.setValue("bar1");
+        constraints.add(pcv1);
+        pcv2.setDescriptor(descriptor);
+        pcv2.setName("foo2");
+        pcv2.setValue("bar2");
+        constraints.add(pcv2);
+        before.setConstraints(constraints);
         before.setName("bar");
         before.setText("lorem ipsum");
     }
