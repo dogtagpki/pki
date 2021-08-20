582e8144f2d13789cd9ace902c59e0244beb4327
Endi S. Dewata <edewata@redhat.com>
2021-08-20 09:43:20 -0500

    Add XML mapping for Info

--- base/common/src/main/java/org/dogtagpki/common/Info.java
+++ base/common/src/main/java/org/dogtagpki/common/Info.java
@@ -18,8 +18,23 @@
 
 package org.dogtagpki.common;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -100,4 +115,89 @@ public class Info extends ResourceMessage {
         return true;
     }
 
+    public Element toDOM(Document document) {
+
+        Element infoElement = document.createElement("Info");
+
+        toDOM(document, infoElement);
+
+        if (name != null) {
+            Element nameElement = document.createElement("Name");
+            nameElement.appendChild(document.createTextNode(name));
+            infoElement.appendChild(nameElement);
+        }
+
+        if (version != null) {
+            Element versionElement = document.createElement("Version");
+            versionElement.appendChild(document.createTextNode(version));
+            infoElement.appendChild(versionElement);
+        }
+
+        if (banner != null) {
+            Element bannerElement = document.createElement("Banner");
+            bannerElement.appendChild(document.createTextNode(banner));
+            infoElement.appendChild(bannerElement);
+        }
+
+        return infoElement;
+    }
+
+    public static Info fromDOM(Element infoElement) {
+
+        Info info = new Info();
+
+        fromDOM(infoElement, info);
+
+        NodeList nameList = infoElement.getElementsByTagName("Name");
+        if (nameList.getLength() > 0) {
+            String value = nameList.item(0).getTextContent();
+            info.setName(value);
+        }
+
+        NodeList versionList = infoElement.getElementsByTagName("Version");
+        if (versionList.getLength() > 0) {
+            String value = versionList.item(0).getTextContent();
+            info.setVersion(value);
+        }
+
+        NodeList bannerList = infoElement.getElementsByTagName("Banner");
+        if (bannerList.getLength() > 0) {
+            String value = bannerList.item(0).getTextContent();
+            info.setName(value);
+        }
+
+        return info;
+    }
+
+    public String toXML() throws Exception {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+
+        Element element = toDOM(document);
+        document.appendChild(element);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+        DOMSource domSource = new DOMSource(document);
+        StringWriter sw = new StringWriter();
+        StreamResult streamResult = new StreamResult(sw);
+        transformer.transform(domSource, streamResult);
+
+        return sw.toString();
+    }
+
+    public static Info fromXML(String xml) throws Exception {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new InputSource(new StringReader(xml)));
+
+        Element element = document.getDocumentElement();
+        return fromDOM(element);
+    }
 }
