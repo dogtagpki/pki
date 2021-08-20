6e8e7360c634740f7f37f36e01a3422f2c24dbf9
Chris Kelley <ckelley@redhat.com>
2021-08-20 11:17:45 +0100

    Improve PolicyDefaultTest by filling out more fields in test object

--- base/common/src/test/java/com/netscape/certsrv/profile/PolicyDefaultTest.java
+++ base/common/src/test/java/com/netscape/certsrv/profile/PolicyDefaultTest.java
@@ -1,23 +1,54 @@
 package com.netscape.certsrv.profile;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.netscape.certsrv.property.Descriptor;
+import com.netscape.certsrv.property.IDescriptor;
 import com.netscape.certsrv.util.JSONSerializer;
 
 public class PolicyDefaultTest {
 
     private static PolicyDefault before = new PolicyDefault();
+    private static ProfileParameter pp1 = new ProfileParameter();
+    private static ProfileParameter pp2 = new ProfileParameter();
+    private static List<ProfileParameter> params = new ArrayList<>();
+    private static ProfileAttribute pa1 = new ProfileAttribute();
+    private static ProfileAttribute pa2 = new ProfileAttribute();
+    private static List<ProfileAttribute> attributes = new ArrayList<>();
+    private static Descriptor descriptor = new Descriptor(
+            IDescriptor.CHOICE,
+            "true,false,-",
+            "-",
+            "CMS_PROFILE_CRITICAL");
 
-    @Before
-    public void setUpBefore() {
-        before.setAttributes(new ArrayList<>());
+    @BeforeClass
+    public static void setUpBefore() {
+        pa1.setDescriptor(descriptor);
+        pa1.setName("spam1");
+        pa1.setValue("ham1");
+        attributes.add(pa1);
+        pa2.setDescriptor(descriptor);
+        pa2.setName("spam2");
+        pa2.setValue("ham2");
+        attributes.add(pa2);
+        before.setAttributes(attributes);
+
         before.setClassId("foo");
         before.setName("bar");
-        before.setParams(new ArrayList<>());
+
+        pp1.setName("foo1");
+        pp1.setValue("bar1");
+        params.add(pp1);
+        pp2.setName("foo2");
+        pp2.setValue("bar2");
+        params.add(pp2);
+        before.setParams(params);
+
         before.setText("lorem");
     }
 
