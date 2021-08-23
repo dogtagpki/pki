c119193e21d6c06597451bf1ef7a1075f2e21c32
Endi S. Dewata <edewata@redhat.com>
2021-08-23 14:56:18 -0500

    Add DOM mapping for CertRequestInfo and CertRequestInfos

--- base/common/src/main/java/com/netscape/certsrv/cert/CertRequestInfo.java
+++ base/common/src/main/java/com/netscape/certsrv/cert/CertRequestInfo.java
@@ -18,6 +18,23 @@
 
 package com.netscape.certsrv.cert;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.commons.lang3.StringUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -33,13 +50,9 @@ public class CertRequestInfo extends CMSRequestInfo {
     public static final String RES_ERROR = "error";
 
     protected CertId certId;
-
     protected String certURL;
-
     protected String certRequestType;
-
     protected String operationResult;
-
     protected String errorMessage;
 
     public CertRequestInfo() {
@@ -149,4 +162,108 @@ public class CertRequestInfo extends CMSRequestInfo {
         }
     }
 
+    public Element toDOM(Document document) {
+
+        Element infoElement = document.createElement("CertDataInfo");
+
+        toDOM(document, infoElement);
+
+        if (certId != null) {
+            infoElement.setAttribute("id", certId.toHexString());
+        }
+
+        if (certURL != null) {
+            Element subjectDNElement = document.createElement("certURL");
+            subjectDNElement.appendChild(document.createTextNode(certURL));
+            infoElement.appendChild(subjectDNElement);
+        }
+
+        if (certRequestType != null) {
+            Element issuerDNElement = document.createElement("certRequestType");
+            issuerDNElement.appendChild(document.createTextNode(certRequestType));
+            infoElement.appendChild(issuerDNElement);
+        }
+
+        if (operationResult != null) {
+            Element statusElement = document.createElement("operationResult");
+            statusElement.appendChild(document.createTextNode(operationResult));
+            infoElement.appendChild(statusElement);
+        }
+
+        if (errorMessage != null) {
+            Element typeElement = document.createElement("errorMessage");
+            typeElement.appendChild(document.createTextNode(errorMessage));
+            infoElement.appendChild(typeElement);
+        }
+
+        return infoElement;
+    }
+
+    public static CertRequestInfo fromDOM(Element infoElement) {
+
+        CertRequestInfo info = new CertRequestInfo();
+
+        CMSRequestInfo.fromDOM(infoElement, info);
+
+        String id = infoElement.getAttribute("id");
+        info.setCertId(StringUtils.isEmpty(id) ? null : new CertId(id));
+
+        NodeList certURLList = infoElement.getElementsByTagName("certURL");
+        if (certURLList.getLength() > 0) {
+            String value = certURLList.item(0).getTextContent();
+            info.setCertURL(value);
+        }
+
+        NodeList certRequestTypeList = infoElement.getElementsByTagName("certRequestType");
+        if (certRequestTypeList.getLength() > 0) {
+            String value = certRequestTypeList.item(0).getTextContent();
+            info.setCertRequestType(value);
+        }
+
+        NodeList operationResultList = infoElement.getElementsByTagName("operationResult");
+        if (operationResultList.getLength() > 0) {
+            String value = operationResultList.item(0).getTextContent();
+            info.setOperationResult(value);
+        }
+
+        NodeList typeList = infoElement.getElementsByTagName("errorMessage");
+        if (typeList.getLength() > 0) {
+            String value = typeList.item(0).getTextContent();
+            info.setErrorMessage(value);
+        }
+
+        return info;
+    }
+
+    public String toXML() throws Exception {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+
+        Element element = toDOM(document);
+        document.appendChild(element);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+        DOMSource domSource = new DOMSource(document);
+        StringWriter sw = new StringWriter();
+        StreamResult streamResult = new StreamResult(sw);
+        transformer.transform(domSource, streamResult);
+
+        return sw.toString();
+    }
+
+    public static CertRequestInfo fromXML(String xml) throws Exception {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new InputSource(new StringReader(xml)));
+
+        Element element = document.getDocumentElement();
+        return fromDOM(element);
+    }
 }
