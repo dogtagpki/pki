f60bc0d485db06f7ae8de92935dc42035c60781f
Endi S. Dewata <edewata@redhat.com>
2021-08-23 14:56:18 -0500

    Add DOM mapping for CertRequestInfo and CertRequestInfos

--- base/common/src/main/java/com/netscape/certsrv/cert/CertRequestInfos.java
+++ base/common/src/main/java/com/netscape/certsrv/cert/CertRequestInfos.java
@@ -17,7 +17,22 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.certsrv.cert;
 
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Collection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -62,4 +77,86 @@ public class CertRequestInfos extends DataCollection<CertRequestInfo> implements
         }
     }
 
+    public Element toDOM(Document document) {
+
+        Element infosElement = document.createElement("CertDataInfos");
+        document.appendChild(infosElement);
+
+        Element totalElement = document.createElement("total");
+        totalElement.appendChild(document.createTextNode(Integer.toString(total)));
+        infosElement.appendChild(totalElement);
+
+        for (CertRequestInfo certRequestInfo : getEntries()) {
+            Element infoElement = certRequestInfo.toDOM(document);
+            infosElement.appendChild(infoElement);
+        }
+
+        for (Link link : getLinks()) {
+            Element infoElement = link.toDOM(document);
+            infosElement.appendChild(infoElement);
+        }
+
+        return infosElement;
+    }
+
+    public String toXML() throws Exception {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+
+        Element element = toDOM(document);
+        document.appendChild(element);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+        DOMSource domSource = new DOMSource(document);
+        StringWriter sw = new StringWriter();
+        StreamResult streamResult = new StreamResult(sw);
+        transformer.transform(domSource, streamResult);
+
+        return sw.toString();
+    }
+
+    public static CertRequestInfos fromDOM(Element infosElement) {
+
+        CertRequestInfos infos = new CertRequestInfos();
+
+        NodeList totalList = infosElement.getElementsByTagName("total");
+        if (totalList.getLength() > 0) {
+            String value = totalList.item(0).getTextContent();
+            infos.setTotal(Integer.parseInt(value));
+        }
+
+        NodeList infoList = infosElement.getElementsByTagName("CertRequestInfo");
+        int infoCount = infoList.getLength();
+        for (int i=0; i<infoCount; i++) {
+           Element infoElement = (Element) infoList.item(i);
+           CertRequestInfo info = CertRequestInfo.fromDOM(infoElement);
+           infos.addEntry(info);
+        }
+
+        NodeList linkList = infosElement.getElementsByTagName("Link");
+        int linkCount = linkList.getLength();
+        for (int i=0; i<linkCount; i++) {
+           Element linkElement = (Element) linkList.item(i);
+           Link link = Link.fromDOM(linkElement);
+           infos.addLink(link);
+        }
+
+        return infos;
+    }
+
+    public static CertRequestInfos fromXML(String xml) throws Exception {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new InputSource(new StringReader(xml)));
+
+        Element element = document.getDocumentElement();
+        return fromDOM(element);
+    }
 }
