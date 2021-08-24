31d0df8dc77b166561c4323c5be965e25a449bc1
Endi S. Dewata <edewata@redhat.com>
2021-08-24 11:21:59 -0500

    Add XML mapping for ProfileDataInfos

--- base/common/src/main/java/com/netscape/certsrv/profile/ProfileDataInfos.java
+++ base/common/src/main/java/com/netscape/certsrv/profile/ProfileDataInfos.java
@@ -17,7 +17,21 @@
 //--- END COPYRIGHT BLOCK ---
 package com.netscape.certsrv.profile;
 
-import java.util.Collection;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -29,26 +43,85 @@ import com.netscape.certsrv.base.Link;
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class ProfileDataInfos extends DataCollection<ProfileDataInfo> {
 
-    @Override
-    public Collection<ProfileDataInfo> getEntries() {
-        return super.getEntries();
+    public Element toDOM(Document document) {
+
+        Element element = document.createElement("ProfileDataInfos");
+
+        Element totalElement = document.createElement("total");
+        totalElement.appendChild(document.createTextNode(Integer.toString(total)));
+        element.appendChild(totalElement);
+
+        for (ProfileDataInfo profileDataInfo : getEntries()) {
+            Element infoElement = profileDataInfo.toDOM(document);
+            element.appendChild(infoElement);
+        }
+
+        for (Link link : getLinks()) {
+            Element linkElement = link.toDOM(document);
+            element.appendChild(linkElement);
+        }
+
+        return element;
     }
 
-    public String getNext() {
-        for (Link link : getLinks()) {
-            if ("next".equals(link. getRelationship())) {
-                return link.getHref().toString();
-            }
-        }
-        return null;
+    public String toXML() throws Exception {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+
+        Element element = toDOM(document);
+        document.appendChild(element);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+        DOMSource domSource = new DOMSource(document);
+        StringWriter sw = new StringWriter();
+        StreamResult streamResult = new StreamResult(sw);
+        transformer.transform(domSource, streamResult);
+
+        return sw.toString();
     }
 
-    public String getPrevious() {
-        for (Link link : getLinks()) {
-            if ("previous".equals(link.getRelationship())) {
-                return link.getHref().toString();
-            }
+    public static ProfileDataInfos fromDOM(Element infosElement) {
+
+        ProfileDataInfos infos = new ProfileDataInfos();
+
+        NodeList totalList = infosElement.getElementsByTagName("total");
+        if (totalList.getLength() > 0) {
+            String value = totalList.item(0).getTextContent();
+            infos.setTotal(Integer.parseInt(value));
         }
-        return null;
+
+        NodeList infoList = infosElement.getElementsByTagName("ProfileDataInfo");
+        int infoCount = infoList.getLength();
+        for (int i=0; i<infoCount; i++) {
+           Element infoElement = (Element) infoList.item(i);
+           ProfileDataInfo info = ProfileDataInfo.fromDOM(infoElement);
+           infos.addEntry(info);
+        }
+
+        NodeList linkList = infosElement.getElementsByTagName("Link");
+        int linkCount = linkList.getLength();
+        for (int i=0; i<linkCount; i++) {
+           Element linkElement = (Element) linkList.item(i);
+           Link link = Link.fromDOM(linkElement);
+           infos.addLink(link);
+        }
+
+        return infos;
+    }
+
+    public static ProfileDataInfos fromXML(String xml) throws Exception {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new InputSource(new StringReader(xml)));
+
+        Element element = document.getDocumentElement();
+        return fromDOM(element);
     }
 }
