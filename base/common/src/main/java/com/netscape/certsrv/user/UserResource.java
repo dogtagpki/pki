b758c038d78a3d95329faa9e76f5a43efd431909
Chris Kelley <ckelley@redhat.com>
2021-09-01 12:47:23 +0100

    Remove UserResource.replaceUser() method
    This REST endpoint is not used internally, nor is it exposed via the PKI
    CLI interface. It is accessible however using curl, with strange
    results, so it is probably safest to simply remove this method.

--- base/common/src/main/java/com/netscape/certsrv/user/UserResource.java
+++ base/common/src/main/java/com/netscape/certsrv/user/UserResource.java
@@ -21,7 +21,6 @@ package com.netscape.certsrv.user;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
@@ -54,10 +53,6 @@ public interface UserResource {
     @GET
     @Path("{userID}")
     public Response getUser(@PathParam("userID") String userID);
-
-    @PUT
-    @Path("{userID}")
-    public Response replaceUser(@PathParam("userID") String userID, UserData userData);
 
     @PATCH
     @Path("{userID}")
