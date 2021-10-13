735e18ecab4e90b3f30c4c8ccfd6db543704957f
Endi S. Dewata <edewata@redhat.com>
2021-10-13 14:47:43 -0500

    Fix missing CA test artifacts
    The ds/pki-artifacts-save.sh scripts have been modified
    to provide options to specify the instance name and the
    output directory and to store the container logs.

--- tests/bin/ds-artifacts-save.sh
+++ tests/bin/ds-artifacts-save.sh
@@ -1,22 +1,58 @@
 #!/bin/bash
 
-CONTAINER=$1
-INSTANCE=$2
+INSTANCE=localhost
+OUTPUT=
 
-if [ "$INSTANCE" == "" ]
+while getopts v-: arg ; do
+    case $arg in
+    -)
+        LONG_OPTARG="${OPTARG#*=}"
+
+        case $OPTARG in
+        instance=?*)
+            INSTANCE="$LONG_OPTARG"
+            ;;
+        output=?*)
+            OUTPUT="$LONG_OPTARG"
+            ;;
+        '')
+            break # "--" terminates argument processing
+            ;;
+        instance* | output*)
+            echo "ERROR: Missing argument for --$OPTARG option" >&2
+            exit 1
+            ;;
+        *)
+            echo "ERROR: Illegal option --$OPTARG" >&2
+            exit 1
+            ;;
+        esac
+        ;;
+    \?)
+        exit 1 # getopts already reported the illegal option
+        ;;
+    esac
+done
+
+# remove parsed options and args from $@ list
+shift $((OPTIND-1))
+
+NAME=$1
+
+if [ "$OUTPUT" == "" ]
 then
-    INSTANCE=localhost
+    OUTPUT=/tmp/artifacts/$NAME
 fi
 
-ARTIFACTS=/tmp/artifacts/$CONTAINER
+docker exec $NAME ls -la /etc/dirsrv
+mkdir -p $OUTPUT/etc
+docker cp $NAME:/etc/dirsrv $OUTPUT/etc
 
-docker exec $CONTAINER ls -la /etc/dirsrv
-mkdir -p $ARTIFACTS/etc
-docker cp $CONTAINER:/etc/dirsrv $ARTIFACTS/etc
+docker exec $NAME ls -la /var/log/dirsrv
+mkdir -p $OUTPUT/var/log
+docker cp $NAME:/var/log/dirsrv $OUTPUT/var/log
 
-docker exec $CONTAINER ls -la /var/log/dirsrv
-mkdir -p $ARTIFACTS/var/log
-docker cp $CONTAINER:/var/log/dirsrv $ARTIFACTS/var/log
+mkdir -p $OUTPUT/var/log/dirsrv/slapd-$INSTANCE
+docker exec $NAME journalctl -u dirsrv@$INSTANCE.service > $OUTPUT/var/log/dirsrv/slapd-$INSTANCE/systemd.log
 
-mkdir -p $ARTIFACTS/var/log/dirsrv/slapd-$INSTANCE
-docker exec $CONTAINER journalctl -u dirsrv@$INSTANCE.service > $ARTIFACTS/var/log/dirsrv/slapd-$INSTANCE/systemd.log
+docker logs $NAME > $OUTPUT/var/log/dirsrv/slapd-$INSTANCE/container.log 2>&1
--- tests/bin/pki-artifacts-save.sh
+++ tests/bin/pki-artifacts-save.sh
@@ -1,23 +1,59 @@
 #!/bin/bash
 
-CONTAINER=$1
-INSTANCE=$2
+INSTANCE=pki-tomcat
+OUTPUT=
 
-if [ "$INSTANCE" == "" ]
+while getopts v-: arg ; do
+    case $arg in
+    -)
+        LONG_OPTARG="${OPTARG#*=}"
+
+        case $OPTARG in
+        instance=?*)
+            INSTANCE="$LONG_OPTARG"
+            ;;
+        output=?*)
+            OUTPUT="$LONG_OPTARG"
+            ;;
+        '')
+            break # "--" terminates argument processing
+            ;;
+        instance* | output*)
+            echo "ERROR: Missing argument for --$OPTARG option" >&2
+            exit 1
+            ;;
+        *)
+            echo "ERROR: Illegal option --$OPTARG" >&2
+            exit 1
+            ;;
+        esac
+        ;;
+    \?)
+        exit 1 # getopts already reported the illegal option
+        ;;
+    esac
+done
+
+# remove parsed options and args from $@ list
+shift $((OPTIND-1))
+
+NAME=$1
+
+if [ "$OUTPUT" == "" ]
 then
-    INSTANCE=pki-tomcat
+    OUTPUT=/tmp/artifacts/$NAME
 fi
 
-ARTIFACTS=/tmp/artifacts/$CONTAINER
+docker exec $NAME ls -la /etc/pki
+mkdir -p $OUTPUT/etc/pki
+docker cp $NAME:/etc/pki/pki.conf $OUTPUT/etc/pki
+docker cp $NAME:/etc/pki/$INSTANCE $OUTPUT/etc/pki
 
-docker exec $CONTAINER ls -la /etc/pki
-mkdir -p $ARTIFACTS/etc/pki
-docker cp $CONTAINER:/etc/pki/pki.conf $ARTIFACTS/etc/pki
-docker cp $CONTAINER:/etc/pki/$INSTANCE $ARTIFACTS/etc/pki
+docker exec $NAME ls -la /var/log/pki
+mkdir -p $OUTPUT/var/log
+docker cp $NAME:/var/log/pki $OUTPUT/var/log
 
-docker exec $CONTAINER ls -la /var/log/pki
-mkdir -p $ARTIFACTS/var/log
-docker cp $CONTAINER:/var/log/pki $ARTIFACTS/var/log
+mkdir -p $OUTPUT/var/log/pki/$INSTANCE
+docker exec $NAME journalctl -u pki-tomcatd@$INSTANCE.service > $OUTPUT/var/log/pki/$INSTANCE/systemd.log
 
-mkdir -p $ARTIFACTS/var/log/pki/$INSTANCE
-docker exec $CONTAINER journalctl -u pki-tomcatd@$INSTANCE.service > $ARTIFACTS/var/log/pki/$INSTANCE/systemd.log
+docker logs $NAME > $OUTPUT/var/log/pki/$INSTANCE/container.log 2>&1
