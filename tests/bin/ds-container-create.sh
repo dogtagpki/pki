4487a8d565d28ded148177649c559a3d2eaabced
Endi S. Dewata <edewata@redhat.com>
2021-10-13 09:02:45 -0500

    Add ds-container-start/stop.sh
    The ds-container-start/stop.sh scripts have been added
    to start/stop DS container.

--- tests/bin/ds-container-create.sh
+++ tests/bin/ds-container-create.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # https://fy.blackhats.net.au/blog/html/2020/03/28/389ds_in_containers.html
+
+SCRIPT_PATH=`readlink -f "$0"`
+SCRIPT_NAME=`basename "$SCRIPT_PATH"`
+SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
 
 NAME=$1
 
@@ -19,50 +23,13 @@ max_wait=60 # seconds
 
 echo "Creating DS container"
 
-docker run \
-    --detach \
+docker create \
     --name=$NAME \
     --hostname=$HOSTNAME \
     -e DS_DM_PASSWORD=$PASSWORD \
     quay.io/389ds/dirsrv > /dev/null
 
-if [ $? -ne 0 ]
-then
-    exit 1
-fi
-
-start_time=$(date +%s)
-
-while :
-do
-    sleep 1
-
-    docker exec $NAME \
-        ldapsearch \
-        -H ldap://$HOSTNAME:3389 \
-        -D "cn=Directory Manager" \
-        -w $PASSWORD \
-        -x \
-        -b "" \
-        -s base > /dev/null 2> /dev/null
-
-    if [ $? -eq 0 ]
-    then
-        echo "DS container is running"
-        break
-    fi
-
-    current_time=$(date +%s)
-    elapsed_time=$(expr $current_time - $start_time)
-
-    if [ $elapsed_time -ge $max_wait ]
-    then
-        echo "DS container did not start after ${max_wait}s"
-        exit 1
-    fi
-
-    echo "Waiting for DS container to start (${elapsed_time}s)"
-done
+$SCRIPT_DIR/ds-container-start.sh $NAME
 
 echo "Creating suffix"
 
@@ -70,11 +37,6 @@ docker exec $NAME \
     dsconf localhost backend create \
         --suffix dc=example,dc=com \
         --be-name userRoot > /dev/null
-
-if [ $? -ne 0 ]
-then
-    exit 1
-fi
 
 echo "Adding base entries"
 
@@ -92,10 +54,5 @@ dn: dc=pki,dc=example,dc=com
 objectClass: domain
 dc: pki
 EOF
-
-if [ $? -ne 0 ]
-then
-    exit 1
-fi
 
 echo "DS container is ready"
