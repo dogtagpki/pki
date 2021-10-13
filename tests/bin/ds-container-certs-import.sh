429553445be471bc81a5a455a18a7d7ed89935b6
Endi S. Dewata <edewata@redhat.com>
2021-10-13 09:02:49 -0500

    Add ds-container-certs-import.sh
    The ds-container-certs-import.sh has been added to import
    certs into DS container.
    
    The ds-container-create.sh has been modified to create the
    folder for importing certs.

---
+++ tests/bin/ds-container-certs-import.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -e
+
+# https://fy.blackhats.net.au/blog/html/2020/03/28/389ds_in_containers.html
+
+NAME=$1
+
+if [ "$NAME" == "" ]
+then
+    echo "Usage: ds-container-certs-import.sh <name> <input>"
+    exit 1
+fi
+
+INPUT=$2
+
+if [ "$INPUT" == "" ]
+then
+    echo "Usage: ds-container-certs-import.sh <name> <input>"
+    exit 1
+fi
+
+if [ "$PASSWORD" == "" ]
+then
+    PASSWORD=Secret.123
+fi
+
+echo "Importing DS certs"
+
+# Import input file into container
+
+docker cp $INPUT $NAME:/tmp/certs.p12
+
+# Fix file ownership
+
+docker exec -u 0 $NAME chown dirsrv.dirsrv /tmp/certs.p12
+
+# Export server cert into /data/tls/server.crt
+
+docker exec $NAME openssl pkcs12 \
+    -in /tmp/certs.p12 \
+    -passin pass:$PASSWORD \
+    -out /data/tls/server.crt \
+    -clcerts \
+    -nokeys
+
+# Export server key into /data/tls/server.key
+
+docker exec $NAME openssl pkcs12 \
+    -in /tmp/certs.p12 \
+    -passin pass:$PASSWORD \
+    -out /data/tls/server.key \
+    -nodes \
+    -nocerts
+
+# Export CA cert into /data/tls/ca/ca.crt
+
+docker exec $NAME openssl pkcs12 \
+    -in /tmp/certs.p12 \
+    -passin pass:$PASSWORD \
+    -out /data/tls/ca/ca.crt \
+    -cacerts \
+    -nokeys
+
+echo "DS certs imported"
