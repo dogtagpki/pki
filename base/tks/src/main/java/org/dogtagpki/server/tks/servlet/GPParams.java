494569f0638194c5e3076aaead32bd111e88a792
Chris Kelley <ckelley@redhat.com>
2021-10-25 17:42:39 +0100

    Tidy up GPParams
    * Make static constants final
    * Decrease visibility of no-op constructor
    * Remove Boolean literals
    * Don't create variables to just return them

--- base/tks/src/main/java/org/dogtagpki/server/tks/servlet/GPParams.java
+++ base/tks/src/main/java/org/dogtagpki/server/tks/servlet/GPParams.java
@@ -25,15 +25,15 @@ package org.dogtagpki.server.tks.servlet;
 
 public class GPParams {
 
-    public static String DIVER_EMV = "emv";
-    public static String DIVER_NONE = "none";
-    public static String DIVER_VISA2 = "visa2";
-    public static String DIVER_GPKMC = "gpkmc";
-    public static String NIST_SP800 = "nistsp_800";
-    public static String AES = "AES";
-    public static String DES3 ="DES3";
+    public static final String DIVER_EMV = "emv";
+    public static final String DIVER_NONE = "none";
+    public static final String DIVER_VISA2 = "visa2";
+    public static final String DIVER_GPKMC = "gpkmc";
+    public static final String NIST_SP800 = "nistsp_800";
+    public static final String AES = "AES";
+    public static final String DES3 ="DES3";
 
-    public GPParams() {
+    protected GPParams() {
     }
 
     // Diversification scheme for all keysets after 1
@@ -65,24 +65,15 @@ public class GPParams {
     }
 
     public boolean isDiversEmv() {
-        if (DIVER_EMV.equalsIgnoreCase(diversificationScheme))
-            return true;
-        else
-            return false;
+        return DIVER_EMV.equalsIgnoreCase(diversificationScheme);
     }
 
     public boolean isDiversVisa2() {
-        if (DIVER_VISA2.equalsIgnoreCase(diversificationScheme))
-            return true;
-        else
-            return false;
+        return DIVER_VISA2.equalsIgnoreCase(diversificationScheme);
     }
 
     public boolean isDiversNone() {
-        if (DIVER_NONE.equalsIgnoreCase(diversificationScheme))
-            return true;
-        else
-            return false;
+        return DIVER_NONE.equalsIgnoreCase(diversificationScheme);
     }
 
     public boolean isVer1DiversGPKMC() {
@@ -90,25 +81,16 @@ public class GPParams {
     }
 
     public boolean isVer1DiversEmv() {
-        if (DIVER_EMV.equalsIgnoreCase(version1DiversificationScheme))
-            return true;
-        else
-            return false;
+        return DIVER_EMV.equalsIgnoreCase(version1DiversificationScheme);
     }
 
     public boolean isVer1DiversVisa2() {
-        if (DIVER_VISA2.equalsIgnoreCase(version1DiversificationScheme))
-            return true;
-        else
-            return false;
+        return DIVER_VISA2.equalsIgnoreCase(version1DiversificationScheme);
 
     }
 
     public boolean isVer1DiversNone() {
-        if (DIVER_NONE.equalsIgnoreCase(version1DiversificationScheme))
-            return true;
-        else
-            return false;
+        return DIVER_NONE.equalsIgnoreCase(version1DiversificationScheme);
     }
 
     public void setDiversificationScheme(String scheme) {
@@ -129,10 +111,8 @@ public class GPParams {
 
     @Override
     public String toString() {
-        String output = " Version1 Diversification Scheme: " + version1DiversificationScheme + " All other versions : "
+        return " Version1 Diversification Scheme: " + version1DiversificationScheme + " All other versions : "
                 + diversificationScheme;
-
-        return output;
     }
 
 }
