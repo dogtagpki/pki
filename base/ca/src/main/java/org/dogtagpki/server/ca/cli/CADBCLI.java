f4a0fb964e60f2b0aabe14c20b8e499db7ea95b3
Endi S. Dewata <edewata@redhat.com>
2021-11-08 21:07:32 -0600

    Add PKISubsystem.setup_replication()
    The Java code that sets up replication in SubsystemDBInitCLI
    has been moved into SubsystemDBReplicationSetupCLI so that
    later it can be reused as a separate CLI. The Python code that
    calls SubsystemDBInitCLI with replication params has been moved
    into setup_replication() to call SubsystemDBReplicationSetupCLI.

--- base/ca/src/main/java/org/dogtagpki/server/ca/cli/CADBCLI.java
+++ base/ca/src/main/java/org/dogtagpki/server/ca/cli/CADBCLI.java
@@ -19,11 +19,12 @@
 package org.dogtagpki.server.ca.cli;
 
 import org.dogtagpki.cli.CLI;
+import org.dogtagpki.server.cli.SubsystemDBAccessCLI;
 import org.dogtagpki.server.cli.SubsystemDBEmptyCLI;
 import org.dogtagpki.server.cli.SubsystemDBInfoCLI;
 import org.dogtagpki.server.cli.SubsystemDBInitCLI;
-import org.dogtagpki.server.cli.SubsystemDBAccessCLI;
 import org.dogtagpki.server.cli.SubsystemDBRemoveCLI;
+import org.dogtagpki.server.cli.SubsystemDBReplicationCLI;
 import org.dogtagpki.server.cli.SubsystemDBVLVCLI;
 
 /**
@@ -41,6 +42,7 @@ public class CADBCLI extends CLI {
         addModule(new CADBUpgradeCLI(this));
 
         addModule(new SubsystemDBAccessCLI(this));
+        addModule(new SubsystemDBReplicationCLI(this));
         addModule(new SubsystemDBVLVCLI(this));
     }
 }
--- base/server/python/pki/server/subsystem.py
+++ base/server/python/pki/server/subsystem.py
@@ -954,10 +954,6 @@ class PKISubsystem(object):
             create_base=False,
             create_containers=False,
             rebuild_indexes=False,
-            setup_replication=False,
-            replication_security=None,
-            replication_port=None,
-            master_replication_port=None,
             as_current_user=False):
 
         cmd = [self.name + '-db-init']
@@ -976,18 +972,6 @@ class PKISubsystem(object):
 
         if rebuild_indexes:
             cmd.append('--rebuild-indexes')
-
-        if setup_replication:
-            cmd.append('--setup-replication')
-
-        if replication_security:
-            cmd.extend(['--replication-security', replication_security])
-
-        if replication_port:
-            cmd.extend(['--replication-port', replication_port])
-
-        if master_replication_port:
-            cmd.extend(['--master-replication-port', master_replication_port])
 
         if logger.isEnabledFor(logging.DEBUG):
             cmd.append('--debug')
@@ -1066,6 +1050,32 @@ class PKISubsystem(object):
             cmd,
             as_current_user=as_current_user,
             capture_output=True)
+
+    def setup_replication(
+            self,
+            master_replication_port=None,
+            replica_replication_port=None,
+            replication_security=None,
+            as_current_user=False):
+
+        cmd = [self.name + '-db-replication-setup']
+
+        if master_replication_port:
+            cmd.extend(['--master-replication-port', master_replication_port])
+
+        if replica_replication_port:
+            cmd.extend(['--replica-replication-port', replica_replication_port])
+
+        if replication_security:
+            cmd.extend(['--replication-security', replication_security])
+
+        if logger.isEnabledFor(logging.DEBUG):
+            cmd.append('--debug')
+
+        elif logger.isEnabledFor(logging.INFO):
+            cmd.append('--verbose')
+
+        self.run(cmd, as_current_user=as_current_user)
 
     def find_vlv(self, as_current_user=False):
 
