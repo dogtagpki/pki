4b04661fa2a9c2b925e2db1cbae60dafa97fc7da
Endi S. Dewata <edewata@redhat.com>
2022-01-14 08:14:37 -0600

    Remove unused find_tus_user_entries_no_vlv()

--- base/tps-client/src/tus/tus_db.c
+++ base/tps-client/src/tus/tus_db.c
@@ -1916,45 +1916,6 @@ static int ldap_sort_entries(LDAP *ld, LDAPMessage **result, const char* attr, L
     return ldap_multisort_entries(ld, result, attr ? attrs : NULL, cmp);
 } 
 
-/**
- * find_tus_user_entries_no_vlv
- * params: filter - ldap search filter
- *         result - hash of LDAP Search results.
- *         order  - 0 (order results increasing by uid), (!=0) order by decreasing uid
- */ 
-TPS_PUBLIC int find_tus_user_entries_no_vlv(char *filter, LDAPMessage **result, int order)
-{
-    int rc = LDAP_OTHER, tries = 0;
-    char peopleBaseDN[256];
-
-    PR_snprintf(peopleBaseDN, 256, "ou=People,%s", userBaseDN);
-    
-    tus_check_conn();
-    for (tries = 0; tries < MAX_RETRIES; tries++) {
-        if ((rc = ldap_search_ext_s (ld, peopleBaseDN, LDAP_SCOPE_ONELEVEL, filter,
-                       userAttributes, 0, NULL, NULL, NULL, 0, result)) == LDAP_SUCCESS) {
-            /* we do client-side sorting here */
-            if (order == 0) {
-                rc = ldap_sort_entries(ld, result, USER_ID, sort_cmp);
-            } else {
-                rc = ldap_sort_entries(ld, result, USER_ID, reverse_sort_cmp);
-            }
-            break;
-        } else if (rc == LDAP_SERVER_DOWN || rc == LDAP_CONNECT_ERROR) {
-            struct berval credential;
-            credential.bv_val = bindPass;
-            credential.bv_len= strlen(bindPass);
-            rc = ldap_sasl_bind_s(ld, bindDN, LDAP_SASL_SIMPLE, &credential, NULL, NULL, NULL);
-            if (rc != LDAP_SUCCESS) {
-                bindStatus = rc;
-                break;
-            }
-        }
-    }
-
-    return rc;
-}
-
 TPS_PUBLIC int find_tus_activity_entries_no_vlv(char *filter, LDAPMessage **result, int order)
 {
     int rc = LDAP_OTHER, tries = 0;
