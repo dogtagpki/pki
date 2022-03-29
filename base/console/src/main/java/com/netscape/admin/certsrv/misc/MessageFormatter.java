e27c4169a2e101c7656e039774501c4a61b8e4f4
Chris Kelley <ckelley@redhat.com>
2022-03-29 17:07:55 +0100

    Deprecate the PKI console com.netscape.admin.certsrv.misc package
    * Add package-info.java to warn about removal at the package level.
    * Add deprecation annotations and javadoc to each class.

--- base/console/src/main/java/com/netscape/admin/certsrv/misc/MessageFormatter.java
+++ base/console/src/main/java/com/netscape/admin/certsrv/misc/MessageFormatter.java
@@ -34,7 +34,9 @@ import java.util.ResourceBundle;
  * @version $Revision$, $Date$
  * @see java.text.MessageFormat
  * @see java.util.ResourceBundle
+ * @deprecated The PKI console will be removed once there are CLI equivalents of desired console features.
  */
+@Deprecated(since="10.14.0", forRemoval=true)
 public class MessageFormatter {
 
     private static final Class<?> [] toStringSignature = { Locale.class };
