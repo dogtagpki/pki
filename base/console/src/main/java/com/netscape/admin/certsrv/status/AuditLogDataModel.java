ff84d5c6423197fc6aa2bac2f8febe92fb6b7ee1
Chris Kelley <ckelley@redhat.com>
2022-03-30 16:43:00 +0100

    Deprecate the PKI console com.netscape.admin.certsrv.status package
    * Add package-info.java to warn about removal at the package level.
    * Add deprecation annotations and javadoc to each class.

--- base/console/src/main/java/com/netscape/admin/certsrv/status/AuditLogDataModel.java
+++ base/console/src/main/java/com/netscape/admin/certsrv/status/AuditLogDataModel.java
@@ -23,7 +23,9 @@ package com.netscape.admin.certsrv.status;
  * @author Jack Pan-Chen
  * @version $Revision$, $Date$
  * @see com.netscape.admin.certsrv.status
+ * @deprecated The PKI console will be removed once there are CLI equivalents of desired console features.
  */
+@Deprecated(since="10.14.0", forRemoval=true)
 public class AuditLogDataModel extends LogDataModel {
 
     /*==========================================================
