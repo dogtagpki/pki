91eaa056a2f924b3050e9102edf23acb5e184635
Chris Kelley <ckelley@redhat.com>
2022-03-30 16:46:54 +0100

    Deprecate the PKI console com.netscape.admin.certsrv.task package
    * Add package-info.java to warn about removal at the package level.
    * Add deprecation annotations and javadoc to each class.

--- base/console/src/main/java/com/netscape/admin/certsrv/task/CMSMigrateCreate.java
+++ base/console/src/main/java/com/netscape/admin/certsrv/task/CMSMigrateCreate.java
@@ -46,7 +46,9 @@ import netscape.ldap.LDAPSearchResults;
  *
  * @author Jack Pan-Chen
  * @version $Revision$, $Date$
+ * @deprecated The PKI console will be removed once there are CLI equivalents of desired console features.
  */
+@Deprecated(since="10.14.0", forRemoval=true)
 public class CMSMigrateCreate extends CGITask
     implements IProductObject
 {
