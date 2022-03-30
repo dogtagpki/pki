eb452eb991c22d02a897ff0f36355173f1f6cd0a
Chris Kelley <ckelley@redhat.com>
2022-03-30 16:53:54 +0100

    Deprecate the PKI console com.netscape.admin.certsrv.wizard package
    * Add package-info.java to warn about removal at the package level.
    * Add deprecation annotations and javadoc to each class.

--- base/console/src/main/java/com/netscape/admin/certsrv/wizard/WizardBasePanel.java
+++ base/console/src/main/java/com/netscape/admin/certsrv/wizard/WizardBasePanel.java
@@ -31,7 +31,9 @@ import java.io.*;
  * @author Jack Pan-Chen
  * @version $Revision$, $Date$
  * @see com.netscape.admin.certsrv.config
+ * @deprecated The PKI console will be removed once there are CLI equivalents of desired console features.
  */
+@Deprecated(since="10.14.0", forRemoval=true)
 public class WizardBasePanel extends CMSBasePanel implements MouseMotionListener,
   ConfigServlet {
 
