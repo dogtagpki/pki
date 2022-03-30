1ea492dda52a15ea884474732c8b08077c9965c2
Chris Kelley <ckelley@redhat.com>
2022-03-30 16:55:17 +0100

    Deprecate the PKI console com.netscape.certsrv.client.connection package
    * Add package-info.java to warn about removal at the package level.
    * Add deprecation annotations and javadoc to each class.

--- base/console/src/main/java/com/netscape/certsrv/client/connection/IAuthenticator.java
+++ base/console/src/main/java/com/netscape/certsrv/client/connection/IAuthenticator.java
@@ -21,6 +21,8 @@ package com.netscape.certsrv.client.connection;
  * An interface represents authentiator.
  *
  * @version $Revision$, $Date$
+ * @deprecated The PKI console will be removed once there are CLI equivalents of desired console features.
  */
+@Deprecated(since="10.14.0", forRemoval=true)
 public interface IAuthenticator {
 }
