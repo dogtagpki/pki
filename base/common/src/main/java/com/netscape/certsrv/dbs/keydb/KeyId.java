18a1d0c4be483347f7635aec2653e811d61ee1ff
Endi S. Dewata <edewata@redhat.com>
2022-04-07 10:41:18 -0500

    Fix KeyId to preserve leading zeros
    Previously KeyId was implemented using a BigInteger which
    does not preserve leading zeros. This could cause a problem
    when searching for a key in NSS database by its ID.
    
    To fix the problem the KeyId has been modified to store the
    ID as a byte array so it will preserve the leading zeros.
    
    The KeyIdTest has been updated to test this scenario.

--- base/common/src/main/java/com/netscape/certsrv/dbs/keydb/KeyId.java
+++ base/common/src/main/java/com/netscape/certsrv/dbs/keydb/KeyId.java
@@ -18,6 +18,10 @@
 package com.netscape.certsrv.dbs.keydb;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -38,89 +42,108 @@ import com.netscape.certsrv.util.JSONSerializer;
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class KeyId implements JSONSerializer {
 
-    protected BigInteger value;
+    protected byte[] value;
 
     /**
      * Creates a new KeyId from its string representation.
-     * <p>
      *
-     * @param id
-     *            a string containing the decimal or hex value for the identifier.
+     * @param id a string containing the decimal or hex value for the identifier.
      */
     public KeyId(String id) {
-        if (id != null) {
-            id = id.trim();
-            if (id.startsWith("0x")) { // hex
-                value = new BigInteger(id.substring(2), 16);
-            } else { // decimal
-                value = new BigInteger(id);
-            }
+
+        if (id == null) {
+            throw new IllegalArgumentException("Missing key ID");
+        }
+
+        id = id.trim();
+
+        if (!id.startsWith("0x")) { // decimal
+            value = new BigInteger(id).toByteArray();
+            return;
+        }
+
+        // hex
+        id = id.substring(2);
+
+        try {
+            value = Hex.decodeHex(id);
+        } catch (DecoderException e) {
+            throw new RuntimeException(e);
         }
     }
 
     /**
-     * Creates a new KeyId from its BigInteger representation.
-     * <p>
+     * Creates a new KeyId from its byte array representation.
      *
-     * @param id
-     *            a BigInteger containing the identifier.
+     * @param id a byte array containing the identifier.
+     */
+    public KeyId(byte[] bytes) {
+        value = bytes;
+    }
+
+    /**
+     * Creates a new KeyId from its BigInteger representation.
+     *
+     * @param id a BigInteger containing the identifier.
      */
     public KeyId(BigInteger id) {
-        value = id;
+        value = id.toByteArray();
     }
 
     /**
      * Creates a new KeyId from its integer representation.
-     * <p>
      *
-     * @param id
-     *            an integer containing the identifier.
+     * @param id an integer containing the identifier.
      */
     public KeyId(int id) {
-        value = BigInteger.valueOf(id);
+        value = BigInteger.valueOf(id).toByteArray();
     }
 
     /**
      * Converts the KeyId into its BigInteger representation.
-     * <p>
      *
-     * @return
-     *         a BigInteger containing the identifier.
+     * @return a BigInteger containing the identifier.
      */
     public BigInteger toBigInteger() {
-        return value;
+        return new BigInteger(value);
     }
 
     /**
      * Converts the KeyId into its string representation. The string
      * form can be stored in a database (such as the LDAP directory)
-     * <p>
      *
-     * @return
-     *         a string containing the decimal (base 10) value for the identifier.
+     * @return a string containing the decimal (base 10) value for the identifier.
      */
     @Override
     public String toString() {
-        return value.toString();
+        return toBigInteger().toString();
     }
 
     /**
      * Converts the KeyId into its hex string representation. The string
      * form can be stored in a database (such as the LDAP directory)
      *
-     * @return
-     *         a string containing the hex (hex 16) value for the identifier.
+     * @return a string containing the hex (hex 16) value for the identifier.
      */
     @JsonValue
     public String toHexString() {
-        return "0x" + value.toString(16);
+        return "0x" + Hex.encodeHexString(value);
+    }
+
+    /**
+     * Converts the KeyId into its byte array representation.
+     *
+     * @return a byte array containing the identifier.
+     */
+    public byte[] toByteArray() {
+        return value;
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        result = prime * result + Arrays.hashCode(value);
         return result;
     }
 
@@ -133,10 +156,7 @@ public class KeyId implements JSONSerializer {
         if (getClass() != obj.getClass())
             return false;
         KeyId other = (KeyId) obj;
-        if (value == null) {
-            if (other.value != null)
-                return false;
-        } else if (!value.equals(other.value))
+        if (!Arrays.equals(value, other.value))
             return false;
         return true;
     }
--- base/common/src/test/java/com/netscape/certsrv/dbs/keydb/KeyIdTest.java
+++ base/common/src/test/java/com/netscape/certsrv/dbs/keydb/KeyIdTest.java
@@ -7,19 +7,84 @@ import com.netscape.certsrv.util.JSONSerializer;
 
 public class KeyIdTest {
 
-    private static KeyId before = new KeyId("0x6");
+    // data #1: zero
+    String DATA1_HEX = "0x00";
+    String DATA1_JSON = "\"0x00\"";
+    byte[] DATA1_BYTES = new byte[] {
+            (byte) 0x00
+    };
+
+    // data #2: long zero
+    String DATA2_HEX = "0x00000000";
+    String DATA2_JSON = "\"0x00000000\"";
+    byte[] DATA2_BYTES = new byte[] {
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00
+    };
+
+    // data #3: 1-byte number
+    String DATA3_HEX = "0x06";
+    String DATA3_JSON = "\"0x06\"";
+    byte[] DATA3_BYTES = new byte[] {
+            (byte) 0x06
+    };
+
+    // data #4: 16-byte number
+    String DATA4_HEX = "0xa8405e456904f79ccb9118196023153b";
+    String DATA4_JSON = "\"0xa8405e456904f79ccb9118196023153b\"";
+    byte[] DATA4_BYTES = new byte[] {
+            (byte) 0xa8, (byte) 0x40, (byte) 0x5e, (byte) 0x45,
+            (byte) 0x69, (byte) 0x04, (byte) 0xf7, (byte) 0x9c,
+            (byte) 0xcb, (byte) 0x91, (byte) 0x18, (byte) 0x19,
+            (byte) 0x60, (byte) 0x23, (byte) 0x15, (byte) 0x3b
+    };
+
+    // data #5: 20-byte number with leading zero
+    String DATA5_HEX = "0x004128630eae9cdaf342b21e55571019c4f06368";
+    String DATA5_JSON = "\"0x004128630eae9cdaf342b21e55571019c4f06368\"";
+    byte[] DATA5_BYTES = new byte[] {
+            (byte) 0x00, (byte) 0x41, (byte) 0x28, (byte) 0x63,
+            (byte) 0x0e, (byte) 0xae, (byte) 0x9c, (byte) 0xda,
+            (byte) 0xf3, (byte) 0x42, (byte) 0xb2, (byte) 0x1e,
+            (byte) 0x55, (byte) 0x57, (byte) 0x10, (byte) 0x19,
+            (byte) 0xc4, (byte) 0xf0, (byte) 0x63, (byte) 0x68
+    };
+
+    Object[][] TEST_DATA = {
+            new Object[] { DATA1_HEX, DATA1_JSON, DATA1_BYTES },
+            new Object[] { DATA2_HEX, DATA2_JSON, DATA2_BYTES },
+            new Object[] { DATA3_HEX, DATA3_JSON, DATA3_BYTES },
+            new Object[] { DATA4_HEX, DATA4_JSON, DATA4_BYTES },
+            new Object[] { DATA5_HEX, DATA5_JSON, DATA5_BYTES }
+    };
 
     @Test
-    public void testJSON() throws Exception {
-        // Act
-        String json = before.toJSON();
-        System.out.println("JSON (before): " + json);
+    public void run() throws Exception {
 
-        KeyId afterJSON = JSONSerializer.fromJSON(json, KeyId.class);
-        System.out.println("JSON (after): " + afterJSON.toJSON());
+        for (int i = 0; i < TEST_DATA.length; i++) {
+            System.out.println("Testing data #" + (i + 1));
 
-        // Assert
-        Assert.assertEquals(before, afterJSON);
+            // convert hex string into KeyId
+            String hex = (String) TEST_DATA[i][0];
+            KeyId keyID = new KeyId(hex);
+
+            // convert KeyId into hex string
+            Assert.assertEquals(hex, keyID.toHexString());
+
+            // convert KeyId into JSON
+            String json = (String) TEST_DATA[i][1];
+            Assert.assertEquals(json, keyID.toJSON());
+
+            // convert JSON into KeyId
+            KeyId afterJSON = JSONSerializer.fromJSON(json, KeyId.class);
+            Assert.assertEquals(keyID, afterJSON);
+
+            // convert KeyId into bytes
+            byte[] bytes = (byte[]) TEST_DATA[i][2];
+            Assert.assertArrayEquals(bytes, keyID.toByteArray());
+
+            // convert bytes into KeyId
+            KeyId afterBytes = new KeyId(bytes);
+            Assert.assertEquals(keyID, afterBytes);
+        }
     }
-
 }
