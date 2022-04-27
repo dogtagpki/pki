07b0e124d2eb7d77f585353d61cc3c99f96adcbc
Chris Kelley <ckelley@redhat.com>
2022-04-27 18:10:02 +0100

    Fix indentation and declare a var in cert.js
    Also fix re-ordering of some modifiers in ConfirmRecoverBySerial

--- base/kra/src/main/java/com/netscape/cms/servlet/key/ConfirmRecoverBySerial.java
+++ base/kra/src/main/java/com/netscape/cms/servlet/key/ConfirmRecoverBySerial.java
@@ -56,7 +56,7 @@ public class ConfirmRecoverBySerial extends CMSServlet {
      *
      */
     private static final long serialVersionUID = 2221819191344494389L;
-    private final static String INFO = "recoverBySerial";
+    private static final String INFO = "recoverBySerial";
     private final static String TPL_FILE =
             "confirmRecoverBySerial.template";
 
