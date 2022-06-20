952e2f5ccba0ce07cc83f983d14f3118ae8ecfd3
Endi S. Dewata <edewata@redhat.com>
2022-06-20 09:47:42 -0500

    Fix missing SKI in CSR
    The NSSCertRequestCLI has been modified to provide a public key
    to the NSSExtensionGenerator such that it can be used to generate
    an SKI extension for the CSR.
    
    The NSSExtensionGenerator.createExtensions() has been extended
    to support CSR (with subject key) and cert (with issuer and
    request).
    
    The NSSExtensionGenerator.createSKIDExtension() has been modified
    to take a subject key instead of PKCS10 object.
    
    The preop.cert.signing.subject_key_id config param is no longer
    needed since the value is only used within the same process (i.e.
    pkispawn) so it has been removed.
    
    The CA test has been updated to validate pki_req_ski=DEFAULT. The
    support for pki_req_ski=<hex> will be restored separately later.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2099312

--- tests/ca/bin/test-ca-signing-csr-ext.sh
+++ tests/ca/bin/test-ca-signing-csr-ext.sh
@@ -8,13 +8,13 @@ fi
 
 openssl req -text -noout -in $INPUT | tee output
 
-# verfiy basic constraints extension
+# verify basic constraints extension
 echo "X509v3 Basic Constraints: critical" > expected
 echo "CA:TRUE" >> expected
 sed -En 'N; s/^ *(X509v3 Basic Constraints: .*)\n *(.*)$/\1\n\2/p; D' output | tee actual
 diff actual expected
 
-# verfiy key usage extension
+# verify key usage extension
 echo "X509v3 Key Usage: critical" > expected
 echo "Digital Signature, Non Repudiation, Certificate Sign, CRL Sign" >> expected
 sed -En 'N; s/^ *(X509v3 Key Usage: .*)\n *(.*)$/\1\n\2/p; D' output | tee actual
