3c74ba36667f620167899d987764e21ad6cbe22e
Fraser Tweedale <ftweedal@redhat.com>
2022-06-30 17:13:50 +1000

    EST: implement stub RA backend
    Implement a stub RA backend that returns 404 (/cacerts) or 500
    (/simpleenroll and /simplereenroll).  The behaviour to communicate
    with Dogtag CA subsystem will be added in a later commit.
    
    Part of: https://github.com/dogtagpki/pki/issues/3297

---
+++ base/est/src/main/java/org/dogtagpki/est/DogtagRABackend.java
@@ -0,0 +1,34 @@
+package org.dogtagpki.est;
+
+import javax.ws.rs.ServiceUnavailableException;
+
+import java.security.cert.X509Certificate;
+import java.util.Optional;
+
+import org.mozilla.jss.netscape.security.pkcs.PKCS10;
+import org.mozilla.jss.netscape.security.x509.CertificateChain;
+
+/**
+ * EST backend that acts as RA for a Dogtag CA subsystem
+ *
+ * @author Fraser Tweedale
+ */
+public class DogtagRABackend implements ESTBackend {
+
+    @Override
+    public CertificateChain cacerts(Optional<String> label) {
+        // TODO use label to select LWCA;
+        return null;
+    }
+
+    @Override
+    public ESTEnrollResult simpleenroll(Optional<String> label, PKCS10 csr) {
+        return ESTEnrollResult.failure(new ServiceUnavailableException("not implemented"));
+    }
+
+    @Override
+    public ESTEnrollResult simplereenroll(Optional<String> label, PKCS10 csr) {
+        return ESTEnrollResult.failure(new ServiceUnavailableException("not implemented)"));
+    }
+
+}
