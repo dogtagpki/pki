6f403a03d4094e8bceb5a99135636dd0fbb9d6e8
Fraser Tweedale <ftweedal@redhat.com>
2022-07-07 16:10:28 +1000

    EST: implement backend configuration
    Read backend configuration from <instance-dir>/est/backend.conf.
    This includes the "class" property which specifies the backend
    implementation to use.  Other properties are loaded and made
    available to the backend implementation.  In this way, different
    implementations may recognise or require different configuration
    properties.
    
    Part of: https://github.com/dogtagpki/pki/issues/3297

---
+++ base/est/src/main/java/org/dogtagpki/est/ESTBackendConfig.java
@@ -0,0 +1,87 @@
+//
+// Copyright Red Hat, Inc.
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+package org.dogtagpki.est;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+public class ESTBackendConfig {
+
+    private String className = null;
+
+    private Map<String, String> parameters = new LinkedHashMap<>();
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Map<String, String> parameters) {
+        this.parameters.clear();
+        this.parameters.putAll(parameters);
+    }
+
+    public Collection<String> getParameterNames() {
+        return parameters.keySet();
+    }
+
+    public Collection<String> getParameterNames(String parent) {
+
+        String prefix = parent + ".";
+        int length = prefix.length();
+
+        return parameters.keySet().stream()
+            .filter(name -> name.startsWith(prefix))
+            .map(name -> name.substring(length))
+            .collect(Collectors.toSet());
+    }
+
+    public String getParameter(String name) {
+        return parameters.get(name);
+    }
+
+    public void setParameter(String name, String value) {
+        parameters.put(name, value);
+    }
+
+    public String removeParameter(String name) {
+        return parameters.remove(name);
+    }
+
+    public static ESTBackendConfig fromProperties(Properties props) throws Exception {
+
+        ESTBackendConfig config = new ESTBackendConfig();
+
+        for (Entry<Object, Object> entry : props.entrySet()) {
+
+            String key = entry.getKey().toString();
+            String value = entry.getValue().toString();
+
+            if (key.equals("class")) {
+                config.setClassName(value);
+            } else {
+                config.setParameter(key, value);
+            }
+        }
+
+        if (config.getClassName() == null) {
+            throw new RuntimeException("ESTBackendConfig: missing 'class' property");
+        }
+
+        return config;
+    }
+}
