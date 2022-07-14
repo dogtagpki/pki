66749316ace4a47cd0604b260a8787a7c562d89b
Fraser Tweedale <ftweedal@redhat.com>
2022-07-14 21:20:57 +1000

    EST: implement /cacerts in Dogtag RA backend
    Implement the /cacerts method in the Dogtag RA backend.
    
    The implementation includes a substantial amount of code to prepare
    the configuration for the PKIClient.  Parameters are read from the
    ESTBackendConfig following a similar pattern to the ACME PKIIssuer.
    
    The code to actually retrieve the certificate chain from the CA
    subsystem is quite succinct.  The AuthorityClient retrieves the
    chain as a PEM-encoded PKCS #7 object.  We then parse the response
    and extract the certificates.  Nevermind that the EST front-end will
    then put those certificates in a new PKCS #7 object and serialise
    it.  Fundamentally it is unnecessary work.  But the API is cleaner
    and more understandable through the use of proper data types rather
    than strings and byte arrays.
    
    Part of: https://github.com/dogtagpki/pki/issues/3297

--- base/est/src/main/java/org/dogtagpki/est/DogtagRABackend.java
+++ base/est/src/main/java/org/dogtagpki/est/DogtagRABackend.java
@@ -1,12 +1,21 @@
 package org.dogtagpki.est;
 
-import javax.ws.rs.ServiceUnavailableException;
-
+import java.io.File;
+import java.io.FileReader;
 import java.security.cert.X509Certificate;
 import java.util.Optional;
+import java.util.Properties;
 
+import javax.ws.rs.ServiceUnavailableException;
+
+import org.mozilla.jss.netscape.security.pkcs.PKCS7;
 import org.mozilla.jss.netscape.security.pkcs.PKCS10;
 import org.mozilla.jss.netscape.security.x509.CertificateChain;
+
+import com.netscape.certsrv.authority.AuthorityClient;
+import com.netscape.certsrv.authority.AuthorityResource;
+import com.netscape.certsrv.client.ClientConfig;
+import com.netscape.certsrv.client.PKIClient;
 
 /**
  * EST backend that acts as RA for a Dogtag CA subsystem
@@ -15,10 +24,75 @@ import org.mozilla.jss.netscape.security.x509.CertificateChain;
  */
 public class DogtagRABackend extends ESTBackend {
 
+    public static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DogtagRABackend.class);
+
+    private ClientConfig clientConfig = new ClientConfig();
+
     @Override
-    public CertificateChain cacerts(Optional<String> label) {
-        // TODO use label to select LWCA;
-        return null;
+    public void start() throws Throwable {
+        logger.info("Initializing Dogtag RA backend");
+
+        String url = config.getParameter("url");
+        logger.info("- URL: " + url);
+        clientConfig.setServerURL(url);
+
+        String nickname = config.getParameter("nickname");
+        if (nickname != null) {
+            logger.info("- nickname: " + nickname);
+            clientConfig.setCertNickname(nickname);
+        }
+
+        String username = config.getParameter("username");
+        if (username != null) {
+            logger.info("- username: " + username);
+            clientConfig.setUsername(username);
+        }
+
+        String password = null;
+        String passwordFilename = config.getParameter("passwordFile");
+        if (passwordFilename != null) {
+            // read password from file
+            File passwordFile = new File(passwordFilename);
+
+            logger.info("Loading password file " + passwordFilename);
+            Properties props = new Properties();
+            try (FileReader reader = new FileReader(passwordFile)) {
+                props.load(reader);
+            }
+            password = props.getProperty("password");
+            if (password == null) {
+                throw new RuntimeException(
+                    "DogtagRABackend: 'password' property not present in file "
+                    + passwordFilename
+                );
+            }
+        } else {
+            password = config.getParameter("password");
+        }
+
+        // password might be null if we are using certificate authn;
+        // we only need to set it in the client config if != null
+        if (password != null) {
+            clientConfig.setPassword(password);
+        }
+    }
+
+    @Override
+    public CertificateChain cacerts(Optional<String> label) throws Throwable {
+        try (PKIClient pkiClient = new PKIClient(clientConfig)) {
+            AuthorityClient authorityClient = new AuthorityClient(pkiClient, "ca");
+
+            String authorityID = label.orElse(AuthorityResource.HOST_AUTHORITY);
+            String pkcs7pem = authorityClient.getChainPEM(authorityID);
+            logger.debug("Cert chain:\n" + pkcs7pem);
+
+            PKCS7 pkcs7 = new PKCS7(pkcs7pem);
+            X509Certificate[] certs = pkcs7.getCertificates();
+            if (certs == null || certs.length == 0) {
+                return null;  // will result in 404
+            }
+            return new CertificateChain(certs);
+        }
     }
 
     @Override
