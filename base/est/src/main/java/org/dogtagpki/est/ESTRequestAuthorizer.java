fb54b61be0cb9101e8172fc2392f404d3a8328c2
Fraser Tweedale <ftweedal@redhat.com>
2022-08-19 15:48:09 +1000

    EST: add authorization interface
    Add the EST authorization interface.  Design sketch:
    
    - abstract class ESTRequestAuthorizer defines the authorization
      interface.  Each enrollment method (currently: simpleenroll and
      simplereenroll) has its own authorization method.
    
    - The arguments of the authorization methods are an
      ESTRequestAuthorizationData and the CSR.
    
    - ESTRequestAuthorizationData contains common data used for making
      authorization decisions, including:
    
      - authenticated principal
      - client IP address
      - EST path label (if any)
    
    - ESTEngine instantiates and initializes a concrete subclasses of
      ESTRequestAuthorizer according to configuration.
    
    - ESTFrontend enrollment methods construct the
      ESTRequestAuthorization data, accesses the ESTRequestAuthorizer
      via the ESTEngine and invokes the relevant authorization method.
    
    - (Next commit) ESTFrontend passes the Object returned by the
      authorization method to the ESTBackend enrollment method.  In this
      way, arbitrary data generated by the authorizer implementation can
      be consumed by the ESTBackend implementation.
    
    Subsequent commits will implement the authorization result Object
    propagation, and provide an authorizer implementation that invokes
    an external process to compute the authorization result.
    
    Part of: https://github.com/dogtagpki/pki/issues/3297

---
+++ base/est/src/main/java/org/dogtagpki/est/ESTRequestAuthorizer.java
@@ -0,0 +1,53 @@
+//
+// Copyright Red Hat, Inc.
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+package org.dogtagpki.est;
+
+import org.mozilla.jss.netscape.security.pkcs.PKCS10;
+
+import com.netscape.certsrv.base.PKIException;
+
+/**
+ * The EST authorization backend interface.
+ *
+ * @author Fraser Tweedale
+ */
+public abstract class ESTRequestAuthorizer {
+
+    void start() throws Throwable { }
+
+    void stop() throws Throwable { }
+
+    ESTRequestAuthorizerConfig config;
+
+    public void setConfig(ESTRequestAuthorizerConfig config) {
+        this.config = config;
+    }
+
+    /**
+     * Authorize a simpleenroll request
+     *
+     * @throws ForbiddenException on authorization failure
+     * @throws PKIException on error
+     * @return on success, an Object, which will be passed to the
+     *         issuance backend (null allowed)
+     */
+    public abstract Object authorizeSimpleenroll(
+        ESTRequestAuthorizationData data, PKCS10 csr)
+            throws PKIException;
+
+    /**
+     * Authorize a simplereenroll request
+     *
+     * @throws ForbiddenException on authorization failure
+     * @throws PKIException on error
+     * @return on success, an Object, which will be passed to the
+     *         issuance backend (null allowed)
+     */
+    public abstract Object authorizeSimplereenroll(
+        ESTRequestAuthorizationData data, PKCS10 csr)
+            throws PKIException;
+
+}
