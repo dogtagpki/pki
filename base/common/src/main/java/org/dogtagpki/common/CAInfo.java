45968b4ada7c93c131e9a34cb052f1eb07fc6cfc
Jack Magne <jmagne@localhost.localdomain>
2022-09-21 11:00:28 -0700

    Fix: Bug 2122409 - pki-tomcat/kra unable to decrypt when using RSA-OAEP padding in RHEL9 with FIPS enabled
    The purpose of this patch is to continue the improvement of this bug in 2 ways:
    
    1. Create a pikspawn variable to cause pkispawn to create a subsystem configured for oaep
    
    pki_use_oaep_rsa_keywrap=True, the default is False.
    
    2. Improve the rest calls for kra info and ca info to provide info on whether the ca or kra is using OAEP.
    
    For the Ca, we print out oaep info for both the local CA config and the CA's corresponding KRA.
    
    Ex:
    
    KRA info:
    
    https://localhost.localdomain:28443/kra/rest/info
    
    <KRAInfo>
    <Attributes/>
    <ArchivalMechanism>keywrap</ArchivalMechanism>
    <RecoveryMechanism>keywrap</RecoveryMechanism>
    <EncryptionAlgorithm>AES/CBC/PKCS5Padding</EncryptionAlgorithm>
    <WrapAlgorithm>AES KeyWrap/Padding</WrapAlgorithm>
    <RsaPublicKeyWrapAlgorithm>RSA_OAEP</RsaPublicKeyWrapAlgorithm>
    </KRAInfo>
    
    Note the new value for RsaPublicKeyWrapAlgorithm.
    
    CA info:
    
    https://localhost.localdomain:8443/ca/rest/info
    
    <CAInfo>
    <Attributes/>
    <ArchivalMechanism>keywrap</ArchivalMechanism>
    <EncryptionAlgorithm>AES/CBC/PKCS5Padding</EncryptionAlgorithm>
    <WrapAlgorithm>AES KeyWrap/Padding</WrapAlgorithm>
    <RsaPublicKeyWrapAlgorithm>RSA_OAEP</RsaPublicKeyWrapAlgorithm>
    <CaRsaPublicKeyWrapAlgorithm>RSA_OAEP</CaRsaPublicKeyWrapAlgorithm>
    </CAInfo>
    
    The value CARsaPublicKeyWrapAlgorithm simply relfects the CA's CS.cfg oaep value.
    The value RsaPublicKeyWrapAlgorithm is part of the info obtained from this CS's KRA subsystem.
    
    This info can be used by interested clients to see if OAEP is in use with the givne KRA or CA.

--- base/common/src/main/java/org/dogtagpki/common/CAInfo.java
+++ base/common/src/main/java/org/dogtagpki/common/CAInfo.java
@@ -18,9 +18,16 @@
 
 package org.dogtagpki.common;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.netscape.certsrv.base.RESTMessage;
 
 /**
@@ -32,11 +39,15 @@ public class CAInfo extends RESTMessage {
 
     public static final String ENCRYPT_MECHANISM = "encrypt";
     public static final String KEYWRAP_MECHANISM = "keywrap";
+    public static final String RSA_PUBLIC_KEY_WRAP = "RSA";
 
     String archivalMechanism;
     String encryptAlgorithm;
     String keyWrapAlgorithm;
+    String rsaPublicKeyWrapAlgorithm;
+    String caRsaPublicKeyWrapAlgorithm;
 
+    @JsonProperty("ArchivalMechanism")
     public String getArchivalMechanism() {
         return archivalMechanism;
     }
@@ -45,6 +56,7 @@ public class CAInfo extends RESTMessage {
         this.archivalMechanism = archivalMechanism;
     }
 
+    @JsonProperty("EncryptionAlgorithm")
     public String getEncryptAlgorithm() {
         return encryptAlgorithm;
     }
@@ -53,6 +65,7 @@ public class CAInfo extends RESTMessage {
         this.encryptAlgorithm = encryptAlgorithm;
     }
 
+    @JsonProperty("KeyWrapAlgorithm")
     public String getKeyWrapAlgorithm() {
         return keyWrapAlgorithm;
     }
@@ -61,6 +74,25 @@ public class CAInfo extends RESTMessage {
         this.keyWrapAlgorithm = keyWrapAlgorithm;
     }
 
+    @JsonProperty("RsaPublicKeyWrapAlgorithm")
+    public String getRsaPublicKeyWrapAlgorithm() {
+        return rsaPublicKeyWrapAlgorithm;
+    }
+
+    public void setRsaPublicKeyWrapAlgorithm(String rsaPublicKeyWrapAlgorithm) {
+        this.rsaPublicKeyWrapAlgorithm = rsaPublicKeyWrapAlgorithm;
+    }
+
+    @JsonProperty("CaRsaPublicKeyWrapAlgorithm")
+    public String getCaRsaPublicKeyWrapAlgorithm() {
+        return caRsaPublicKeyWrapAlgorithm;
+    }
+
+    public void setCaRsaPublicKeyWrapAlgorithm(String caRsaPublicKeyWrapAlgorithm) {
+        this.caRsaPublicKeyWrapAlgorithm = caRsaPublicKeyWrapAlgorithm;
+    }
+
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -68,6 +100,8 @@ public class CAInfo extends RESTMessage {
         result = prime * result + ((archivalMechanism == null) ? 0 : archivalMechanism.hashCode());
         result = prime * result + ((encryptAlgorithm == null) ? 0 : encryptAlgorithm.hashCode());
         result = prime * result + ((keyWrapAlgorithm == null) ? 0 : keyWrapAlgorithm.hashCode());
+        result = prime * result + ((rsaPublicKeyWrapAlgorithm == null) ? 0 : rsaPublicKeyWrapAlgorithm.hashCode());
+        result = prime * result + ((caRsaPublicKeyWrapAlgorithm == null) ? 0 : caRsaPublicKeyWrapAlgorithm.hashCode());
         return result;
     }
 
@@ -93,10 +127,95 @@ public class CAInfo extends RESTMessage {
         if (keyWrapAlgorithm == null) {
             if (other.keyWrapAlgorithm != null)
                 return false;
-        } else if (!keyWrapAlgorithm.equals(other.keyWrapAlgorithm))
+        } else if (!keyWrapAlgorithm.equals(other.keyWrapAlgorithm)) {
             return false;
+        } else if (!rsaPublicKeyWrapAlgorithm.equals(other.rsaPublicKeyWrapAlgorithm)) {
+            return false;
+        } else if (!caRsaPublicKeyWrapAlgorithm.equals(other.caRsaPublicKeyWrapAlgorithm)) {
+            return false;
+        }
+
         return true;
     }
 
+
+    public Element toDOM(Document document) {
+
+        Element infoElement = document.createElement("CAInfo");
+
+        toDOM(document, infoElement);
+
+        if (archivalMechanism != null) {
+            Element archivalElement = document.createElement("ArchivalMechanism");
+            archivalElement.appendChild(document.createTextNode(archivalMechanism));
+            infoElement.appendChild(archivalElement);
+        }
+
+        if (encryptAlgorithm != null) {
+            Element encryptElement = document.createElement("EncryptionAlgorithm");
+            encryptElement.appendChild(document.createTextNode(encryptAlgorithm));
+            infoElement.appendChild(encryptElement);
+        }
+
+        if (keyWrapAlgorithm != null) {
+            Element wrapElement = document.createElement("WrapAlgorithm");
+            wrapElement.appendChild(document.createTextNode(keyWrapAlgorithm));
+            infoElement.appendChild(wrapElement);
+        }
+
+        if (rsaPublicKeyWrapAlgorithm != null) {
+            Element rsaPublicWrapElement = document.createElement("RsaPublicKeyWrapAlgorithm");
+            rsaPublicWrapElement.appendChild(document.createTextNode(rsaPublicKeyWrapAlgorithm));
+            infoElement.appendChild(rsaPublicWrapElement);
+        }
+
+        if(caRsaPublicKeyWrapAlgorithm != null) {
+            Element caRsaPublicWrapElement = document.createElement("CaRsaPublicKeyWrapAlgorithm");
+            caRsaPublicWrapElement.appendChild(document.createTextNode(caRsaPublicKeyWrapAlgorithm));
+            infoElement.appendChild(caRsaPublicWrapElement);
+        }
+
+
+        return infoElement;
+    }
+
+    public static CAInfo fromDOM(Element infoElement) {
+
+        CAInfo info = new CAInfo();
+
+        fromDOM(infoElement, info);
+
+        NodeList archivalList = infoElement.getElementsByTagName("ArchivalMechanism");
+        if (archivalList.getLength() > 0) {
+            String value = archivalList.item(0).getTextContent();
+            info.setArchivalMechanism(value);
+        }
+
+        NodeList encryptionList = infoElement.getElementsByTagName("EncryptionAlgorithm");
+        if (encryptionList.getLength() > 0) {
+            String value = encryptionList.item(0).getTextContent();
+            info.setEncryptAlgorithm(value);
+        }
+
+        NodeList wrapList = infoElement.getElementsByTagName("KeyWrapAlgorithm");
+        if (wrapList.getLength() > 0) {
+            String value = wrapList.item(0).getTextContent();
+            info.setKeyWrapAlgorithm(value);
+        }
+
+        NodeList rsaPublicKeyWrapList = infoElement.getElementsByTagName("RsaPublicKeyWrapAlgorithm");
+        if (rsaPublicKeyWrapList.getLength() > 0) {
+            String value = rsaPublicKeyWrapList.item(0).getTextContent();
+            info.setRsaPublicKeyWrapAlgorithm(value);
+        }
+
+        NodeList caRsaPublicKeyWrapList = infoElement.getElementsByTagName("CaRsaPublicKeyWrapAlgorithm");
+        if (caRsaPublicKeyWrapList.getLength() > 0) {
+            String value = caRsaPublicKeyWrapList.item(0).getTextContent();
+            info.setCaRsaPublicKeyWrapAlgorithm(value);
+        }
+
+        return info;
+    }
 }
 
