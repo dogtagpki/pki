40e5b51c1828742a05b7791b1d3203c403fdddcd
Endi S. Dewata <edewata@redhat.com>
2022-10-05 22:14:24 -0500

    Add job owner
    The jobs have been modified to support an optional owner. An
    owner can only access the jobs they own, while an admin can
    access all jobs.
    
    The JobResource has been modified to no longer use @RolesAllowed.
    Instead, the JobService has been modified to validate the
    authorization inside the method implementation.

--- base/common/src/main/java/org/dogtagpki/job/JobResource.java
+++ base/common/src/main/java/org/dogtagpki/job/JobResource.java
@@ -5,7 +5,6 @@
 //
 package org.dogtagpki.job;
 
-import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -18,16 +17,31 @@ import com.netscape.certsrv.base.EBaseException;
  * @author Endi S. Dewata
  */
 @Path("jobs")
-@RolesAllowed("Administrators")
 public interface JobResource {
 
+    /**
+     * Returns all jobs.
+     *
+     * If the method is executed by an admin, it will return all jobs.
+     * Otherwise, it will return all jobs owned by the user.
+     */
     @GET
     public Response findJobs() throws EBaseException;
 
+    /**
+     * Returns a specific job.
+     *
+     * This method can only be executed by an admin or the job owner.
+     */
     @GET
     @Path("{id}")
     public Response getJob(@PathParam("id") String id) throws EBaseException;
 
+    /**
+     * Starts a specific job.
+     *
+     * This method can only be executed by an admin or the job owner.
+     */
     @POST
     public Response startJob(String id) throws EBaseException;
 }
