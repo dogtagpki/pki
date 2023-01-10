84be1e6fa18a390cda4fa62d5defb508fad8f99f
Endi S. Dewata <edewata@redhat.com>
2023-01-10 19:47:32 +0700

    Add init job

--- tests/bin/init-workflow.sh
+++ tests/bin/init-workflow.sh
@@ -1,21 +1,38 @@
 #!/bin/bash -e
 
-if [ "$BASE64_MATRIX" == "" ]
+################################################################################
+# Base image
+
+if [ "$BASE64_OS" != "" ]
 then
-    MATRIX="{\"os\":[\"34\"]}"
+    OS_VERSION=$(echo "$BASE64_OS" | base64 -d)
 else
-    MATRIX=$(echo "$BASE64_MATRIX" | base64 -d)
+    OS_VERSION=34
 fi
 
-echo "MATRIX: $MATRIX"
-echo "matrix=$MATRIX" >> $GITHUB_OUTPUT
+BASE_IMAGE=registry.fedoraproject.org/fedora:$OS_VERSION
+echo "BASE_IMAGE: $BASE_IMAGE"
+echo "base-image=$BASE_IMAGE" >> $GITHUB_OUTPUT
 
-if [ "$BASE64_REPO" == "" ]
+################################################################################
+# COPR repository
+
+if [ "$BASE64_REPO" != "" ]
 then
-    REPO="@pki/10.13"
-else
     REPO=$(echo "$BASE64_REPO" | base64 -d)
 fi
 
 echo "REPO: $REPO"
 echo "repo=$REPO" >> $GITHUB_OUTPUT
+
+################################################################################
+# Database image
+
+if [ "$BASE64_DATABASE" != "" ]
+then
+    DATABASE=$(echo "$BASE64_DATABASE" | base64 -d)
+    DB_IMAGE=$(echo "$DATABASE" | jq -r .image)
+fi
+
+echo "DB_IMAGE: $DB_IMAGE"
+echo "db-image=$DB_IMAGE" >> $GITHUB_OUTPUT
