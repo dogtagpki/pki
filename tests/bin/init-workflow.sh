fe085b80dd5c87ca2cffe4df6004ab50cddada11
Endi S. Dewata <edewata@redhat.com>
2023-01-17 06:52:00 -0600

    Add support for container registry configuration
    The publish job has been modified to support container registry
    configuration and will only run if the registry is configured.
    The job will also no longer publish the runner image since it's
    only used internally.
    
    https://github.com/dogtagpki/pki/wiki/Configuring-Container-Registry

--- tests/bin/init-workflow.sh
+++ tests/bin/init-workflow.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+################################################################################
+# Base image
+
 if [ "$BASE64_OS" != "" ]
 then
     OS_VERSION=$(echo "$BASE64_OS" | base64 -d)
@@ -11,6 +14,9 @@ BASE_IMAGE=registry.fedoraproject.org/fedora:$OS_VERSION
 echo "BASE_IMAGE: $BASE_IMAGE"
 echo "base-image=$BASE_IMAGE" >> $GITHUB_OUTPUT
 
+################################################################################
+# COPR repository
+
 if [ "$BASE64_REPO" == "" ]
 then
     REPO=""
@@ -20,3 +26,32 @@ fi
 
 echo "REPO: $REPO"
 echo "repo=$REPO" >> $GITHUB_OUTPUT
+
+################################################################################
+# Database image
+
+if [ "$BASE64_DATABASE" != "" ]
+then
+    DATABASE=$(echo "$BASE64_DATABASE" | base64 -d)
+    DB_IMAGE=$(echo "$DATABASE" | jq -r .image)
+fi
+
+echo "DB_IMAGE: $DB_IMAGE"
+echo "db-image=$DB_IMAGE" >> $GITHUB_OUTPUT
+
+################################################################################
+# Container registry
+
+echo "REGISTRY: $REGISTRY"
+echo "registry=$REGISTRY" >> $GITHUB_OUTPUT
+
+################################################################################
+# Container registry namespace
+
+if [ "$REGISTRY_NAMESPACE" == "" ]
+then
+    REGISTRY_NAMESPACE=$GITHUB_REPOSITORY_OWNER
+fi
+
+echo "REGISTRY_NAMESPACE: $REGISTRY_NAMESPACE"
+echo "registry-namespace=$REGISTRY_NAMESPACE" >> $GITHUB_OUTPUT
