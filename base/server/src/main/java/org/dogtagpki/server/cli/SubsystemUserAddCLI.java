390173c9fad12aa6ce6268e31535337bf6d8581a
Chris Kelley <ckelley@redhat.com>
2023-01-25 11:44:59 +0000

    Add new --attributes option to <subsystem>-user-add
    Originally, this was to set nsPagedSizeLimit=20000 on creation of users
    
    Currently every entry is pulled by default which has been causing users
    problems. There is an RFE to change this attribute dynamically based on
    the size of the backend DB but for now set a reasonable default.
    
    Instead of just adding this one attribute, a general method for adding
    arbitrary single-valued attributes on user creation is introduced.
    
    A new test is added
    
    Resolves: #1959057

--- base/server/src/main/java/org/dogtagpki/server/cli/SubsystemUserAddCLI.java
+++ base/server/src/main/java/org/dogtagpki/server/cli/SubsystemUserAddCLI.java
@@ -7,6 +7,7 @@ package org.dogtagpki.server.cli;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,7 +17,6 @@ import org.dogtagpki.cli.CLI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.netscape.certsrv.user.UserData;
 import com.netscape.cmscore.apps.EngineConfig;
 import com.netscape.cmscore.ldapconn.LDAPConfig;
 import com.netscape.cmscore.ldapconn.LDAPConnectionConfig;
@@ -29,6 +29,8 @@ import com.netscape.cmscore.usrgrp.UGSubsystemConfig;
 import com.netscape.cmscore.usrgrp.User;
 import com.netscape.cmsutil.password.IPasswordStore;
 import com.netscape.cmsutil.password.PasswordStoreConfig;
+
+import netscape.ldap.LDAPAttribute;
 
 /**
  * @author Endi S. Dewata
@@ -75,6 +77,10 @@ public class SubsystemUserAddCLI extends SubsystemCLI {
         option = new Option(null, "tps-profiles", true, "Comma-separated TPS profiles");
         option.setArgName("profiles");
         options.addOption(option);
+
+        option = new Option(null, "attributes", true, "Attributes");
+        option.setArgName("attributes");
+        options.addOption(option);
     }
 
     @Override
@@ -101,6 +107,7 @@ public class SubsystemUserAddCLI extends SubsystemCLI {
         String type = cmd.getOptionValue("type");
         String state = cmd.getOptionValue("state");
         String tpsProfiles = cmd.getOptionValue("tps-profiles");
+        String attributes = cmd.getOptionValue("attributes");
 
         if (passwordFile != null) {
             password = new String(Files.readAllBytes(Paths.get(passwordFile)), "UTF-8").trim();
@@ -133,8 +140,6 @@ public class SubsystemUserAddCLI extends SubsystemCLI {
         UGSubsystemConfig ugConfig = cs.getUGSubsystemConfig();
         UGSubsystem ugSubsystem = new UGSubsystem();
 
-        UserData userData = new UserData();
-
         try {
             ugSubsystem.init(socketConfig, ugConfig, passwordStore);
 
@@ -152,6 +157,15 @@ public class SubsystemUserAddCLI extends SubsystemCLI {
                 user.setTpsProfiles(list);
             }
 
+            if (attributes != null) {
+                String[] attrs = attributes.split(",");
+                List<LDAPAttribute> ldapAttrList = new ArrayList<>();
+                for (String s : attrs) {
+                    String[] split = s.split(":");
+                    ldapAttrList.add(new LDAPAttribute(split[0], split[1]));
+                }
+                user.setAttributes(ldapAttrList);
+            }
             ugSubsystem.addUser(user);
 
         } finally {
