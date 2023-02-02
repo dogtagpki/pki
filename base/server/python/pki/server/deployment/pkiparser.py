e649bdaf0da2ad65e7728e3afb0fef6c4a8f38d4
Endi S. Dewata <edewata@redhat.com>
2023-02-01 18:02:20 -0600

    Convert pki_target_cacert_profile into variable

--- base/server/python/pki/server/deployment/pkiparser.py
+++ base/server/python/pki/server/deployment/pkiparser.py
@@ -658,10 +658,6 @@ class PKIConfigParser:
                     os.path.join(
                         self.mdict['pki_subsystem_configuration_path'],
                         "caAuditSigningCert.profile")
-                self.mdict['pki_target_cacert_profile'] = \
-                    os.path.join(
-                        self.mdict['pki_subsystem_configuration_path'],
-                        "caCert.profile")
 
                 # in-place slot substitution name/value pairs
                 if config.str2bool(self.mdict['pki_profiles_in_ldap']):
