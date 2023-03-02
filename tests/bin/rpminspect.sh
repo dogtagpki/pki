dfe2d551d28172371e44f060f9af4ca5efe0f588
Chris Kelley <ckelley@redhat.com>
2023-03-02 08:53:07 +0000

    Introduce pki-rpminspect profile for rpminspect
    Allows more fine-grained control of checks than CLI arguments, which
    allows us to run the javabytecode, runpath and annocheck jobs again
    without disabling them completely.
    
    The license job is disabled because of a bug in rpminspect itself.
    
    The metadata job is still disabled, but we can probably now configure it
    to pass using this profile.

--- tests/bin/rpminspect.sh
+++ tests/bin/rpminspect.sh
@@ -1,22 +1,11 @@
 #!/bin/bash -e
 
-# Don't run metadata check as we can't know the build host subdomain
-# of CI runners in advance to add to an allow list
-
 echo "Running RPMInspect on SRPM"
-rpminspect-fedora -E metadata build/SRPMS/*.rpm
+rpminspect-fedora -p pki-rpminspect build/SRPMS/*.rpm
 
 # Run RPMInspect on RPMs
 for f in build/RPMS/*rpm; do
-
   echo "::group::Running RPMInspect on $f"
-  if [[ "$f" == *"dogtag-pki-tools-"[0-9]* ]]
-  then
-    # "Don't run runpath test for dogtag-pki-tools as expect it to fail."
-    # "dogtag-pki-tools utilizes internal libraries located under '%{_libdir}/tps'"
-    rpminspect-fedora -E runpath,metadata,javabytecode "$f"
-  else
-    rpminspect-fedora -E metadata,javabytecode "$f"
-  fi
+  rpminspect-fedora -p pki-rpminspect "$f"
   echo "::endgroup::"
-  done
+done
