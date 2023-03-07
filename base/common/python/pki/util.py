d09ac5239c2f76f8914931ace22d748eefab4abf
Endi S. Dewata <edewata@redhat.com>
2023-03-07 15:09:29 -0600

    Fix index calculation in replace_params()

--- base/common/python/pki/util.py
+++ base/common/python/pki/util.py
@@ -84,14 +84,14 @@ def replace_params(line, params=None):
             line = line[0:begin] + value + line[end + 1:]
 
             # calculate the new end position
-            end = begin + len(value) + 1
+            end = begin + len(value)
 
         except KeyError:
             # undefined parameter, skip
-            logger.warning('Ignoring [%s] parameter', line[begin:end + 1])
+            logger.warning('Ignoring %s parameter', line[begin:end + 1])
 
         # find the next parameter in the remainder of the line
-        begin = line.find('[', end + 1)
+        begin = line.find('[', end)
 
     return line
 
