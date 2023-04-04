0dddbe998b8a396e666413cb81ca906c0e0e92c1
Endi S. Dewata <edewata@redhat.com>
2023-04-04 16:04:56 -0500

    Update CALogAdminServlet to use @WebServlet

--- base/ca/src/main/java/com/netscape/cms/servlet/admin/CALogAdminServlet.java
+++ base/ca/src/main/java/com/netscape/cms/servlet/admin/CALogAdminServlet.java
@@ -17,6 +17,17 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.admin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "calog",
+        urlPatterns = "/log",
+        initParams = {
+                @WebInitParam(name="ID",       value="calog"),
+                @WebInitParam(name="AuthzMgr", value="BasicAclAuthz")
+        }
+)
 public class CALogAdminServlet extends LogAdminServlet {
     private static final long serialVersionUID = 1L;
 }
