311def8c80f8cde2bced5eaa06abb721e39de037
Endi S. Dewata <edewata@redhat.com>
2023-04-12 16:45:29 -0500

    Update DisplayCertFromRequestAgent to use @WebServlet

--- base/ca/src/main/java/com/netscape/cms/servlet/cert/DisplayCertFromRequestAgent.java
+++ base/ca/src/main/java/com/netscape/cms/servlet/cert/DisplayCertFromRequestAgent.java
@@ -17,6 +17,23 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.cert;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "caDisplayCertFromRequest-agent",
+        urlPatterns = "/ca/displayCertFromRequest",
+        initParams = {
+                @WebInitParam(name="GetClientCert", value="true"),
+                @WebInitParam(name="AuthzMgr",      value="BasicAclAuthz"),
+                @WebInitParam(name="authority",     value="ca"),
+                @WebInitParam(name="interface",     value="agent"),
+                @WebInitParam(name="ID",            value="caDisplayCertFromRequest"),
+                @WebInitParam(name="resourceID",    value="certServer.ca.certificate"),
+                @WebInitParam(name="AuthMgr",       value="certUserDBAuthMgr"),
+                @WebInitParam(name="importCert",    value="false")
+        }
+)
 public class DisplayCertFromRequestAgent extends GetCertFromRequest {
     private static final long serialVersionUID = 1L;
 }
