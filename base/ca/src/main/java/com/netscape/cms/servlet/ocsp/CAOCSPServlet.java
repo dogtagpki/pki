d6391723941190496fc8a82841d8075c096df9d0
Endi S. Dewata <edewata@redhat.com>
2023-04-27 10:14:49 -0500

    Update CAOCSPServlet to use @WebServlet

--- base/ca/src/main/java/com/netscape/cms/servlet/ocsp/CAOCSPServlet.java
+++ base/ca/src/main/java/com/netscape/cms/servlet/ocsp/CAOCSPServlet.java
@@ -17,6 +17,24 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.ocsp;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "caOCSP",
+        urlPatterns = {
+                "/ocsp",
+                "/ocsp/*"
+        },
+        initParams = {
+                @WebInitParam(name="GetClientCert", value="false"),
+                @WebInitParam(name="AuthzMgr",      value="BasicAclAuthz"),
+                @WebInitParam(name="authority",     value="ca"),
+                @WebInitParam(name="interface",     value="ee"),
+                @WebInitParam(name="ID",            value="caOCSP"),
+                @WebInitParam(name="resourceID",    value="certServer.ee.request.ocsp")
+        }
+)
 public class CAOCSPServlet extends OCSPServlet {
     private static final long serialVersionUID = 1L;
 }
