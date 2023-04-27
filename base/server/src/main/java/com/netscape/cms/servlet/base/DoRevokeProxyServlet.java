89a69c5fd2c327c11d1fb7567440ac11c25c9687
Endi S. Dewata <edewata@redhat.com>
2023-04-27 10:15:15 -0500

    Update DoRevokeProxyServlet to use @WebServlet

--- base/server/src/main/java/com/netscape/cms/servlet/base/DoRevokeProxyServlet.java
+++ base/server/src/main/java/com/netscape/cms/servlet/base/DoRevokeProxyServlet.java
@@ -2,6 +2,16 @@
 
 package com.netscape.cms.servlet.base;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "caProxyDoRevoke",
+        urlPatterns = "/doRevoke",
+        initParams = {
+                @WebInitParam(name="destServlet", value="/agent/ca/doRevoke")
+        }
+)
 public class DoRevokeProxyServlet extends ProxyServlet {
     private static final long serialVersionUID = 1L;
 }
