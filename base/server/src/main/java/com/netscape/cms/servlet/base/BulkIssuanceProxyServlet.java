3c002226e6bc39ad25927df3ada6a0aa5c9c0f5e
Endi S. Dewata <edewata@redhat.com>
2023-04-27 10:15:12 -0500

    Update BulkIssuanceProxyServlet to use @WebServlet

--- base/server/src/main/java/com/netscape/cms/servlet/base/BulkIssuanceProxyServlet.java
+++ base/server/src/main/java/com/netscape/cms/servlet/base/BulkIssuanceProxyServlet.java
@@ -2,6 +2,16 @@
 
 package com.netscape.cms.servlet.base;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "caProxyBulkIssuance",
+        urlPatterns = "/agent/bulkissuance",
+        initParams = {
+                @WebInitParam(name="destServlet", value="/agent/ca/bulkissuance")
+        }
+)
 public class BulkIssuanceProxyServlet extends ProxyServlet {
     private static final long serialVersionUID = 1L;
 }
