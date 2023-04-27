163ccbe5aa42dbdafb3003e8c73205aa853e1142
Endi S. Dewata <edewata@redhat.com>
2023-04-27 10:15:06 -0500

    Update ProfileSubmitProxyServlet to use @WebServlet

--- base/server/src/main/java/com/netscape/cms/servlet/base/ProfileSubmitProxyServlet.java
+++ base/server/src/main/java/com/netscape/cms/servlet/base/ProfileSubmitProxyServlet.java
@@ -2,6 +2,16 @@
 
 package com.netscape.cms.servlet.base;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "caProxyProfileSubmit",
+        urlPatterns = "/profileSubmit",
+        initParams = {
+                @WebInitParam(name="destServlet", value="/ee/ca/profileSubmit")
+        }
+)
 public class ProfileSubmitProxyServlet extends ProxyServlet {
     private static final long serialVersionUID = 1L;
 }
