31106fe9bde924318cc1ad76eb11aae47e3e5912
Endi S. Dewata <edewata@redhat.com>
2023-04-27 10:14:17 -0500

    Add new servlets for CA
    Some new servlets have been added to provide a separate class
    for each servlet in CA's web.xml.

---
+++ base/server/src/main/java/com/netscape/cms/servlet/base/BulkIssuanceProxyServlet.java
@@ -0,0 +1,7 @@
+/* CMS_SDK_LICENSE_TEXT */
+
+package com.netscape.cms.servlet.base;
+
+public class BulkIssuanceProxyServlet extends ProxyServlet {
+    private static final long serialVersionUID = 1L;
+}
---
+++ base/server/src/main/java/com/netscape/cms/servlet/base/ProfileSubmitProxyServlet.java
@@ -0,0 +1,7 @@
+/* CMS_SDK_LICENSE_TEXT */
+
+package com.netscape.cms.servlet.base;
+
+public class ProfileSubmitProxyServlet extends ProxyServlet {
+    private static final long serialVersionUID = 1L;
+}
