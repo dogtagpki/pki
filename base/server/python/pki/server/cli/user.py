8d00486ff1c632ea3f653acb06bf39bf6ee42552
Endi S. Dewata <edewata@redhat.com>
2023-04-28 10:58:23 -0500

    Add pki-server <subsystem>-user-role-add
    The pki-server <subsystem>-user-role-add command has been added
    to add a role for a user.

--- base/server/python/pki/server/cli/user.py
+++ base/server/python/pki/server/cli/user.py
@@ -805,6 +805,7 @@ class UserRoleCLI(pki.cli.CLI):
 
         self.parent = parent
         self.add_module(UserRoleFindCLI(self))
+        self.add_module(UserRoleAddCLI(self))
 
 
 class UserRoleFindCLI(pki.cli.CLI):
@@ -893,3 +894,93 @@ class UserRoleFindCLI(pki.cli.CLI):
             sys.exit(1)
 
         subsystem.find_user_roles(user_id, output_format=output_format)
+
+
+class UserRoleAddCLI(pki.cli.CLI):
+    '''
+    Add {subsystem} user role
+    '''
+
+    help = '''\
+        Usage: pki-server {subsystem}-user-role-add [OPTIONS] <user ID> <role ID>
+
+          -i, --instance <instance ID>       Instance ID (default: pki-tomcat)
+          -v, --verbose                      Run in verbose mode.
+              --debug                        Run in debug mode.
+              --help                         Show help message.
+    '''
+
+    def __init__(self, parent):
+        super().__init__(
+            'add',
+            inspect.cleandoc(self.__class__.__doc__).format(
+                subsystem=parent.parent.parent.name.upper()))
+
+        self.parent = parent
+
+    def print_help(self):
+        print(textwrap.dedent(self.__class__.help).format(
+            subsystem=self.parent.parent.parent.name))
+
+    def execute(self, argv):
+        try:
+            opts, args = getopt.gnu_getopt(argv, 'i:v', [
+                'instance=',
+                'verbose', 'debug', 'help'])
+
+        except getopt.GetoptError as e:
+            logger.error(e)
+            self.print_help()
+            sys.exit(1)
+
+        instance_name = 'pki-tomcat'
+        subsystem_name = self.parent.parent.parent.name
+
+        for o, a in opts:
+            if o in ('-i', '--instance'):
+                instance_name = a
+
+            elif o in ('-v', '--verbose'):
+                logging.getLogger().setLevel(logging.INFO)
+
+            elif o == '--debug':
+                logging.getLogger().setLevel(logging.DEBUG)
+
+            elif o == '--help':
+                self.print_help()
+                sys.exit()
+
+            else:
+                logger.error('Invalid option: %s', o)
+                self.print_help()
+                sys.exit(1)
+
+        if len(args) < 1:
+            logger.error('Missing user ID')
+            self.print_help()
+            sys.exit(1)
+
+        user_id = args[0]
+
+        if len(args) < 2:
+            logger.error('Missing role ID')
+            self.print_help()
+            sys.exit(1)
+
+        role_id = args[1]
+
+        instance = pki.server.instance.PKIServerFactory.create(instance_name)
+        if not instance.exists():
+            logger.error('Invalid instance: %s', instance_name)
+            sys.exit(1)
+
+        instance.load()
+
+        subsystem = instance.get_subsystem(subsystem_name)
+
+        if not subsystem:
+            logger.error('No %s subsystem in instance %s',
+                         subsystem_name.upper(), instance_name)
+            sys.exit(1)
+
+        subsystem.add_user_role(user_id, role_id)
