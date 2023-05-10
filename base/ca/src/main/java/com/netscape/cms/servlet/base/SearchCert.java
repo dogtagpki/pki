b78b58fd405bceef990d4b1ce5bdb9b97d2756d1
Endi S. Dewata <edewata@redhat.com>
2023-05-10 09:36:52 -0500

    Update SearchCert to use @WebServlet

--- base/ca/src/main/java/com/netscape/cms/servlet/base/SearchCert.java
+++ base/ca/src/main/java/com/netscape/cms/servlet/base/SearchCert.java
@@ -17,6 +17,22 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.base;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "caSrchCert",
+        urlPatterns = "/agent/ca/srchCert.html",
+        initParams = {
+                @WebInitParam(name="GetClientCert",        value="true"),
+                @WebInitParam(name="htmlPath",             value="/agent/ca/SrchCert.html"),
+                @WebInitParam(name="authority",            value="ca"),
+                @WebInitParam(name="interface",            value="agent"),
+                @WebInitParam(name="ID",                   value="caSrchCert"),
+                @WebInitParam(name="unauthorizedTemplate", value="/agent/GenUnauthorized.template"),
+                @WebInitParam(name="AuthMgr",              value="certUserDBAuthMgr")
+        }
+)
 public class SearchCert extends DisplayHtmlServlet {
     private static final long serialVersionUID = 1L;
 }
