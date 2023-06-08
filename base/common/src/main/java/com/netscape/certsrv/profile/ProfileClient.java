f89d2bc9778b623643f9f88a84dc9c36e55ff407
Endi S. Dewata <edewata@redhat.com>
2023-06-08 13:42:33 -0500

    Update CA ProfileClient to use JAX-RS client API

--- base/common/src/main/java/com/netscape/certsrv/profile/ProfileClient.java
+++ base/common/src/main/java/com/netscape/certsrv/profile/ProfileClient.java
@@ -17,7 +17,10 @@
 //--- END COPYRIGHT BLOCK ---
 package com.netscape.certsrv.profile;
 
-import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.client.Entity;
 
 import com.netscape.certsrv.client.Client;
 import com.netscape.certsrv.client.PKIClient;
@@ -27,68 +30,60 @@ import com.netscape.certsrv.client.PKIClient;
  */
 public class ProfileClient extends Client {
 
-    public ProfileResource profileClient;
-
     public ProfileClient(PKIClient client, String subsystem) throws Exception {
-        super(client, subsystem, "profile");
-        init();
-    }
-
-    public void init() throws Exception {
-        profileClient = createProxy(ProfileResource.class);
+        super(client, subsystem, "profiles");
     }
 
     public ProfileData retrieveProfile(String id) throws Exception {
-        Response response = profileClient.retrieveProfile(id);
-        return client.getEntity(response, ProfileData.class);
+        return get(id, ProfileData.class);
     }
 
     public byte[] retrieveProfileRaw(String id) throws Exception {
-        Response response = profileClient.retrieveProfileRaw(id);
-        return client.getEntity(response, byte[].class);
+        return get(id + "/raw", byte[].class);
     }
 
     public ProfileDataInfos listProfiles(Integer start, Integer size) throws Exception {
-        Response response =  profileClient.listProfiles(start, size);
-        return client.getEntity(response, ProfileDataInfos.class);
+        Map<String, Object> params = new HashMap<>();
+        if (start != null) params.put("start", start);
+        if (size != null) params.put("size", size);
+        return get(null, params, ProfileDataInfos.class);
     }
 
     public void enableProfile(String id) throws Exception {
-        Response response = profileClient.modifyProfileState(id, "enable");
-        client.getEntity(response, Void.class);
+        Map<String, Object> params = new HashMap<>();
+        params.put("action", "enable");
+        post(id, params, null, Void.class);
     }
 
     public void disableProfile(String id) throws Exception {
-        Response response = profileClient.modifyProfileState(id, "disable");
-        client.getEntity(response, Void.class);
+        Map<String, Object> params = new HashMap<>();
+        params.put("action", "disable");
+        post(id, params, null, Void.class);
     }
 
     public ProfileData createProfile(ProfileData data) throws Exception {
         String createProfileRequest = (String) client.marshall(data);
-        Response response = profileClient.createProfile(createProfileRequest);
-        return client.getEntity(response, ProfileData.class);
+        Entity<String> entity = client.entity(createProfileRequest);
+        return post(null, null, entity, ProfileData.class);
     }
 
     public byte[] createProfileRaw(byte[] properties) throws Exception {
-        Response response =
-            profileClient.createProfileRaw(properties);
-        return client.getEntity(response, byte[].class);
+        Entity<byte[]> entity = client.entity(properties);
+        return post("raw", null, entity, byte[].class);
     }
 
     public ProfileData modifyProfile(ProfileData data) throws Exception {
         String modifyProfileRequest = (String) client.marshall(data);
-        Response response = profileClient.modifyProfile(data.getId(), modifyProfileRequest);
-        return client.getEntity(response, ProfileData.class);
+        Entity<String> entity = client.entity(modifyProfileRequest);
+        return put(data.getId(), null, entity, ProfileData.class);
     }
 
     public byte[] modifyProfileRaw(String profileId, byte[] properties) throws Exception {
-        Response response = profileClient.modifyProfileRaw(profileId, properties);
-        return client.getEntity(response, byte[].class);
+        Entity<byte[]> entity = client.entity(properties);
+        return put(profileId + "/raw", null, entity, byte[].class);
     }
 
     public void deleteProfile(String id) throws Exception {
-        Response response = profileClient.deleteProfile(id);
-        client.getEntity(response, Void.class);
+        delete(id, Void.class);
     }
-
 }
