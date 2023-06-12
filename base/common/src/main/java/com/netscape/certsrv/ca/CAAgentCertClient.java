6da29a50247fe76b0c65f922afc0e4f2e92f7e05
Endi S. Dewata <edewata@redhat.com>
2023-06-12 18:58:55 -0500

    Update CAAgentCertClient to use JAX-RS client API

--- base/common/src/main/java/com/netscape/certsrv/ca/CAAgentCertClient.java
+++ base/common/src/main/java/com/netscape/certsrv/ca/CAAgentCertClient.java
@@ -17,12 +17,11 @@
 //--- END COPYRIGHT BLOCK ---
 package com.netscape.certsrv.ca;
 
-import javax.ws.rs.core.Response;
+import javax.ws.rs.client.Entity;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.netscape.certsrv.cert.AgentCertResource;
 import com.netscape.certsrv.cert.CertData;
 import com.netscape.certsrv.cert.CertRequestInfo;
 import com.netscape.certsrv.cert.CertRevokeRequest;
@@ -37,34 +36,25 @@ public class CAAgentCertClient extends Client {
 
     public final static Logger logger = LoggerFactory.getLogger(CAAgentCertClient.class);
 
-    public AgentCertResource agentCertClient;
-
     public CAAgentCertClient(PKIClient client) throws Exception {
         super(client, "ca", "agent/certs");
-        init();
-    }
-
-    public void init() throws Exception {
-        agentCertClient = createProxy(AgentCertResource.class);
     }
 
     public CertData reviewCert(CertId id) throws Exception {
-        Response response = agentCertClient.reviewCert(id);
-        return client.getEntity(response, CertData.class);
+        return get(id.toHexString(), CertData.class);
     }
 
     public CertRequestInfo revokeCert(CertId id, CertRevokeRequest request) throws Exception {
-        Response response = agentCertClient.revokeCert(id, request);
-        return client.getEntity(response, CertRequestInfo.class);
+        Entity<CertRevokeRequest> entity = client.entity(request);
+        return post(id.toHexString() + "/revoke", null, entity, CertRequestInfo.class);
     }
 
     public CertRequestInfo revokeCACert(CertId id, CertRevokeRequest request) throws Exception {
-        Response response = agentCertClient.revokeCACert(id, request);
-        return client.getEntity(response, CertRequestInfo.class);
+        Entity<CertRevokeRequest> entity = client.entity(request);
+        return post(id.toHexString() + "/revoke-ca", null, entity, CertRequestInfo.class);
     }
 
     public CertRequestInfo unrevokeCert(CertId id) throws Exception {
-        Response response = agentCertClient.unrevokeCert(id);
-        return client.getEntity(response, CertRequestInfo.class);
+        return post(id.toHexString() + "/unrevoke", CertRequestInfo.class);
     }
 }
