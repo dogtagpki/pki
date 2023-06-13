92d913e78b40c9905e933b13c0d6b58501945ae3
Endi S. Dewata <edewata@redhat.com>
2023-06-12 19:08:38 -0500

    Update CACertRequestClient to use JAX-RS client API

--- base/common/src/main/java/com/netscape/certsrv/ca/CACertRequestClient.java
+++ base/common/src/main/java/com/netscape/certsrv/ca/CACertRequestClient.java
@@ -17,7 +17,10 @@
 //--- END COPYRIGHT BLOCK ---
 package com.netscape.certsrv.ca;
 
-import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.client.Entity;
 
 import org.mozilla.jss.netscape.security.x509.X500Name;
 import org.slf4j.Logger;
@@ -26,7 +29,6 @@ import org.slf4j.LoggerFactory;
 import com.netscape.certsrv.cert.CertEnrollmentRequest;
 import com.netscape.certsrv.cert.CertRequestInfo;
 import com.netscape.certsrv.cert.CertRequestInfos;
-import com.netscape.certsrv.cert.CertRequestResource;
 import com.netscape.certsrv.client.Client;
 import com.netscape.certsrv.client.PKIClient;
 import com.netscape.certsrv.profile.ProfileDataInfos;
@@ -39,38 +41,32 @@ public class CACertRequestClient extends Client {
 
     public final static Logger logger = LoggerFactory.getLogger(CACertRequestClient.class);
 
-    public CertRequestResource certRequestClient;
-
     public CACertRequestClient(PKIClient client) throws Exception {
         super(client, "ca", "certrequests");
-        init();
-    }
-
-    public void init() throws Exception {
-        certRequestClient = createProxy(CertRequestResource.class);
     }
 
     public CertRequestInfo getRequest(RequestId id) throws Exception {
-        Response response = certRequestClient.getRequestInfo(id);
-        return client.getEntity(response, CertRequestInfo.class);
+        return get(id.toHexString(), CertRequestInfo.class);
     }
 
     public CertRequestInfos enrollRequest(
             CertEnrollmentRequest data, AuthorityID aid, X500Name adn) throws Exception {
-        String aidString = aid != null ? aid.toString() : null;
-        String adnString = adn != null ? adn.toLdapDNString() : null;
+        Map<String, Object> params = new HashMap<>();
+        if (aid != null) params.put("issuer-id", aid.toString());
+        if (adn != null) params.put("issuer-dn", adn.toLdapDNString());
         String enrollmentRequest = (String) client.marshall(data);
-        Response response = certRequestClient.enrollCert(enrollmentRequest, aidString, adnString);
-        return client.getEntity(response, CertRequestInfos.class);
+        Entity<String> entity = client.entity(enrollmentRequest);
+        return post(null, params, entity, CertRequestInfos.class);
     }
 
     public ProfileDataInfos listEnrollmentTemplates(Integer start, Integer size) throws Exception {
-        Response response = certRequestClient.listEnrollmentTemplates(start, size);
-        return client.getEntity(response, ProfileDataInfos.class);
+        Map<String, Object> params = new HashMap<>();
+        if (start != null) params.put("start",start);
+        if (size != null) params.put("size", size);
+        return get("profiles", ProfileDataInfos.class);
     }
 
     public CertEnrollmentRequest getEnrollmentTemplate(String id) throws Exception {
-        Response response = certRequestClient.getEnrollmentTemplate(id);
-        return client.getEntity(response, CertEnrollmentRequest.class);
+        return get("profiles/" + id, CertEnrollmentRequest.class);
     }
 }
