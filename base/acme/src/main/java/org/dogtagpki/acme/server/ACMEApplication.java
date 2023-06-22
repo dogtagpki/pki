994d932100c7d335752fe817a7d8757f62439b08
Endi S. Dewata <edewata@redhat.com>
2023-06-22 16:59:48 -0500

    Auto-initialize ACME application
    The ACME webapp has been modified to automatically initialize
    the ACME application without an explicit dependency on RESTEasy.
    Since a JAX-RS application cannot have an empty path, the ACME
    application needs to be relocated to /rest and the endpoints need
    to be mapped to the new location as well.
    
    https://docs.jboss.org/resteasy/docs/3.0.24.Final/userguide/html_single/#d4e143
    https://stackoverflow.com/questions/10874188/jax-rs-application-on-the-root-context-how-can-it-be-done

--- base/acme/src/main/java/org/dogtagpki/acme/server/ACMEApplication.java
+++ base/acme/src/main/java/org/dogtagpki/acme/server/ACMEApplication.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.Application;
 /**
  * @author Endi S. Dewata
  */
-@ApplicationPath("")
+@ApplicationPath("/rest")
 public class ACMEApplication extends Application {
 
     public static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ACMEApplication.class);
