14dcbbbd2d7912b0cd836c8abe21170c308f4c05
Endi S. Dewata <edewata@redhat.com>
2023-07-20 14:56:42 -0500

    Update KRAACLAdminServlet to use @WebServlet

--- base/kra/src/main/java/com/netscape/cms/servlet/admin/KRAACLAdminServlet.java
+++ base/kra/src/main/java/com/netscape/cms/servlet/admin/KRAACLAdminServlet.java
@@ -17,6 +17,17 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.admin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "kraacl",
+        urlPatterns = "/acl",
+        initParams = {
+                @WebInitParam(name="ID",       value="kraacl"),
+                @WebInitParam(name="AuthzMgr", value="BasicAclAuthz")
+        }
+)
 public class KRAACLAdminServlet extends ACLAdminServlet {
     private static final long serialVersionUID = 1L;
 }
