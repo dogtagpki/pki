a9bfc92846803cde248ee644049ad21e90a6e05e
Endi S. Dewata <edewata@redhat.com>
2023-07-21 12:34:43 -0500

    Update SrchKey to use @WebServlet

--- base/kra/src/main/java/com/netscape/cms/servlet/key/SrchKey.java
+++ base/kra/src/main/java/com/netscape/cms/servlet/key/SrchKey.java
@@ -24,6 +24,8 @@ import java.util.Locale;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -49,9 +51,20 @@ import com.netscape.kra.KeyRecoveryAuthority;
 
 /**
  * Retrieve archived keys matching search criteria
- *
- * @version $Revision$, $Date$
  */
+@WebServlet(
+        name = "kraKRASrchKey",
+        urlPatterns = "/agent/kra/srchKey",
+        initParams = {
+                @WebInitParam(name="GetClientCert", value="true"),
+                @WebInitParam(name="AuthzMgr",      value="BasicAclAuthz"),
+                @WebInitParam(name="authority",     value="kra"),
+                @WebInitParam(name="templatePath",  value="/agent/kra/srchKey.template"),
+                @WebInitParam(name="ID",            value="kraKRASrchKey"),
+                @WebInitParam(name="AuthMgr",       value="certUserDBAuthMgr"),
+                @WebInitParam(name="resourceID",    value="certServer.kra.keys")
+        }
+)
 public class SrchKey extends CMSServlet {
 
     public static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(SrchKey.class);
