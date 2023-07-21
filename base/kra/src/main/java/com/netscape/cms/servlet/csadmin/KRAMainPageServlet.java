1ae4adda7d594a2037e06d0f9394d0fa71e43b27
Endi S. Dewata <edewata@redhat.com>
2023-07-21 12:41:31 -0500

    Update KRAMainPageServlet to use @WebServlet

--- base/kra/src/main/java/com/netscape/cms/servlet/csadmin/KRAMainPageServlet.java
+++ base/kra/src/main/java/com/netscape/cms/servlet/csadmin/KRAMainPageServlet.java
@@ -17,6 +17,19 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.csadmin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "services",
+        urlPatterns = "/services",
+        initParams = {
+                @WebInitParam(name="GetClientCert", value="false"),
+                @WebInitParam(name="authorityId",   value="kra"),
+                @WebInitParam(name="ID",            value="services"),
+                @WebInitParam(name="templatePath",  value="/services.template")
+        }
+)
 public class KRAMainPageServlet extends MainPageServlet {
     private static final long serialVersionUID = 1L;
 }
