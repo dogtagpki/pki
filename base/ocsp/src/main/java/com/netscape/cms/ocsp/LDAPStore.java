7f1e896b95552d20d0294dc44ef2c94e15d17773
Marco Fargetta <mfargett@redhat.com>
2023-07-28 16:30:27 +0200

    Add crl check for OCSP acting as server
    When OCSP is acting as server certificate can be verified using CRL
    internally stored.
    
    To verify the certificates the `LDAPStore` has to be enabled with the
    variable `ocsp.store.ldapStore.checkSubsystemConnection` and the
    variable `auths.revocationChecking.enabled` both set to true.
    
    Solve RHCS-4262

--- base/ocsp/src/main/java/com/netscape/cms/ocsp/LDAPStore.java
+++ base/ocsp/src/main/java/com/netscape/cms/ocsp/LDAPStore.java
@@ -94,6 +94,8 @@ public class LDAPStore implements IDefStore, IExtendedPluginInfo {
     private String mCACertAttr = null;
     protected Hashtable<String, Long> mReqCounts = new Hashtable<>();
     private Hashtable<X509CertImpl, X509CRLImpl> mCRLs = new Hashtable<>();
+    private boolean mCheckConnection = false;
+
 
     /**
      * Constructs the default store.
@@ -141,6 +143,7 @@ public class LDAPStore implements IDefStore, IExtendedPluginInfo {
                     DEF_CA_CERT_ATTR);
         mByName = mConfig.getBoolean(PROP_BY_NAME, true);
 
+        mCheckConnection = mConfig.getBoolean(PROP_CHECK_SUBSYSTEM_CONNECTION, false);
     }
 
     /**
@@ -278,7 +281,7 @@ public class LDAPStore implements IDefStore, IExtendedPluginInfo {
 
             updater.start();
         }
-        if(mConfig.getBoolean(PROP_CHECK_SUBSYSTEM_CONNECTION, false)) {
+        if(mCheckConnection) {
             CMS.setApprovalCallbask(new CRLLdapValidator(this));
         }
     }
@@ -535,6 +538,11 @@ public class LDAPStore implements IDefStore, IExtendedPluginInfo {
             mConfig.put(key, pairs.get(key));
         }
     }
+
+    public boolean isCRLCheckAvailable() {
+        return mCheckConnection;
+    }
+
 }
 
 class CRLUpdater extends Thread {
