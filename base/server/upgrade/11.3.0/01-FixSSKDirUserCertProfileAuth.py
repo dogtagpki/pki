c0775a5da8715554bd69029df3eb1b1588f0efc9
parrjd <92112129+parrjd@users.noreply.github.com>
2023-08-04 15:55:04 -0700

    Update 01-FixSSKDirUserCertProfileAuth.py
    Only update profile if exists.  Currently if the file does not exist CA fails to start

--- base/server/upgrade/11.3.0/01-FixSSKDirUserCertProfileAuth.py
+++ base/server/upgrade/11.3.0/01-FixSSKDirUserCertProfileAuth.py
@@ -26,19 +26,20 @@ class FixSSKDirUserCertProfileAuth(pki.server.upgrade.PKIServerUpgradeScriptlet)
             return
 
         path = os.path.join(subsystem.base_dir, 'profiles', 'ca', 'caServerKeygen_UserCert.cfg')
-        self.backup(path)
+        if os.path.exists(path):
+            self.backup(path)
 
-        config = {}
+            config = {}
 
-        logger.info('Loading %s', path)
-        pki.util.load_properties(path, config)
+            logger.info('Loading %s', path)
+            pki.util.load_properties(path, config)
 
-        config['input.list'] = 'i1'
-        config.pop('input.i2.class_id', None)
-        config.pop('input.i3.class_id', None)
-        config['policyset.userCertSet.1.default.class_id'] = 'authTokenSubjectNameDefaultImpl'
-        config['policyset.userCertSet.8.default.params.subjAltExtPattern_0'] = \
-            '$request.auth_token.mail[0]$'
+            config['input.list'] = 'i1'
+            config.pop('input.i2.class_id', None)
+            config.pop('input.i3.class_id', None)
+            config['policyset.userCertSet.1.default.class_id'] = 'authTokenSubjectNameDefaultImpl'
+            config['policyset.userCertSet.8.default.params.subjAltExtPattern_0'] = \
+                '$request.auth_token.mail[0]$'
 
-        logger.info('Storing %s', path)
-        pki.util.store_properties(path, config)
+            logger.info('Storing %s', path)
+            pki.util.store_properties(path, config)
