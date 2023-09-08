0087ee92dacd723115f204daa1a0ea7e75f441ec
Endi S. Dewata <edewata@redhat.com>
2023-09-08 18:20:48 -0500

    Update OCSPAuthAdminServlet to use @WebServlet

--- base/ocsp/src/main/java/com/netscape/cms/servlet/admin/OCSPAuthAdminServlet.java
+++ base/ocsp/src/main/java/com/netscape/cms/servlet/admin/OCSPAuthAdminServlet.java
@@ -17,6 +17,17 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.admin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "ocspauths",
+        urlPatterns = "/auths",
+        initParams = {
+                @WebInitParam(name="ID",       value="ocspauths"),
+                @WebInitParam(name="AuthzMgr", value="BasicAclAuthz")
+        }
+)
 public class OCSPAuthAdminServlet extends AuthAdminServlet {
     private static final long serialVersionUID = 1L;
 }
