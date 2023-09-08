a1ed44694cf72f24f1cf62683cfd9dd35e43ff73
Endi S. Dewata <edewata@redhat.com>
2023-09-08 18:20:46 -0500

    Update OCSPLogAdminServlet to use @WebServlet

--- base/ocsp/src/main/java/com/netscape/cms/servlet/admin/OCSPLogAdminServlet.java
+++ base/ocsp/src/main/java/com/netscape/cms/servlet/admin/OCSPLogAdminServlet.java
@@ -17,6 +17,17 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.admin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "ocsplog",
+        urlPatterns = "/log",
+        initParams = {
+                @WebInitParam(name="ID",       value="ocsplog"),
+                @WebInitParam(name="AuthzMgr", value="BasicAclAuthz")
+        }
+)
 public class OCSPLogAdminServlet extends LogAdminServlet {
     private static final long serialVersionUID = 1L;
 }
