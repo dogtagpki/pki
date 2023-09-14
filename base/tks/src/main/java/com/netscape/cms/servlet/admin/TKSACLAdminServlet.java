5d63cd0e29f3c50f51cb8be0ab7bf497b160a61d
Endi S. Dewata <edewata@redhat.com>
2023-09-14 12:08:15 -0500

    Update TKSACLAdminServlet to use @WebServlet

--- base/tks/src/main/java/com/netscape/cms/servlet/admin/TKSACLAdminServlet.java
+++ base/tks/src/main/java/com/netscape/cms/servlet/admin/TKSACLAdminServlet.java
@@ -17,6 +17,17 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.admin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "tksacl",
+        urlPatterns = "/acl",
+        initParams = {
+                @WebInitParam(name="ID",       value="tksacl"),
+                @WebInitParam(name="AuthzMgr", value="BasicAclAuthz")
+        }
+)
 public class TKSACLAdminServlet extends ACLAdminServlet {
     private static final long serialVersionUID = 1L;
 }
