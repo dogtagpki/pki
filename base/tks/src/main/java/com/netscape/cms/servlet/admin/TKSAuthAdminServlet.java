9d1546c4cd319b98c9111426ed28849c8edc7970
Endi S. Dewata <edewata@redhat.com>
2023-09-14 12:08:10 -0500

    Update TKSAuthAdminServlet to use @WebServlet

--- base/tks/src/main/java/com/netscape/cms/servlet/admin/TKSAuthAdminServlet.java
+++ base/tks/src/main/java/com/netscape/cms/servlet/admin/TKSAuthAdminServlet.java
@@ -17,6 +17,17 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.admin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "tksauths",
+        urlPatterns = "/auths",
+        initParams = {
+                @WebInitParam(name="ID",       value="tksauths"),
+                @WebInitParam(name="AuthzMgr", value="BasicAclAuthz")
+        }
+)
 public class TKSAuthAdminServlet extends AuthAdminServlet {
     private static final long serialVersionUID = 1L;
 }
