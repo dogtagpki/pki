83cf9a82110bcdcf1fffc8f14384246b7d0ff351
Endi S. Dewata <edewata@redhat.com>
2023-09-14 12:08:07 -0500

    Update TKSLogAdminServlet to use @WebServlet

--- base/tks/src/main/java/com/netscape/cms/servlet/admin/TKSLogAdminServlet.java
+++ base/tks/src/main/java/com/netscape/cms/servlet/admin/TKSLogAdminServlet.java
@@ -17,6 +17,17 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.admin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "tkslog",
+        urlPatterns = "/log",
+        initParams = {
+                @WebInitParam(name="ID",       value="tkslog"),
+                @WebInitParam(name="AuthzMgr", value="BasicAclAuthz")
+        }
+)
 public class TKSLogAdminServlet extends LogAdminServlet {
     private static final long serialVersionUID = 1L;
 }
