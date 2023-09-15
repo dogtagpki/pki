25d8ec570582d7d3f49a4967fe732ad80ac529e5
Endi S. Dewata <edewata@redhat.com>
2023-09-15 11:51:18 -0500

    Update TPSGetStatus to use @WebServlet

--- base/tps/src/main/java/com/netscape/cms/servlet/csadmin/TPSGetStatus.java
+++ base/tps/src/main/java/com/netscape/cms/servlet/csadmin/TPSGetStatus.java
@@ -17,6 +17,18 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.csadmin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "tpsGetStatus",
+        urlPatterns = "/admin/tps/getStatus",
+        initParams = {
+                @WebInitParam(name="GetClientCert", value="false"),
+                @WebInitParam(name="authority",     value="tps"),
+                @WebInitParam(name="ID",            value="tpsGetStatus")
+        }
+)
 public class TPSGetStatus extends GetStatus {
     private static final long serialVersionUID = 1L;
 }
