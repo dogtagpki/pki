349ab9d9b27c660f9563dbd17f59491adef2f757
Endi S. Dewata <edewata@redhat.com>
2023-09-15 11:48:43 -0500

    Update TPSLogAdminServlet to use @WebServlet

--- base/tps/src/main/java/com/netscape/cms/servlet/admin/TPSLogAdminServlet.java
+++ base/tps/src/main/java/com/netscape/cms/servlet/admin/TPSLogAdminServlet.java
@@ -17,6 +17,17 @@
 // --- END COPYRIGHT BLOCK ---
 package com.netscape.cms.servlet.admin;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "tpslog",
+        urlPatterns = "/log",
+        initParams = {
+                @WebInitParam(name="ID",       value="tpslog"),
+                @WebInitParam(name="AuthzMgr", value="BasicAclAuthz")
+        }
+)
 public class TPSLogAdminServlet extends LogAdminServlet {
     private static final long serialVersionUID = 1L;
 }
