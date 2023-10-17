cc587137524a651a776f302d9ea850f3ea475a8b
Marco Fargetta <mfargett@redhat.com>
2023-10-17 10:33:16 +0200

    Concert CSR file name to cert_id

--- base/server/upgrade/11.5.0/04-RemoveCertCSRfromConfig.py
+++ base/server/upgrade/11.5.0/04-RemoveCertCSRfromConfig.py
@@ -25,28 +25,20 @@ class RemoveCertCSRfromConfig(pki.server.upgrade.PKIServerUpgradeScriptlet):
         certs_path = os.path.join(instance.conf_dir, 'certs')
         instance.makedirs(certs_path, exist_ok=True)
         logger.info('Removing certs data')
-        if subsystem.name == 'ca':
-            self.clean_cert_csr('signing', subsystem, certs_path)
-            self.clean_cert_csr('ocsp_signing', subsystem, certs_path)
-        if subsystem.name == 'kra':
-            self.clean_cert_csr('storage', subsystem, certs_path)
-            self.clean_cert_csr('transport', subsystem, certs_path)
-        if subsystem.name == 'ocsp':
-            self.clean_cert_csr('signing', subsystem, certs_path)
-
-        self.clean_cert_csr('sslserver', subsystem, certs_path)
-        self.clean_cert_csr('subsystem', subsystem, certs_path)
-        self.clean_cert_csr('audit_signing', subsystem, certs_path)
+        certs = subsystem.find_system_certs()
+        for cert in certs:
+            self.clean_cert_csr(cert['id'], subsystem, certs_path)
 
         subsystem.save()
 
     def clean_cert_csr(self, tag, subsystem, dest_path):
         subsystem.config.pop('%s.%s.cert' % (subsystem.name, tag), None)
         cert_req = subsystem.config.pop('%s.%s.certreq' % (subsystem.name, tag), None)
-        nickname = subsystem.config.get('%s.%s.nickname' % (subsystem.name, tag))
+        if tag != 'sslserver' and tag != 'subsystem':
+            tag = subsystem.name + '_' + tag
         if cert_req:
             csr_data = pki.nssdb.convert_csr(cert_req, 'base64', 'pem')
-            csr_file = os.path.join(dest_path, nickname + '.csr')
+            csr_file = os.path.join(dest_path, tag + '.csr')
             with open(csr_file, 'w', encoding='utf-8') as f:
                 f.write(csr_data)
             os.chown(csr_file, subsystem.instance.uid, subsystem.instance.gid)
