469c06af0062cef8366535691b4d325e4305cdc3
Endi S. Dewata <edewata@redhat.com>
2023-12-11 09:04:20 +0700

    Add setter methods in LDAPAuthenticationConfig

--- base/server/src/main/java/com/netscape/cmscore/ldapconn/LDAPAuthenticationConfig.java
+++ base/server/src/main/java/com/netscape/cmscore/ldapconn/LDAPAuthenticationConfig.java
@@ -30,11 +30,19 @@ public class LDAPAuthenticationConfig extends ConfigStore {
         return getString(LdapAuthInfo.PROP_LDAPAUTHTYPE);
     }
 
+    public void setAuthType(String authType) throws EBaseException {
+        putString(LdapAuthInfo.PROP_LDAPAUTHTYPE, authType);
+    }
+
     /**
      * Returns <LDAP>.ldapauth.clientCertNickname parameter.
      */
     public String getClientCertNickname() throws EBaseException {
         return getString(LdapAuthInfo.PROP_CLIENTCERTNICKNAME);
+    }
+
+    public void setClientCertNickname(String nickname) throws EBaseException {
+        putString(LdapAuthInfo.PROP_CLIENTCERTNICKNAME, nickname);
     }
 
     /**
@@ -44,6 +52,10 @@ public class LDAPAuthenticationConfig extends ConfigStore {
         return getString(LdapAuthInfo.PROP_BINDDN);
     }
 
+    public void setBindDN(String bindDN) throws EBaseException {
+        putString(LdapAuthInfo.PROP_BINDDN, bindDN);
+    }
+
     /**
      * Returns <LDAP>.ldapauth.bindPassword parameter.
      */
@@ -51,10 +63,18 @@ public class LDAPAuthenticationConfig extends ConfigStore {
         return getString(LdapAuthInfo.PROP_BINDPW, null);
     }
 
+    public void setBindPassword(String bindPassword) throws EBaseException {
+        putString(LdapAuthInfo.PROP_BINDPW, bindPassword);
+    }
+
     /**
      * Returns <LDAP>.ldapauth.bindPWPrompt parameter.
      */
     public String getBindPasswordPrompt() throws EBaseException {
         return getString(LdapAuthInfo.PROP_BINDPW_PROMPT, null);
+    }
+
+    public void setBindPasswordPrompt(String bindPasswordPrompt) throws EBaseException {
+        putString(LdapAuthInfo.PROP_BINDPW_PROMPT, bindPasswordPrompt);
     }
 }
