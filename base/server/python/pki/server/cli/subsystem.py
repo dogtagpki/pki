252456e757048949c824b5a921f43f2a9960e922
Endi S. Dewata <edewata@redhat.com>
2024-02-14 14:51:02 -0600

    Fix pki-server subsystem-show
    The PKIServer.load_subsystems() has been modified to skip
    loading a subsystem if the subsystem folder doesn't exist
    or is empty. This will restore pki-server subsystem-show
    to work more consistently as in the previous PKI version.
    
    Resolves: https://issues.redhat.com/browse/RHEL-21568

--- base/server/python/pki/server/cli/subsystem.py
+++ base/server/python/pki/server/cli/subsystem.py
@@ -189,7 +189,7 @@ class SubsystemShowCLI(pki.cli.CLI):
 
         subsystem = instance.get_subsystem(subsystem_name)
         if not subsystem:
-            logger.error('ERROR: No %s subsystem in instance %s.',
+            logger.error('No %s subsystem in instance %s.',
                          subsystem_name, instance_name)
             sys.exit(1)
 
@@ -944,7 +944,7 @@ class SubsystemCertShowCLI(pki.cli.CLI):
 
         subsystem = instance.get_subsystem(subsystem_name)
         if not subsystem:
-            logger.error('ERROR: No %s subsystem in instance %s.',
+            logger.error('No %s subsystem in instance %s.',
                          subsystem_name, instance_name)
             sys.exit(1)
         cert = subsystem.get_subsystem_cert(cert_id)
