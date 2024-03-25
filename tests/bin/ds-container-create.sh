e496bb59d32bacba144bb04c2002c0d4f33a36a1
Endi S. Dewata <edewata@redhat.com>
2024-03-25 14:42:31 -0500

    Update ds-container-create.sh
    The ds-container-create.sh has been updated to provide options
    to specify the suffix and the base DN of the directory.

--- tests/bin/ds-container-create.sh
+++ tests/bin/ds-container-create.sh
@@ -2,28 +2,71 @@
 
 # https://fy.blackhats.net.au/blog/html/2020/03/28/389ds_in_containers.html
 
-SCRIPT_PATH=`readlink -f "$0"`
-SCRIPT_NAME=`basename "$SCRIPT_PATH"`
-SCRIPT_DIR=`dirname "$SCRIPT_PATH"`
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_NAME=$(basename "$SCRIPT_PATH")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-NAME=$1
+SUFFIX=
+BASE_DN=
 
-if [ "$NAME" == "" ]
-then
-    echo "Usage: ds-container-create.sh <name>"
-    exit 1
-fi
+VERBOSE=
+DEBUG=
 
-if [ "$PASSWORD" == "" ]
-then
-    echo "Missing Directory Manager password"
-    exit 1
-fi
+usage() {
+    echo "Usage: $SCRIPT_NAME [OPTIONS]"
+    echo
+    echo "Options:"
+    echo "    --suffix=<DN>          Suffix (default: dc=example,dc=com)"
+    echo "    --base-dn=<DN>         Base DN (default: dc=pki,dc=example,dc=com)"
+    echo " -v,--verbose              Run in verbose mode."
+    echo "    --debug                Run in debug mode."
+    echo "    --help                 Show help message."
+}
 
-if [ "$IMAGE" == "" ]
-then
-    IMAGE=quay.io/389ds/dirsrv
-fi
+while getopts v-: arg ; do
+    case $arg in
+    v)
+        VERBOSE=true
+        ;;
+    -)
+        LONG_OPTARG="${OPTARG#*=}"
+
+        case $OPTARG in
+        suffix=?*)
+            SUFFIX="$LONG_OPTARG"
+            ;;
+        base-dn=?*)
+            BASE_DN="$LONG_OPTARG"
+            ;;
+        verbose)
+            VERBOSE=true
+            ;;
+        debug)
+            VERBOSE=true
+            DEBUG=true
+            ;;
+        help)
+            usage
+            exit
+            ;;
+        '')
+            break # "--" terminates argument processing
+            ;;
+        suffix* | base-dn*)
+            echo "ERROR: Missing argument for --$OPTARG option" >&2
+            exit 1
+            ;;
+        *)
+            echo "ERROR: Illegal option --$OPTARG" >&2
+            exit 1
+            ;;
+        esac
+        ;;
+    \?)
+        exit 1 # getopts already reported the illegal option
+        ;;
+    esac
+done
 
 create_server() {
 
@@ -39,8 +82,8 @@ create_server() {
         -e "s/;instance_name = .*/instance_name = localhost/g" \
         -e "s/;port = .*/port = 3389/g" \
         -e "s/;secure_port = .*/secure_port = 3636/g" \
-        -e "s/;root_password = .*/root_password = Secret.123/g" \
-        -e "s/;suffix = .*/suffix = dc=example,dc=com/g" \
+        -e "s/;root_password = .*/root_password = $PASSWORD/g" \
+        -e "s/;suffix = .*/suffix = $SUFFIX/g" \
         -e "s/;self_sign_cert = .*/self_sign_cert = False/g" \
         ds.inf
 
@@ -74,7 +117,7 @@ create_container() {
     echo "Creating database backend"
 
     docker exec $NAME dsconf localhost backend create \
-        --suffix dc=example,dc=com \
+        --suffix "$SUFFIX" \
         --be-name userRoot > /dev/null
 
     docker exec $NAME dsconf localhost backend suffix list
@@ -84,22 +127,68 @@ add_base_entries() {
 
     echo "Adding base entries"
 
+    SUFFIX_DC=$(echo "$SUFFIX" | sed 's/^dc=\([^,]*\),.*$/\1/')
+    BASE_DC=$(echo "$BASE_DN" | sed 's/^dc=\([^,]*\),.*$/\1/')
+
     docker exec -i $NAME ldapadd \
         -H ldap://$HOSTNAME:3389 \
         -D "cn=Directory Manager" \
         -w $PASSWORD \
         -x > /dev/null << EOF
-dn: dc=example,dc=com
+dn: $SUFFIX
 objectClass: domain
-dc: example
+dc: $SUFFIX_DC
 
-dn: dc=pki,dc=example,dc=com
+dn: $BASE_DN
 objectClass: domain
-dc: pki
+dc: $BASE_DC
 EOF
 }
 
-if [ "$IMAGE" == "pki-runner" ]
+# remove parsed options and args from $@ list
+shift $((OPTIND-1))
+
+NAME=$1
+
+if [ "$NAME" = "" ]
+then
+    echo "Usage: ds-container-create.sh <name>"
+    exit 1
+fi
+
+if [ "$PASSWORD" = "" ]
+then
+    echo "Missing Directory Manager password"
+    exit 1
+fi
+
+if [ "$IMAGE" = "" ]
+then
+    IMAGE=quay.io/389ds/dirsrv
+fi
+
+if [ "$SUFFIX" = "" ] && [ "$BASE_DN" = "" ]
+then
+    SUFFIX="dc=example,dc=com"
+    BASE_DN="dc=pki,$SUFFIX"
+
+elif [ "$SUFFIX" = "" ]
+then
+    SUFFIX=$(echo "$BASE_DN" | sed 's/^dc=[^,]*,\(.*$\)/\1/')
+
+elif [ "$BASE_DN" = "" ]
+then
+    BASE_DN="dc=pki,$SUFFIX"
+fi
+
+if [ "$DEBUG" = true ] ; then
+    echo "NAME: $NAME"
+    echo "IMAGE: $IMAGE"
+    echo "SUFFIX: $SUFFIX"
+    echo "BASE_DN: $BASE_DN"
+fi
+
+if [ "$IMAGE" = "pki-runner" ]
 then
     create_server
 else
@@ -113,6 +202,6 @@ docker exec $NAME ldapsearch \
     -D "cn=Directory Manager" \
     -w $PASSWORD \
     -x \
-    -b "dc=example,dc=com"
+    -b "$SUFFIX"
 
 echo "DS container is ready"
