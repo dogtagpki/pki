57552c439c675727733faba3107eb943073cd79d
Minsu Park <bakminsu@protonmail.com>
2024-03-29 19:38:10 -0500

    Use standard RFC naming

--- base/common/src/main/java/org/dogtagpki/acme/ACMEAlgorithm.java
+++ base/common/src/main/java/org/dogtagpki/acme/ACMEAlgorithm.java
@@ -8,17 +8,14 @@ public enum ACMEAlgorithm {
     // RFC 7518 Appendix A.1
     // Digital Signature/MAC Algorithm Identifier Cross-Reference
 
-    // Dogtag's JSS has slightly different algorithm names
-    // than what is in RFC 7518
-
     // TODO: Implement HS256, HS384, HS512 once JSS provider
     // implements those algorithms
     RS256("RS256", "SHA256withRSA"),
     RS384("RS384", "SHA384withRSA"),
     RS512("RS512", "SHA512withRSA"),
-    ES256("ES256", "SHA256withEC"),
-    ES384("ES384", "SHA384withEC"),
-    ES512("ES512", "SHA512withEC"),
+    ES256("ES256", "SHA256withECDSA"),
+    ES384("ES384", "SHA384withECDSA"),
+    ES512("ES512", "SHA512withECDSA"),
     PS256("PS256", "SHA256withRSA/PSS"),
     PS384("PS384", "SHA384withRSA/PSS"),
     PS512("PS512", "SHA512withRSA/PSS");
