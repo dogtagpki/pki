7f0647856bc839b66ca8b3931acb5cbd52c6e7e3
Marco Fargetta <mfargett@redhat.com>
2024-05-22 10:27:37 +0200

    Fix mispelled "extension" word

--- base/console/src/main/java/com/netscape/admin/certsrv/UIMapperRegistry.java
+++ base/console/src/main/java/com/netscape/admin/certsrv/UIMapperRegistry.java
@@ -48,7 +48,7 @@ public class UIMapperRegistry {
      *
      * @param className fully qualified class name implementing CertAttrSet
      * @param oid the string representation of the object identifier
-     * @param extentionName the name of the attribute.
+     * @param attrName the name of the attribute.
      * @param mapperClassName fully qualified class name implementing UI
      */
     /* XXX WE DON'T NEED THIS
@@ -68,7 +68,7 @@ public class UIMapperRegistry {
      * internal register new cert attr
      *
      * @param className fully qualified class name implementing CertAttrSet
-     * @param extentionName the name of the attribute.
+     * @param attrName the name of the attribute.
      * @param mapperClassName fully qualified class name implementing UI
      */
     public static void registerCertAttrUI(String attrName, String mapperClassName) {
