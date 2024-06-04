686341e106f06066ddf187689bac583c1f51dffe
Marco Fargetta <mfargett@redhat.com>
2024-06-04 12:20:46 +0200

    Modify CMCRevReqServlet to use paged search

--- base/ca/src/main/java/com/netscape/cms/servlet/cert/CMCRevReqServlet.java
+++ base/ca/src/main/java/com/netscape/cms/servlet/cert/CMCRevReqServlet.java
@@ -20,7 +20,6 @@ package com.netscape.cms.servlet.cert;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Vector;
 
@@ -60,8 +59,8 @@ import com.netscape.cms.servlet.common.ECMSGWException;
 import com.netscape.cmscore.apps.CMS;
 import com.netscape.cmscore.base.ArgBlock;
 import com.netscape.cmscore.dbs.CertRecord;
-import com.netscape.cmscore.dbs.CertRecordList;
 import com.netscape.cmscore.dbs.CertificateRepository;
+import com.netscape.cmscore.dbs.RecordPagedList;
 import com.netscape.cmscore.ldap.CAPublisherProcessor;
 import com.netscape.cmscore.logging.Auditor;
 import com.netscape.cmscore.request.CertRequestRepository;
@@ -401,11 +400,9 @@ public class CMCRevReqServlet extends CMSServlet {
                 entryExtn.set(invalidityDateExtn.getName(), invalidityDateExtn);
             }
 
-            CertRecordList list = mCertDB.findCertRecordsInList(revokeAll, null, totalRecordCount);
-            Enumeration<CertRecord> e = list.getCertRecords(0, totalRecordCount - 1);
+            RecordPagedList<CertRecord> list = mCertDB.findPagedCertRecords(revokeAll, null, null);
 
-            while (e != null && e.hasMoreElements()) {
-                CertRecord rec = e.nextElement();
+            for (CertRecord rec: list) {
                 X509CertImpl cert = rec.getCertificate();
                 ArgBlock rarg = new ArgBlock();
 
