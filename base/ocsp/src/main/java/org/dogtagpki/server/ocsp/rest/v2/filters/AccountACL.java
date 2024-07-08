4422e4797d98493c45ab3ced1d0b4dd725a7d1a8
Marco Fargetta <mfargett@redhat.com>
2024-07-08 18:56:49 +0200

    Implement v2 AccountService for OCSP

---
+++ base/ocsp/src/main/java/org/dogtagpki/server/ocsp/rest/v2/filters/AccountACL.java
@@ -0,0 +1,23 @@
+package org.dogtagpki.server.ocsp.rest.v2.filters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+
+import org.dogtagpki.server.rest.v2.ACLFilter;
+
+@WebFilter(servletNames = "ocspAccount")
+public class AccountACL extends ACLFilter {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void init() throws ServletException {
+        setAcl("account.login");
+        Map<String, String> aclMap = new HashMap<>();
+        aclMap.put("GET:/login", "account.login");
+        aclMap.put("GET:/logout", "account.logout");
+        setAclMap(aclMap);
+    }
+}
