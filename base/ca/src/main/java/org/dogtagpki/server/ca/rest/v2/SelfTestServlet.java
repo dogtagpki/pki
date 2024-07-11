8e94f5cca7712ae0a6b2d6fba5f0ede8cec62c1d
Marco Fargetta <mfargett@redhat.com>
2024-07-11 16:37:50 +0200

    Add SelfTestServlet to CA v2 APIs

---
+++ base/ca/src/main/java/org/dogtagpki/server/ca/rest/v2/SelfTestServlet.java
@@ -0,0 +1,35 @@
+//
+// Copyright Red Hat, Inc.
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+package org.dogtagpki.server.ca.rest.v2;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.dogtagpki.server.rest.v2.SelfTestServletBase;
+
+/**
+ * @author Marco Fargetta {@literal <mfargett@redhat.com>}
+ */
+@WebServlet(
+        name = "caSelfTests",
+        urlPatterns = "/v2/selftests/*")
+public class SelfTestServlet extends CAServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void get(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        SelfTestServletBase selfTestServlet = new SelfTestServletBase(getCAEngine());
+        selfTestServlet.get(request, response);
+    }
+
+    @Override
+    public void post(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        SelfTestServletBase selfTestServlet = new SelfTestServletBase(getCAEngine());
+        selfTestServlet.post(request, response);
+    }
+
+}
