dec96774982c0ad8793461c75288bbc7e5f257a1
Marco Fargetta <mfargett@redhat.com>
2024-07-12 17:27:27 +0200

    Add JobServlet to CA, KRA, OCSP, TKS and TPS v2 APIs

--- base/ocsp/src/main/java/org/dogtagpki/server/ocsp/rest/v2/filters/EmptyAuthMethod.java
+++ base/ocsp/src/main/java/org/dogtagpki/server/ocsp/rest/v2/filters/EmptyAuthMethod.java
@@ -3,13 +3,13 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 //
-package org.dogtagpki.server.tps.rest.v2.filters;
+package org.dogtagpki.server.ocsp.rest.v2.filters;
 
 import javax.servlet.annotation.WebFilter;
 
 import org.dogtagpki.server.rest.v2.AuthMethodFilter;
 
-@WebFilter(servletNames = {"activity", "tpsJobs"})
+@WebFilter(servletNames = {"ocspJobs"})
 public class EmptyAuthMethod extends AuthMethodFilter {
 
     private static final long serialVersionUID = 1L;
