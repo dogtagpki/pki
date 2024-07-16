b9c4eb6fb450814a0e3e6f2b40c16945e8f3cb87
Marco Fargetta <mfargett@redhat.com>
2024-07-16 11:43:21 +0200

    Update JobServlet to WebAction annotation

--- base/tks/src/main/java/org/dogtagpki/server/tks/rest/v2/JobServlet.java
+++ base/tks/src/main/java/org/dogtagpki/server/tks/rest/v2/JobServlet.java
@@ -5,11 +5,20 @@
 //
 package org.dogtagpki.server.tks.rest.v2;
 
+import java.io.PrintWriter;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.dogtagpki.job.JobCollection;
+import org.dogtagpki.job.JobInfo;
 import org.dogtagpki.server.rest.v2.JobServletBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netscape.certsrv.base.WebAction;
 
 /**
  * @author Marco Fargetta {@literal <mfargett@redhat.com>}
@@ -19,16 +28,37 @@ import org.dogtagpki.server.rest.v2.JobServletBase;
         urlPatterns = "/v2/jobs/*")
 public class JobServlet extends TKSServlet {
     private static final long serialVersionUID = 1L;
+    public static final Logger logger = LoggerFactory.getLogger(JobServlet.class);
 
-    @Override
-    public void get(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @WebAction(method = HttpMethod.GET, paths = { "/"})
+    public void getJobs(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+        logger.debug("JobServlet.getJobs(): session: {}", session.getId());
+        PrintWriter out = response.getWriter();
         JobServletBase jobServlet = new JobServletBase(getTKSEngine());
-        jobServlet.get(request, response);
+        JobCollection jobs = jobServlet.findJobs(request.getUserPrincipal());
+        out.println(jobs.toJSON());
     }
 
-    @Override
-    public void post(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @WebAction(method = HttpMethod.GET, paths = { "/{}"})
+    public void getJob(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+        logger.debug("JobServlet.getJob(): session: {}", session.getId());
+        String[] pathElement = request.getPathInfo().substring(1).split("/");
+        String jobId = pathElement[0];
         JobServletBase jobServlet = new JobServletBase(getTKSEngine());
-        jobServlet.post(request, response);
+        JobInfo job = jobServlet.getJob(jobId, request.getUserPrincipal());
+        PrintWriter out = response.getWriter();
+        out.println(job.toJSON());
+    }
+
+    @WebAction(method = HttpMethod.POST, paths = {"/{}/start"})
+    public void postJobStart(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+        logger.debug("JobServlet.postJobStart(): session: {}", session.getId());
+        String[] pathElement = request.getPathInfo().substring(1).split("/");
+        String jobId = pathElement[0];
+        JobServletBase jobServlet = new JobServletBase(getTKSEngine());
+        jobServlet.startJob(jobId, request.getUserPrincipal());
     }
 }
