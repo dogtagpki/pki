e6bb0a6c0471f83c6fbff82e5b327cee231f90a8
Marco Fargetta <mfargett@redhat.com>
2024-07-23 15:21:58 +0200

    Add FeatureService to v2 APIs

---
+++ base/ca/src/main/java/org/dogtagpki/server/ca/rest/v2/CAFeatureServlet.java
@@ -0,0 +1,20 @@
+//
+// Copyright Red Hat, Inc.
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+package org.dogtagpki.server.ca.rest.v2;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.dogtagpki.server.rest.v2.FeatureServlet;
+
+/**
+ * @author Marco Fargetta {@literal <mfargett@redhat.com>}
+ */
+@WebServlet(
+        name = "caFeature",
+        urlPatterns = "/v2/config/features/*")
+public class CAFeatureServlet extends FeatureServlet {
+    private static final long serialVersionUID = 1L;
+}
