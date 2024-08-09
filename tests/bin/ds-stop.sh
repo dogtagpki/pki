e813063d0e90b99d91b403b1e7f3b93a8468182d
Endi S. Dewata <edewata@redhat.com>
2024-08-09 12:35:59 -0500

    Replace ds-container-stop.sh with ds-stop.sh

---
+++ tests/bin/ds-stop.sh
@@ -0,0 +1,89 @@
+#!/bin/bash -e
+
+# https://fy.blackhats.net.au/blog/html/2020/03/28/389ds_in_containers.html
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_NAME=$(basename "$SCRIPT_PATH")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+VERBOSE=
+DEBUG=
+
+usage() {
+    echo "Usage: $SCRIPT_NAME [OPTIONS] <name>"
+    echo
+    echo "Options:"
+    echo "    --image=<image>          Container image (default: quay.io/389ds/dirsrv)"
+    echo " -v,--verbose                Run in verbose mode."
+    echo "    --debug                  Run in debug mode."
+    echo "    --help                   Show help message."
+}
+
+while getopts v-: arg ; do
+    case $arg in
+    v)
+        VERBOSE=true
+        ;;
+    -)
+        LONG_OPTARG="${OPTARG#*=}"
+
+        case $OPTARG in
+        image=?*)
+            IMAGE="$LONG_OPTARG"
+            ;;
+        verbose)
+            VERBOSE=true
+            ;;
+        debug)
+            VERBOSE=true
+            DEBUG=true
+            ;;
+        help)
+            usage
+            exit
+            ;;
+        '')
+            break # "--" terminates argument processing
+            ;;
+        image*)
+            echo "ERROR: Missing argument for --$OPTARG option" >&2
+            exit 1
+            ;;
+        *)
+            echo "ERROR: Illegal option --$OPTARG" >&2
+            exit 1
+            ;;
+        esac
+        ;;
+    \?)
+        exit 1 # getopts already reported the illegal option
+        ;;
+    esac
+done
+
+# remove parsed options and args from $@ list
+shift $((OPTIND-1))
+
+NAME=$1
+
+if [ "$NAME" == "" ]
+then
+    echo "ERROR: Missing container name"
+    exit 1
+fi
+
+if [ "$IMAGE" = "" ]
+then
+    IMAGE=quay.io/389ds/dirsrv
+fi
+
+echo "Stopping DS container"
+
+if [ "$IMAGE" == "pki-runner" ]
+then
+    docker exec $NAME dsctl localhost stop
+else
+    docker stop $NAME > /dev/null
+fi
+
+echo "DS container is stopped"
