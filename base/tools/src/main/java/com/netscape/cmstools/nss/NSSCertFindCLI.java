0d5eccbd9dcae97b2e632374712e9d9a003ed534
Endi S. Dewata <edewata@redhat.com>
2024-08-26 13:41:25 -0500

    Add pki nss-cert-find --subject and --issuer options

--- base/tools/src/main/java/com/netscape/cmstools/nss/NSSCertFindCLI.java
+++ base/tools/src/main/java/com/netscape/cmstools/nss/NSSCertFindCLI.java
@@ -5,10 +5,12 @@
 //
 package com.netscape.cmstools.nss;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
 import org.dogtagpki.cli.CommandCLI;
 import org.mozilla.jss.crypto.CryptoStore;
 import org.mozilla.jss.crypto.CryptoToken;
@@ -31,25 +33,55 @@ public class NSSCertFindCLI extends CommandCLI {
         formatter.printHelp(getFullName() + " [OPTIONS...]", options);
     }
 
-    public Collection<X509Certificate> findAllCerts() throws Exception {
+    @Override
+    public void createOptions() {
+        Option option = new Option(null, "subject", true, "Subject DN");
+        option.setArgName("DN");
+        options.addOption(option);
 
-        logger.info("Searching for all certs");
+        option = new Option(null, "issuer", true, "Issuer DN");
+        option.setArgName("DN");
+        options.addOption(option);
+    }
+
+    public Collection<X509Certificate> findCerts(
+            String subject,
+            String issuer
+            ) throws Exception {
+
+        logger.info("Searching for certs");
         String tokenName = getConfig().getTokenName();
         CryptoToken token = CryptoUtil.getKeyStorageToken(tokenName);
         CryptoStore store = token.getCryptoStore();
 
-        return Arrays.asList(store.getCertificates());
+        List<X509Certificate> results = new ArrayList<>();
+        for (X509Certificate cert : store.getCertificates()) {
+
+            if (subject != null && !subject.equals(cert.getSubjectDN().toString())) {
+                continue;
+            }
+
+            if (issuer != null && !issuer.equals(cert.getIssuerDN().toString())) {
+                continue;
+            }
+
+            results.add(cert);
+        }
+        return results;
     }
 
     @Override
     public void execute(CommandLine cmd) throws Exception {
+
+        String subject = cmd.getOptionValue("subject");
+        String issuer = cmd.getOptionValue("issuer");
 
         MainCLI mainCLI = (MainCLI) getRoot();
         mainCLI.init();
 
         boolean first = true;
 
-        for (X509Certificate cert : findAllCerts()) {
+        for (X509Certificate cert : findCerts(subject, issuer)) {
 
             if (first) {
                 first = false;
