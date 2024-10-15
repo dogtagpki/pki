0c1cd6ab7b3ebcb22a4d47ec4435c197b93e00a1
Endi S. Dewata <edewata@redhat.com>
2024-10-15 17:56:57 -0500

    Add ca-<type>-range-objects.sh scripts
    The ca-<type>-range-objects.sh scripts have been added to
    simplify range object verification.

---
+++ tests/ca/bin/ca-cert-range-objects.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+NAME=$1
+
+LIST=$(docker exec $NAME ldapsearch \
+    -H ldap://$NAME.example.com:3389 \
+    -D "cn=Directory Manager" \
+    -w Secret.123 \
+    -b ou=certificateRepository,ou=ranges,dc=ca,dc=pki,dc=example,dc=com \
+    -s one \
+    -o ldif_wrap=no \
+    -LLL \
+    dn \
+    | sed -n 's/^dn: *\(.*\)$/\1/p')
+
+for DN in $LIST
+do
+    docker exec $NAME ldapsearch \
+        -H ldap://$NAME.example.com:3389 \
+        -D "cn=Directory Manager" \
+        -w Secret.123 \
+        -b $DN \
+        -s base \
+        -o ldif_wrap=no \
+        -LLL \
+        | grep \
+            -e SecurePort: \
+            -e beginRange: \
+            -e endRange: \
+            -e host: \
+        | sort
+
+    echo
+done
