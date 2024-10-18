108fe1ba8c0fc224852e38f5f4e50c3101806932
Endi S. Dewata <edewata@redhat.com>
2024-10-18 15:37:50 -0500

    Add tests for next begin/end numbers
    The tests for CA with sequential serial numbers have been
    updated to verify the dbs.nextBegin... and dbs.nextEnd...
    parameters.

--- tests/ca/bin/ca-cert-range-config.sh
+++ tests/ca/bin/ca-cert-range-config.sh
@@ -6,6 +6,8 @@ docker exec $NAME pki-server ca-config-find \
     | grep \
         -e dbs.beginSerialNumber \
         -e dbs.endSerialNumber \
+        -e dbs.nextBeginSerialNumber \
+        -e dbs.nextEndSerialNumber \
         -e dbs.serialCloneTransferNumber \
         -e dbs.serialIncrement \
         -e dbs.serialLowWaterMark
