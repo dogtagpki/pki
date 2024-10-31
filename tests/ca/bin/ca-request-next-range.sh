e7f430cf1c78fc7958fcfab7eb2b7d5cbc6149b1
Endi S. Dewata <edewata@redhat.com>
2024-10-31 14:42:52 -0500

    Update SSNv1 and SSNv2 tests
    The SSNv1 and SSNv2 tests have been updated to use the new
    SSNv2 nextRange location.

--- tests/ca/bin/ca-request-next-range.sh
+++ tests/ca/bin/ca-request-next-range.sh
@@ -1,12 +1,32 @@
 #!/bin/bash -e
 
+GENERATOR_TYPE=legacy
+
+while getopts "t:" arg ; do
+    case $arg in
+    t)
+        GENERATOR_TYPE=$OPTARG
+        ;;
+    esac
+done
+
+# remove parsed options and args from $@ list
+shift $((OPTIND-1))
+
 NAME=$1
+
+if [ "$GENERATOR_TYPE" == "legacy2" ]
+then
+    RANGE_DN=ou=requests,ou=ranges_v2,dc=ca,dc=pki,dc=example,dc=com
+else
+    RANGE_DN=ou=ca,ou=requests,dc=ca,dc=pki,dc=example,dc=com
+fi
 
 docker exec $NAME ldapsearch \
     -H ldap://$NAME.example.com:3389 \
     -D "cn=Directory Manager" \
     -w Secret.123 \
-    -b ou=ca,ou=requests,dc=ca,dc=pki,dc=example,dc=com \
+    -b $RANGE_DN \
     -s base \
     -o ldif_wrap=no \
     -LLL \
