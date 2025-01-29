448d9a1551411bdf646710923ced1fd1daaf9383
Endi S. Dewata <edewata@redhat.com>
2025-01-29 12:56:06 -0600

    Fix pki_server_external_certs_path
    Previously if the pki_server_pkcs12_path was not specified
    the external certs specified in pki_server_external_certs_path
    would not be imported either. This is incorrect since these
    parameters are unrelated.
    
    To address the issue the code that imports the external certs
    in PKIDeployer.import_server_pkcs12() has been moved into
    import_external_certs() which will always be invoked during
    installation. The update_external_certs_conf() has been merged
    into this method as well.
    
    The PKIInstance.load_external_certs() has been modified to add
    the external cert using add_external_cert() to avoid creating
    duplicate entries in the external_certs.conf.
    
    The PKIInstance.store_external_certs() has been modified to
    remove the external_certs.conf if it's empty.
    
    In the future the pki_server_external_certs_path param and the
    pki-server instance-externalcert-* commands might be deprecated
    and eventually removed since there are other ways to deal with
    external certs without having to maintain external_certs.conf.

--- base/server/python/pki/server/deployment/scriptlets/security_databases.py
+++ base/server/python/pki/server/deployment/scriptlets/security_databases.py
@@ -39,6 +39,7 @@ class PkiScriptlet(pkiscriptlet.AbstractBasePkiScriptlet):
         deployer.import_server_pkcs12()
         deployer.import_clone_pkcs12()
         deployer.install_cert_chain()
+        deployer.import_external_certs()
 
         if config.str2bool(deployer.mdict['pki_ds_setup']):
             deployer.import_ds_ca_cert()
