84ab3d4304990a16397ffc40cd50c0a0a44e625a
Endi S. Dewata <edewata@redhat.com>
2025-03-10 21:54:09 -0500

    Convert TPSClientCLI.resetPIN() to Java
    The TPSClientCLI.resetPIN() has been converted into Java. The
    old style method will use Java threads to call the native code
    that performs the actual operation. The new style method is
    still implemented in C++ and might be converted into Java in
    the future. The RA_Client::OpConnResetPin() has been moved
    into tpsclient.cpp.
    
    The basic TPS test has been updated to perform PIN reset.

--- base/tools/src/main/java/com/netscape/cmstools/tps/TPSClientCLI.java
+++ base/tools/src/main/java/com/netscape/cmstools/tps/TPSClientCLI.java
@@ -121,7 +121,62 @@ public class TPSClientCLI extends CommandCLI {
         }
     }
 
-    public native void resetPIN(long client, Map<String, String> params) throws Exception;
+    public native void performResetPIN(long client, Map<String, String> params) throws Exception;
+
+    public void oldResetPIN(
+            long client,
+            Map<String, String> params)
+            throws Exception {
+
+        String value = params.get("num_threads");
+        int numThreads = value == null ? 1 : Integer.parseInt(value);
+
+        Thread[] threads = new Thread[numThreads];
+        Exception[] exceptions = new Exception[numThreads];
+
+        // start threads
+        for (int i=0; i<numThreads; i++) {
+
+            int index = i;
+
+            threads[i] = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        performResetPIN(client, params);
+                    } catch (Exception e) {
+                        exceptions[index] = e;
+                    }
+                }
+            });
+
+            threads[i].start();
+        }
+
+        // wait for threads to complete
+        for (int i=0; i<numThreads; i++) {
+            threads[i].join();
+        }
+
+        // check for exceptions
+        for (int i=0; i<numThreads; i++) {
+            if (exceptions[i] != null) throw exceptions[i];
+        }
+    }
+
+    public native void newResetPIN(long client, Map<String, String> params) throws Exception;
+
+    public void resetPIN(
+            long client,
+            Map<String, String> params)
+            throws Exception {
+
+        if (getOldStyle(client)) {
+            oldResetPIN(client, params);
+        } else {
+            newResetPIN(client, params);
+        }
+    }
+
     public native void enrollToken(long client, Map<String, String> params) throws Exception;
     public native void displayToken(long client, Map<String, String> params) throws Exception;
     public native void setupToken(long client, Map<String, String> params) throws Exception;
