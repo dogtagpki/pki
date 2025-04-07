3550611305dfca8172048f281625b0a10dc569b3
Marco Fargetta <mfargett@redhat.com>
2025-04-07 18:34:31 +0200

    Replace httpclient deprecated code
    PKI client connection are based on "DefaultHttpClient" and
    "SchemeLayeredSocketFactory" but these classes have been deprecated and will
    be removed so the code is updated to use their replacement.

--- base/common/src/main/java/com/netscape/certsrv/client/PKIConnection.java
+++ base/common/src/main/java/com/netscape/certsrv/client/PKIConnection.java
@@ -37,25 +37,23 @@ import org.apache.http.HttpRequest;
 import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpResponseInterceptor;
-import org.apache.http.ProtocolException;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.params.HttpClientParams;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeLayeredSocketFactory;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
 import org.apache.http.entity.BufferedHttpEntity;
-import org.apache.http.impl.client.ClientParamsStack;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
-import org.apache.http.impl.client.DefaultRedirectStrategy;
-import org.apache.http.impl.client.EntityEnclosingRequestWrapper;
-import org.apache.http.impl.client.RequestWrapper;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.message.BasicHttpResponse;
-import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
 import org.dogtagpki.client.JSSSocketFactory;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
 import org.mozilla.jss.ssl.SSLCertificateApprovalCallback;
+
+import com.netscape.certsrv.base.PKIException;
 
 public class PKIConnection implements AutoCloseable {
 
@@ -63,9 +61,9 @@ public class PKIConnection implements AutoCloseable {
 
     ClientConfig config;
 
-    DefaultHttpClient httpClient = new DefaultHttpClient();
+    CloseableHttpClient httpClient;
     SSLCertificateApprovalCallback callback;
-    SchemeLayeredSocketFactory socketFactory;
+    LayeredConnectionSocketFactory socketFactory;
 
     ApacheHttpClient4Engine engine;
     javax.ws.rs.client.Client client;
@@ -86,20 +84,18 @@ public class PKIConnection implements AutoCloseable {
                 JSSSocketFactory.class.getName());
         logger.info("PKIConnection: Socket factory: " + className);
 
-        Class<? extends SchemeLayeredSocketFactory> clazz =
-                Class.forName(className).asSubclass(SchemeLayeredSocketFactory.class);
+        Class<? extends LayeredConnectionSocketFactory> clazz =
+                Class.forName(className).asSubclass(LayeredConnectionSocketFactory.class);
 
         socketFactory = clazz.getConstructor(PKIConnection.class).newInstance(this);
 
-        // Register https scheme.
-        Scheme scheme = new Scheme("https", 443, socketFactory);
-
-        httpClient.getConnectionManager().getSchemeRegistry().register(scheme);
+        HttpClientBuilder httpClientBuilder = HttpClients.custom().setSSLSocketFactory(socketFactory);
 
         // Don't retry operations.
-        httpClient.setHttpRequestRetryHandler(new DefaultHttpRequestRetryHandler(0, false));
+        httpClientBuilder.setRetryHandler(new DefaultHttpRequestRetryHandler(0, false));
 
-        httpClient.addRequestInterceptor(new HttpRequestInterceptor() {
+
+        httpClientBuilder.addInterceptorFirst(new HttpRequestInterceptor() {
             @Override
             public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
 
@@ -132,17 +128,17 @@ public class PKIConnection implements AutoCloseable {
                     logger.debug("Request:\n" + os.toString("UTF-8"));
                 }
 
-                // Set the request parameter to follow redirections.
-                HttpParams params = request.getParams();
-                if (params instanceof ClientParamsStack) {
-                    ClientParamsStack paramsStack = (ClientParamsStack)request.getParams();
-                    params = paramsStack.getRequestParams();
+                if (context instanceof HttpClientContext clientContext) {
+                    RequestConfig reqConfig = RequestConfig.copy(clientContext.getRequestConfig())
+                            .setRedirectsEnabled(true)
+                            .setRelativeRedirectsAllowed(true)
+                            .build();
+                    clientContext.setRequestConfig(reqConfig);
                 }
-                HttpClientParams.setRedirecting(params, true);
             }
         });
 
-        httpClient.addResponseInterceptor(new HttpResponseInterceptor() {
+        httpClientBuilder.addInterceptorFirst(new HttpResponseInterceptor() {
             @Override
             public void process(HttpResponse response, HttpContext context) throws HttpException, IOException {
 
@@ -170,38 +166,9 @@ public class PKIConnection implements AutoCloseable {
             }
         });
 
-        httpClient.setRedirectStrategy(new DefaultRedirectStrategy() {
-            @Override
-            public HttpUriRequest getRedirect(HttpRequest request, HttpResponse response, HttpContext context)
-                    throws ProtocolException {
+        httpClientBuilder.setRedirectStrategy(new LaxRedirectStrategy());
 
-                HttpUriRequest uriRequest = super.getRedirect(request, response, context);
-
-                URI uri = uriRequest.getURI();
-                logger.info("HTTP redirect: "+uri);
-
-                // Redirect the original request to the new URI.
-                RequestWrapper wrapper;
-                if (request instanceof HttpEntityEnclosingRequest) {
-                    wrapper = new EntityEnclosingRequestWrapper((HttpEntityEnclosingRequest)request);
-                } else {
-                    wrapper = new RequestWrapper(request);
-                }
-                wrapper.setURI(uri);
-
-                return wrapper;
-            }
-
-            @Override
-            public boolean isRedirected(HttpRequest request, HttpResponse response, HttpContext context)
-                    throws ProtocolException {
-
-                // The default redirection policy does not redirect POST or PUT.
-                // This overrides the policy to follow redirections for all HTTP methods.
-                return response.getStatusLine().getStatusCode() == 302;
-            }
-        });
-
+        httpClient = httpClientBuilder.build();
         engine = new ApacheHttpClient4Engine(httpClient);
 
         client = new ResteasyClientBuilder().httpEngine(engine).build();
@@ -227,15 +194,14 @@ public class PKIConnection implements AutoCloseable {
 
     public void storeRequest(PrintStream out, HttpRequest request) throws IOException {
 
-        if (request instanceof EntityEnclosingRequestWrapper) {
-            EntityEnclosingRequestWrapper wrapper = (EntityEnclosingRequestWrapper) request;
+        if (request instanceof HttpEntityEnclosingRequest enclose) {
 
-            HttpEntity entity = wrapper.getEntity();
+            HttpEntity entity = enclose.getEntity();
             if (entity == null) return;
 
             if (!entity.isRepeatable()) {
                 BufferedHttpEntity bufferedEntity = new BufferedHttpEntity(entity);
-                wrapper.setEntity(bufferedEntity);
+                enclose.setEntity(bufferedEntity);
                 entity = bufferedEntity;
             }
 
@@ -289,6 +255,11 @@ public class PKIConnection implements AutoCloseable {
     public void close() {
         client.close();
         engine.close();
-        httpClient.close();
+        try {
+            httpClient.close();
+        } catch (IOException e) {
+            logger.error("PKIConnection: cannot close httpClient - {}", e.getMessage());
+            throw new PKIException(e);
+        }
     }
 }
