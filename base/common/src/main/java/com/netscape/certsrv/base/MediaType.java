233a282d930aa528b8efed34c8036ca7dac7a072
Marco Fargetta <mfargett@redhat.com>
2025-04-09 18:05:23 +0200

    Converting a CLI HTTP GET to httpclient
    PKIClient implement several GET operations. One of these operation is
    converted from RESTEasy client to apache httpcomponent-client to verify
    the implementation.

--- base/common/src/main/java/com/netscape/certsrv/base/MediaType.java
+++ base/common/src/main/java/com/netscape/certsrv/base/MediaType.java
@@ -16,6 +16,8 @@ public class MediaType {
 
     public static final String APPLICATION_JSON = "application/json";
 
+    public static final String APPLICATION_XML = "application/xml";
+
     public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
 
     public static final String APPLICATION_PKIX_CERT = "application/pkix-cert";
