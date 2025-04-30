f75662f001dc5745aede7382f804ed5ffa33bb6a
Endi S. Dewata <edewata@redhat.com>
2025-04-30 14:17:45 -0500

    Update clone tests to check replica ID ranges
    The CA and KRA clone tests have been modified to check the
    changes to replica ID ranges throughout the cloning process
    to prevent regressions.
    
    Some scripts have been added to check replica ID ranges in
    CS.cfg and DS.

---
+++ tests/kra/bin/kra-replica-next-range.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+NAME=$1
+
+RANGE_DN=ou=replica,dc=kra,dc=pki,dc=example,dc=com
+
+docker exec $NAME ldapsearch \
+    -H ldap://$NAME.example.com:3389 \
+    -D "cn=Directory Manager" \
+    -w Secret.123 \
+    -b $RANGE_DN \
+    -s base \
+    -o ldif_wrap=no \
+    -LLL \
+    | grep nextRange:
