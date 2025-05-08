2959d0515ba8b3794fed1dcd2e334a0e70dcec9d
Marco Fargetta <mfargett@redhat.com>
2025-05-08 15:37:59 +0200

    Remove RESTEasy based services from ACME

--- base/common/src/main/java/org/dogtagpki/acme/ACMEException.java
+++ base/common/src/main/java/org/dogtagpki/acme/ACMEException.java
@@ -7,15 +7,21 @@ package org.dogtagpki.acme;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.netscape.certsrv.base.PKIException;
-
+/**
+ * ACME exception.
+ *
+ * @author Marco Fargetta {@literal <mfargett@redhat.com>}
+ */
 public class ACMEException extends PKIException {
 
     private static final long serialVersionUID = 1L;
     private ACMEError error;
+
     public ACMEException(int code, ACMEError error) {
         super(code, error.getDetail());
         this.error = error;
     }
+
     @Override
     public String getSerializedFormat() {
         return "application/problem+json";
