b063a02ff2ba8f0831b725c097075c6b80cb270e
gkimetto <gkimetto@gmail.com>
2025-12-09 12:01:20 -0500

    [DEV] [FIX] OCSP response provides a SHA1 signature in the header packet #DOGTAG-4129
      Add support for detecting and optionally rejecting OCSP requests that
      use deprecated digest algorithms (MD2, MD5, SHA-1), and allow
      configuration of the OCSP response signing algorithm.
    
    Doc update:
    
     Add this  CS.cfg Configuration for CA's built in responder in pki-tomcat:
      # /var/lib/pki/{instance}/conf/ca/CS.cfg
      # Reject deprecated algorithms (default: false)
      ca.ocspRejectDeprecatedAlgorithms=true
      # Configure response signing algorithm (default: use signing unit default)
      ca.ocspResponseSigningAlgorithm=SHA256withRSA
     Alternatively for Standalone OCSP Responder
      Edit /var/lib/pki/<instance>/ocsp/CS.cfg and add:
    
      ocsp.rejectDeprecatedAlgorithms=true
      ocsp.responseSigningAlgorithm=SHA256withRSA
    
      Supported Signing Algorithms:
    
      - SHA256withRSA
      - SHA384withRSA
      - SHA512withRSA
      - SHA256withEC
      - SHA384withEC
      - SHA512withEC
    
      After making changes, restart the PKI instance:
    
      systemctl restart pki-tomcatd@<instance>.service
    
    Fix: DOGTAG-4129

--- base/ocsp/src/main/java/org/dogtagpki/server/ocsp/OCSPConfig.java
+++ base/ocsp/src/main/java/org/dogtagpki/server/ocsp/OCSPConfig.java
@@ -5,6 +5,7 @@
 //
 package org.dogtagpki.server.ocsp;
 
+import com.netscape.certsrv.base.EBaseException;
 import com.netscape.certsrv.security.SigningUnitConfig;
 import com.netscape.cmscore.base.ConfigStorage;
 import com.netscape.cmscore.base.ConfigStore;
@@ -29,4 +30,50 @@ public class OCSPConfig extends ConfigStore {
     public SigningUnitConfig getSigningUnitConfig() {
         return getSubStore("signing", SigningUnitConfig.class);
     }
+
+    /**
+     * Returns the comma-separated list of digest algorithms to reject in OCSP requests.
+     * If not configured or empty, all algorithms are accepted.
+     * In the OCSP CS.cfg the param ocsp.rejectAlgorithms=SHA-1,MD5,MD2 is added
+     *
+     * @return the list of algorithm names to reject, or empty string if not set
+     * @throws EBaseException if configuration cannot be read
+     */
+    public String getRejectAlgorithms() throws EBaseException {
+        return getString("rejectAlgorithms", "");
+    }
+
+    /**
+     * Sets the list of digest algorithms to reject in OCSP requests.
+     *
+     * @param algorithms comma-separated list of algorithm names to reject
+     */
+    public void setRejectAlgorithms(String algorithms) {
+        if (algorithms == null || algorithms.isEmpty()) {
+            remove("rejectAlgorithms");
+        } else {
+            putString("rejectAlgorithms", algorithms);
+        }
+    }
+
+    /**
+     * Checks if a digest algorithm should be rejected in OCSP requests.
+     *
+     * @param algorithm the algorithm name to check (e.g., "SHA-1", "MD5")
+     * @return true if the algorithm should be rejected, false otherwise
+     * @throws EBaseException if configuration cannot be read
+     */
+    public boolean isAlgorithmRejected(String algorithm) throws EBaseException {
+        String rejectList = getRejectAlgorithms();
+        if (rejectList == null || rejectList.isEmpty()) {
+            return false;
+        }
+        for (String rejected : rejectList.split(",")) {
+            if (rejected.trim().equalsIgnoreCase(algorithm)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
