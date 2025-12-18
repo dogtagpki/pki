205623db9f25327b456a3756da0f1cadbf66f204
Endi S. Dewata <edewata@redhat.com>
2025-12-19 01:52:57 +0700

    Convert pki-server <subsystem>-acl-* to Java
    In order to minimize the number of NSS authentications the
    pki-server <subsystem>-acl-* commands used during installation
    have been converted to run the corresponding Java code directly
    so later they can be executed in shell/batch mode.

--- base/server/python/pki/server/cli/acl.py
+++ base/server/python/pki/server/cli/acl.py
@@ -6,7 +6,6 @@
 
 import argparse
 import logging
-import sys
 
 import pki.cli
 
@@ -67,42 +66,6 @@ class SubsystemACLFindCLI(pki.cli.CLI):
         print('      --help                         Show help message.')
         print()
 
-    def execute(self, argv, args=None):
-
-        if not args:
-            args = self.parser.parse_args(args=argv)
-
-        if args.help:
-            self.print_help()
-            return
-
-        if args.debug:
-            logging.getLogger().setLevel(logging.DEBUG)
-
-        elif args.verbose:
-            logging.getLogger().setLevel(logging.INFO)
-
-        instance_name = args.instance
-        subsystem_name = self.parent.parent.name
-        as_current_user = args.as_current_user
-
-        instance = pki.server.instance.PKIInstance(instance_name)
-
-        if not instance.exists():
-            logger.error('Invalid instance: %s', instance_name)
-            sys.exit(1)
-
-        instance.load()
-
-        subsystem = instance.get_subsystem(subsystem_name)
-
-        if not subsystem:
-            logger.error('No %s subsystem in instance %s.',
-                         subsystem_name.upper(), instance_name)
-            sys.exit(1)
-
-        subsystem.find_acl(as_current_user=as_current_user)
-
 
 class SubsystemACLAddCLI(pki.cli.CLI):
 
@@ -149,45 +112,6 @@ class SubsystemACLAddCLI(pki.cli.CLI):
         print('      --help                         Show help message.')
         print()
 
-    def execute(self, argv, args=None):
-
-        if not args:
-            args = self.parser.parse_args(args=argv)
-
-        if args.help:
-            self.print_help()
-            return
-
-        if args.debug:
-            logging.getLogger().setLevel(logging.DEBUG)
-
-        elif args.verbose:
-            logging.getLogger().setLevel(logging.INFO)
-
-        instance_name = args.instance
-        subsystem_name = self.parent.parent.name
-        as_current_user = args.as_current_user
-        acl = args.acl
-        instance = pki.server.instance.PKIInstance(instance_name)
-
-        if acl is None:
-            raise pki.cli.CLIException('Missing ACL')
-
-        if not instance.exists():
-            logger.error('Invalid instance: %s', instance_name)
-            sys.exit(1)
-
-        instance.load()
-
-        subsystem = instance.get_subsystem(subsystem_name)
-
-        if not subsystem:
-            logger.error('No %s subsystem in instance %s.',
-                         subsystem_name.upper(), instance_name)
-            sys.exit(1)
-
-        subsystem.add_acl(acl, as_current_user=as_current_user)
-
 
 class SubsystemACLDeleteCLI(pki.cli.CLI):
 
@@ -233,42 +157,3 @@ class SubsystemACLDeleteCLI(pki.cli.CLI):
         print('      --debug                        Run in debug mode.')
         print('      --help                         Show help message.')
         print()
-
-    def execute(self, argv, args=None):
-
-        if not args:
-            args = self.parser.parse_args(args=argv)
-
-        if args.help:
-            self.print_help()
-            return
-
-        if args.debug:
-            logging.getLogger().setLevel(logging.DEBUG)
-
-        elif args.verbose:
-            logging.getLogger().setLevel(logging.INFO)
-
-        instance_name = args.instance
-        subsystem_name = self.parent.parent.name
-        as_current_user = args.as_current_user
-        acl = args.acl
-        instance = pki.server.instance.PKIInstance(instance_name)
-
-        if acl is None:
-            raise pki.cli.CLIException('Missing ACL')
-
-        if not instance.exists():
-            logger.error('Invalid instance: %s', instance_name)
-            sys.exit(1)
-
-        instance.load()
-
-        subsystem = instance.get_subsystem(subsystem_name)
-
-        if not subsystem:
-            logger.error('No %s subsystem in instance %s.',
-                         subsystem_name.upper(), instance_name)
-            sys.exit(1)
-
-        subsystem.delete_acl(acl, as_current_user=as_current_user)
