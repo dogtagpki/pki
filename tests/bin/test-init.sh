2c8e81a6f5ce3ad7b41a1471b7b59b8c11d33947
Endi S. Dewata <edewata@redhat.com>
2026-02-10 15:30:43 -0600

    Fix default values in test-init.sh
    The test-init.sh has been updated to set the default values
    of environment variables properly for all branches.

--- tests/bin/test-init.sh
+++ tests/bin/test-init.sh
@@ -1,24 +1,36 @@
 #!/bin/bash -e
 
+release_branch='^v[0-9]+\.[0-9]+$'
+release_branch_with_suffix='^v[0-9]+\.[0-9]+-.*$'
+
 if [ "$BASE_IMAGE" = "" ]; then
-    # For master branch use fedora:latest to provide a stable development
-    # platform. For other branches use the target Fedora version.
-    if [ "$BRANCH_NAME" = "master" ]; then
-        BASE_IMAGE=registry.fedoraproject.org/fedora:latest
+    # By default use fedora:latest to provide a stable development platform.
+    # For release branches use the target Fedora version (e.g. rawhide).
+    if [[ "$BRANCH_NAME" =~ ^v11\.9$ ]] \
+            || [[ "$BRANCH_NAME" =~ ^v11\.9-.*$ ]]; then
+        BASE_IMAGE=registry.fedoraproject.org/fedora:44
         echo "BASE_IMAGE=$BASE_IMAGE" | tee -a $GITHUB_ENV
 
-    elif [ "$BRANCH_NAME" = "v11.9" ]; then
-        BASE_IMAGE=registry.fedoraproject.org/fedora:44
+    elif [[ "$BRANCH_NAME" =~ $release_branch ]] \
+            || [[ "$BRANCH_NAME" =~ $release_branch_with_suffix ]]; then
+        BASE_IMAGE=registry.fedoraproject.org/fedora:rawhide
+        echo "BASE_IMAGE=$BASE_IMAGE" | tee -a $GITHUB_ENV
+
+    else
+        BASE_IMAGE=registry.fedoraproject.org/fedora:latest
         echo "BASE_IMAGE=$BASE_IMAGE" | tee -a $GITHUB_ENV
     fi
 fi
 
 if [ "$COPR_REPO" = "" ]; then
-    # For master branch use @pki/master to introduce new dependencies.
-    # For other branches don't use COPR repo since the dependencies
-    # should have already been added into the official Fedora repository.
-    if [ "$BRANCH_NAME" = "master" ]
-    then
+    # By default use @pki/master for development (e.g. to try new dependencies).
+    # For release branches don't use COPR repo since the dependencies should
+    # have already been added into the official Fedora repository.
+    if [[ "$BRANCH_NAME" =~ $release_branch ]] \
+            || [[ "$BRANCH_NAME" =~ $release_branch_with_suffix ]]; then
+        : # skip
+
+    else
         COPR_REPO=@pki/master
         echo "COPR_REPO=$COPR_REPO" | tee -a $GITHUB_ENV
     fi
