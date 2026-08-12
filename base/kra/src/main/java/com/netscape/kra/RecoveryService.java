4cc39cabf63188e3de4d711c5c4c736151e52413
jmagne <jmagne@redhat.com>
2026-08-12 16:08:49 -0700

    Fix: DOGTAG-4408 SSKG Fails on Thales HSM with error CKR_ATTRIBUTE_VALUE_INVALID. (#5419)
    When decrypting the p12 password in the KRA with a session key, we must unwrap
    the session key with DECRYPT usage, since the next operation does a decryption with
    the key.

--- base/kra/src/main/java/com/netscape/kra/RecoveryService.java
+++ base/kra/src/main/java/com/netscape/kra/RecoveryService.java
@@ -197,10 +197,9 @@ public class RecoveryService implements IService {
                 if(useOAEPKeyWrap == true) {
                     wrapAlg = KeyWrapAlgorithm.RSA_OAEP;
                 }
-
                 SymmetricKey unwrappedSessionKey =
                         CryptoUtil.unwrap(token,  SymmetricKey.AES, 128,
-                        SymmetricKey.Usage.UNWRAP,
+                        SymmetricKey.Usage.DECRYPT,
                         transPrivateKey,
                         transWrappedSessionKey,
                         wrapAlg);
